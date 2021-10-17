c569391900cac253242ca586c7b925d84d4de122
LtJulius <senator.spartas@googlemail.com>
2021-10-17 21:26:00 +0200

    SDL Library, Cleanup
    * added sdl2 lib and include files
    * removed unused pseudo untracked files of vs solution

--- src/Engine.Assets/Library/Geometry/Geometry.cpp
+++
@@ -1,6 +0,0 @@
-#include "Geometry.hpp"
-
-using namespace clockwork::__internal::core;
-using namespace clockwork::__internal::asset::types;
-
-type_id Geometry::__typeId = "Geometry"_typeId;
--- src/Engine.Assets/Library/Material/Material.cpp
+++
@@ -1,6 +0,0 @@
-#include "Material.hpp"
-
-using namespace clockwork::__internal::core;
-using namespace clockwork::__internal::asset::types;
-
-type_id Material::__typeId = "Material"_typeId;
