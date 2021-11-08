f5adcdb1b16c67c28ed3352d2a8654fe6993eee7
Lt. Julius <2.senator.spartas@googlemail.com>
2021-11-08 11:04:41 +0100

    vkTranslate, Staged Schedule
    * added function to schedule batch tasks from component processor
    * added missing texture formats to support more displays

--- src/Engine.GFX/TextureFormat.hpp
+++ src/Engine.GFX/TextureFormat.hpp
@@ -3,80 +3,83 @@
 
 namespace ember::engine::gfx {
 
-    enum class TextureFormat {
-        eUndefined = 0x0,
+	enum class TextureFormat {
+		eUndefined = 0x0,
 
-        eR8Unorm,
-        eR8G8Unorm,
-        eR8G8B8Unorm,
-        eR8G8B8A8Unorm,
+		eR8Unorm,
+		eR8G8Unorm,
+		eR8G8B8Unorm,
+		eR8G8B8A8Unorm,
 
-        eR16Unorm,
-        eR16G16Unorm,
-        eR16G16B16Unorm,
-        eR16G16B16A16Unorm,
+		eB8G8R8Unorm,
+		eB8G8R8A8Unorm,
 
-        eR32Unorm,
-        eR32G32Unorm,
-        eR32G32B32Unorm,
-        eR32G32B32A32Unorm,
+		eR16Unorm,
+		eR16G16Unorm,
+		eR16G16B16Unorm,
+		eR16G16B16A16Unorm,
 
-        eR64Unorm,
-        eR64G64Unorm,
-        eR64G64B64Unorm,
-        eR64G64B64A64Unorm,
+		eR32Unorm,
+		eR32G32Unorm,
+		eR32G32B32Unorm,
+		eR32G32B32A32Unorm,
 
-        eR16Sfloat,
-        eR32Sfloat,
-        eR64Sfloat,
+		eR64Unorm,
+		eR64G64Unorm,
+		eR64G64B64Unorm,
+		eR64G64B64A64Unorm,
 
-        eR16G16B16Sfloat,
-        eR32G32B32Sfloat,
+		eR16Sfloat,
+		eR32Sfloat,
+		eR64Sfloat,
 
-        eR16G16B16A16Sfloat,
-        eR32G32B32A32Sfloat,
+		eR16G16B16Sfloat,
+		eR32G32B32Sfloat,
 
-        eD16Unorm,
-        eD32Sfloat,
-        eS8Uint,
-        eD16UnormS8Uint,
-        eD24UnormS8Uint,
-        eD32SfloatS8Uint
-    };
+		eR16G16B16A16Sfloat,
+		eR32G32B32A32Sfloat,
 
-    /**
-     * Query if 'format_' is depth format
-     *
-     * @author Julius
-     * @date 09.01.2021
-     *
-     * @param  format_ Describes the format to use.
-     *
-     * @returns True if depth format, false if not.
-     */
-    [[nodiscard]] bool isDepthFormat(cref<TextureFormat> format_) noexcept;
+		eD16Unorm,
+		eD32Sfloat,
+		eS8Uint,
+		eD16UnormS8Uint,
+		eD24UnormS8Uint,
+		eD32SfloatS8Uint
+	};
 
-    /**
-     * Query if 'format_' is stencil format
-     *
-     * @author Julius
-     * @date 09.01.2021
-     *
-     * @param  format_ Describes the format to use.
-     *
-     * @returns True if stencil format, false if not.
-     */
-    [[nodiscard]] bool isStencilFormat(cref<TextureFormat> format_) noexcept;
+	/**
+	 * Query if 'format_' is depth format
+	 *
+	 * @author Julius
+	 * @date 09.01.2021
+	 *
+	 * @param  format_ Describes the format to use.
+	 *
+	 * @returns True if depth format, false if not.
+	 */
+	[[nodiscard]] bool isDepthFormat(cref<TextureFormat> format_) noexcept;
 
-    /**
-     * Query if 'format_' is depth stencil format
-     *
-     * @author Julius
-     * @date 09.01.2021
-     *
-     * @param  format_ Describes the format to use.
-     *
-     * @returns True if depth stencil format, false if not.
-     */
-    [[nodiscard]] bool isDepthStencilFormat(cref<TextureFormat> format_) noexcept;
+	/**
+	 * Query if 'format_' is stencil format
+	 *
+	 * @author Julius
+	 * @date 09.01.2021
+	 *
+	 * @param  format_ Describes the format to use.
+	 *
+	 * @returns True if stencil format, false if not.
+	 */
+	[[nodiscard]] bool isStencilFormat(cref<TextureFormat> format_) noexcept;
+
+	/**
+	 * Query if 'format_' is depth stencil format
+	 *
+	 * @author Julius
+	 * @date 09.01.2021
+	 *
+	 * @param  format_ Describes the format to use.
+	 *
+	 * @returns True if depth stencil format, false if not.
+	 */
+	[[nodiscard]] bool isDepthStencilFormat(cref<TextureFormat> format_) noexcept;
 }
