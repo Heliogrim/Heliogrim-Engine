da5c5f69a74f83ef295fad47b262a15368c5cbb8
LtJulius <senator.spartas@googlemail.com>
2022-11-18 11:02:16 +0100

    Collapse
    * added collapse ui element to ui widget liib
    * replaced previous wrapper of value mapper with named collapses

--- src/Editor.UI/Modules/ObjectEditor/GeometryComponentValueMapper.cpp
+++ src/Editor.UI/Modules/ObjectEditor/GeometryComponentValueMapper.cpp
@@ -16,6 +16,7 @@
 #include "../../Color/Dark.hpp"
 #include "../../Style/Style.hpp"
 #include "../../Widget/Input/InputVec.hpp"
+#include "../../Widget/Collapse.hpp"
 #include "Editor.UI/Widget/Input/InputAsset.hpp"
 #include "Ember/Ember.hpp"
 
@@ -97,50 +98,79 @@ void ObjectValueMapper<StaticGeometryComponent>::build(cref<sptr<engine::reflow:
     title->style().maxWidth = ReflowUnit { ReflowUnitType::eRelative, 1.F };
     title->setText(R"(<<Static Geometry Component>>)");
 
-    auto pos = make_sptr<InputVec3>();
-    auto rot = make_sptr<InputVec3>();
-    auto scale = make_sptr<InputVec3>();
-
-    pos->setLabel(0, "X");
-    pos->setLabel(1, "Y");
-    pos->setLabel(2, "Z");
-
-    rot->setLabel(0, "P");
-    rot->setLabel(1, "Y");
-    rot->setLabel(2, "R");
-
-    scale->setLabel(0, "X");
-    scale->setLabel(1, "Y");
-    scale->setLabel(2, "Z");
-
-    /**/
-
     parent_->addChild(title);
 
-    parent_->addChild(pos);
-    parent_->addChild(rot);
-    parent_->addChild(scale);
+    /**/
+    auto transform { make_sptr<Collapse>() };
+    transform->setup();
+    transform->getHeader()->setTitle("Transform");
+
+    parent_->addChild(transform);
+
+    {
+        const auto wrap { transform->getContent() };
+        wrap->style()->minHeight = ReflowUnit { ReflowUnitType::eAuto, 0.F };
+        wrap->style()->height = ReflowUnit { ReflowUnitType::eAuto, 0.F };
+        wrap->style()->wrap = ReflowWrap::eNoWrap;
+        wrap->style()->colGap = ReflowUnit { ReflowUnitType::eAbsolute, 4.F };
+        wrap->style()->color = color::Dark::backgroundDefault;
+
+        auto pos = make_sptr<InputVec3>();
+        auto rot = make_sptr<InputVec3>();
+        auto scale = make_sptr<InputVec3>();
+
+        pos->setLabel(0, "X");
+        pos->setLabel(1, "Y");
+        pos->setLabel(2, "Z");
+
+        rot->setLabel(0, "P");
+        rot->setLabel(1, "Y");
+        rot->setLabel(2, "R");
+
+        scale->setLabel(0, "X");
+        scale->setLabel(1, "Y");
+        scale->setLabel(2, "Z");
+
+        wrap->addChild(pos);
+        wrap->addChild(rot);
+        wrap->addChild(scale);
+    }
 
     /**/
 
-    auto staticMesh { make_sptr<InputAsset>() };
+    auto staticMesh { make_sptr<Collapse>() };
+    staticMesh->setup();
+    staticMesh->getHeader()->setTitle("Static Geometry");
+
     parent_->addChild(staticMesh);
 
-    /**/
+    {
+        const auto wrap { staticMesh->getContent() };
+        wrap->style()->minHeight = ReflowUnit { ReflowUnitType::eAuto, 0.F };
+        wrap->style()->height = ReflowUnit { ReflowUnitType::eAuto, 0.F };
+        wrap->style()->wrap = ReflowWrap::eNoWrap;
+        wrap->style()->colGap = ReflowUnit { ReflowUnitType::eAbsolute, 4.F };
+        wrap->style()->color = color::Dark::backgroundDefault;
 
-    auto materials {
-        make_sptr<VBox>(BoundStyleSheet::make(StyleSheet {
-            .minWidth { true, ReflowUnit { ReflowUnitType::eRelative, 1.F } },
-            .width { true, ReflowUnit { ReflowUnitType::eRelative, 1.F } },
-            .maxWidth { true, ReflowUnit { ReflowUnitType::eRelative, 1.F } },
-            .height { true, ReflowUnit { ReflowUnitType::eAuto, 0.F } },
-            .maxHeight { true, ReflowUnit { ReflowUnitType::eRelative, 1.F } },
-            .wrap { true, ReflowWrap::eNoWrap },
-            .colGap { true, ReflowUnit { ReflowUnitType::eAbsolute, 4.F } },
-            .color { true, color::Dark::backgroundDefault },
-        }))
-    };
+        auto assetInput { make_sptr<InputAsset>() };
+        wrap->addChild(assetInput);
+    }
+
+    /**/
+    auto materials { make_sptr<Collapse>() };
+    materials->setup();
+    materials->getHeader()->setTitle("Materials");
+
     parent_->addChild(materials);
+
+    {
+        const auto wrap { materials->getContent() };
+        wrap->style()->minHeight = ReflowUnit { ReflowUnitType::eAuto, 0.F };
+        wrap->style()->height = ReflowUnit { ReflowUnitType::eAuto, 0.F };
+        wrap->style()->wrap = ReflowWrap::eNoWrap;
+        wrap->style()->colGap = ReflowUnit { ReflowUnitType::eAbsolute, 4.F };
+        wrap->style()->color = color::Dark::backgroundDefault;
+    }
 }
 
 template <>
@@ -150,32 +180,42 @@ void ObjectValueMapper<StaticGeometryComponent>::update(cref<sptr<engine::reflow
     const auto& mat { sgc.getWorldTransform() };
     const auto& children { *parent_->children() };
 
-    _STD static_pointer_cast<InputVec3, Widget>(children[1])->setValue(mat.position());
-    _STD static_pointer_cast<InputVec3, Widget>(children[2])->setValue(mat.rotation().euler());
-    _STD static_pointer_cast<InputVec3, Widget>(children[3])->setValue(mat.scale());
+    auto* const transform { static_cast<ptr<Collapse>>(children[1].get()) };
 
-    _STD static_pointer_cast<InputAsset, Widget>(children[4])->setValue(sgc.getStaticGeometryGuid());
+    {
+        const auto wrap { transform->getContent() };
 
-    /**/
+        _STD static_pointer_cast<InputVec3, Widget>((*wrap->children())[0])->setValue(mat.position());
+        _STD static_pointer_cast<InputVec3, Widget>((*wrap->children())[1])->setValue(mat.rotation().euler());
+        _STD static_pointer_cast<InputVec3, Widget>((*wrap->children())[2])->setValue(mat.scale());
 
-    _STD static_pointer_cast<InputVec3, Widget>(children[1])->_callback = [sgc = &sgc](math::vec3 value_) {
-        const_cast<ref<Transform>>(sgc->getWorldTransform()).setPosition(_STD move(value_));
-    };
+        _STD static_pointer_cast<InputVec3, Widget>((*wrap->children())[0])->_callback = [sgc = &sgc
+            ](math::vec3 value_) {
+                const_cast<ref<Transform>>(sgc->getWorldTransform()).setPosition(_STD move(value_));
+            };
 
-    _STD static_pointer_cast<InputVec3, Widget>(children[2])->_callback = [sgc = &sgc](math::vec3 value_) {
-        const_cast<ref<Transform>>(sgc->getWorldTransform()).setRotation(
-            math::quaternion::euler(value_));
-    };
+        _STD static_pointer_cast<InputVec3, Widget>((*wrap->children())[1])->_callback = [sgc = &sgc
+            ](math::vec3 value_) {
+                const_cast<ref<Transform>>(sgc->getWorldTransform()).setRotation(
+                    math::quaternion::euler(value_));
+            };
 
-    _STD static_pointer_cast<InputVec3, Widget>(children[3])->_callback = [sgc = &sgc](math::vec3 value_) {
+        _STD static_pointer_cast<InputVec3, Widget>((*wrap->children())[2])->_callback = [sgc = &sgc
+            ](math::vec3 value_) {
 
-        const_cast<ref<Transform>>(sgc->getWorldTransform()).setScale(_STD move(value_));
-    };
+                const_cast<ref<Transform>>(sgc->getWorldTransform()).setScale(_STD move(value_));
+            };
+    }
+
+    const auto staticMeshWrap { static_cast<ptr<Collapse>>(children[2].get())->getContent() };
+    const auto staticMesh { static_cast<ptr<InputAsset>>(staticMeshWrap->children()->front().get()) };
+
+    staticMesh->setValue(sgc.getStaticGeometryGuid());
 
     /**/
 
     const auto& sga { sgc.getStaticGeometryAsset() };
-    const auto materials { _STD static_pointer_cast<VBox, Widget>(children[5]) };
+    const auto materials { _STD static_pointer_cast<Collapse, Widget>(children[3])->getContent() };
     const auto overrides { sgc.overrideMaterials() };
 
     const auto materialSlotCount { materials->children()->size() };
