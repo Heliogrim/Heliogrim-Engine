13f59adc70c52344e8a99dd81703d10a265a129b
LtJulius <senator.spartas@googlemail.com>
2022-12-12 17:51:22 +0100

    Async Helper
    * added helper function to wait on atomic variable until a value arrives or changes

---
+++ src/Engine.Scheduler/Helper/Wait.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <atomic>
+
+#include <Engine.Common/Wrapper.hpp>
+#include <Engine.Common/Types.hpp>
+
+namespace ember::engine::scheduler {
+    /**
+     * Wait on a atomic variable
+     *
+     * @details This function will determine the current execution context
+     *  and use a proper way to wait on an atomic integral with an expectation.
+     *
+     * @param atomic_ The variable holding the atomic value.
+     * @param prev_ The value which should change.
+     */
+    void waitOnAtomic(ref<_STD atomic_flag> atomic_, const bool prev_);
+
+    /**
+     * Wait on a atomic variable
+     *
+     * @details This function will determine the current execution context
+     *  and use a proper way to wait on an atomic integral with an expectation.
+     *
+     * @param atomic_ The variable holding the atomic value.
+     * @param prev_ The value which should change.
+     */
+    void waitOnAtomic(ref<_STD atomic_uint_fast8_t> atomic_, const u8 prev_);
+
+    /**
+     * Wait until a atomic variable matches the expectation
+     *
+     * @details This function will determine the current execution context
+     *  and use a proper way to wait on an atomic integral with an expectation.
+     *
+     * @param atomic_ The variable holding the atomic value.
+     * @param expect_ The value to wait for.
+     */
+    void waitUntilAtomic(ref<_STD atomic_flag> atomic_, const bool expect_);
+
+    /**
+     * Wait until a atomic variable matches the expectation
+     *
+     * @details This function will determine the current execution context
+     *  and use a proper way to wait on an atomic integral with an expectation.
+     *
+     * @param atomic_ The variable holding the atomic value.
+     * @param expect_ The value to wait for.
+     */
+    void waitUntilAtomic(ref<_STD atomic_uint_fast8_t> atomic_, const u8 expect_);
+}
