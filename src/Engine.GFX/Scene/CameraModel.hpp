56f5fe5714ff699def68f8006c1cb3ccd725dda7
LtJulius <senator.spartas@googlemail.com>
2022-12-14 18:13:32 +0100

    Graphics Camera Model
    * added camera model as component representation to graphics

---
+++ src/Engine.GFX/Scene/CameraModel.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "SceneNodeModel.hpp"
+
+namespace ember::engine::gfx::scene {
+    class SceneView;
+}
+
+namespace ember::engine::gfx {
+    class CameraModel final :
+        public SceneNodeModel {
+    public:
+        using this_type = CameraModel;
+
+        inline constexpr static type_id typeId { "CameraModel"_typeId };
+
+    public:
+        CameraModel(const non_owning_rptr<SceneComponent> owner_);
+
+        ~CameraModel() override final;
+
+    public:
+        void create(const ptr<engine::scene::Scene> scene_) override;
+
+        void update(const ptr<engine::scene::Scene> scene_) override;
+
+        void destroy(const ptr<engine::scene::Scene> scene_) override;
+
+    public:
+        [[nodiscard]] Vector<render::RenderDataToken> providedToken() const noexcept override;
+
+        [[nodiscard]] ptr<cache::ModelBatch> batch(const ptr<render::RenderPassState> state_) override;
+
+    private:
+        uptr<scene::SceneView> _sceneView;
+
+    public:
+        [[nodiscard]] const non_owning_rptr<scene::SceneView> getSceneView() const noexcept;
+    };
+}
