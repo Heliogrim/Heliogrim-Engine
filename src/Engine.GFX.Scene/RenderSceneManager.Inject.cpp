08a2d02fb5c4a51c7b33f279e23c854bc3800ac0
LtJulius <senator.spartas@googlemail.com>
2022-12-14 18:14:13 +0100

    Camera Model Injection
    * added camera model to scene type injection

--- src/Engine.GFX.Scene/RenderSceneManager.Inject.cpp
+++ src/Engine.GFX.Scene/RenderSceneManager.Inject.cpp
@@ -1,10 +1,12 @@
 #include <Ember/SkyboxComponent.hpp>
 #include <Ember/StaticGeometryComponent.hpp>
 #include <Ember/UIComponent.hpp>
+#include <Ember/Components/CameraComponent.hpp>
 #include <Engine.GFX/Scene/SceneTag.hpp>
 #include <Engine.GFX/Scene/StaticGeometryModel.hpp>
 #include <Engine.GFX/Scene/SkyboxModel.hpp>
 #include <Engine.GFX.Glow.UI/Scene/UISceneModel.hpp>
+#include <Engine.GFX/Scene/CameraModel.hpp>
 #include <Engine.Scene/RevScene.hpp>
 
 #include "RenderSceneManager.hpp"
@@ -17,6 +19,11 @@ void RenderSceneManager::injectSceneHooks(const ptr<RenderScene> renderScene_) {
 
     auto* const scene { static_cast<const ptr<::ember::engine::scene::RevScene>>(renderScene_) };
     constexpr GfxSceneTag tag {};
+
+    /**/
+
+    scene->setNodeType(tag, CameraComponent::typeId,
+        EmberObject::create<CameraModel, const ptr<SceneComponent>>);
 
     /**/
 
