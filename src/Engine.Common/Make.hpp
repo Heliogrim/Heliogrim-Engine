91bf8bba7e68f09ef07f778d5f44f988584eb430
LtJulius <senator.spartas@googlemail.com>
2023-01-23 00:28:15 +0100

    Make Shared Memory References
    * added make helper for shared memory references
    * added conversion helper from unqiue pointer to shared memory reference
    * fixed inital ref-count bumping for shared memory references, causing memory leak due to missing dec

--- src/Engine.Common/Make.hpp
+++ src/Engine.Common/Make.hpp
@@ -3,8 +3,10 @@
 #include "__macro.hpp"
 #include "Wrapper.hpp"
 
-namespace ember {
+#include "Concurrent/SharedMemoryReference.hpp"
+#include "Meta/Concept.hpp"
 
+namespace ember {
     template <class Ty, class... Args>
     ptr<Ty> make_ptr(Args&&... args_) {
         return new Ty(_STD forward<Args>(args_)...);
@@ -18,5 +20,30 @@ namespace ember {
     template <class Ty, class... Args>
     uptr<Ty> make_uptr(Args&&... args_) {
         return _STD make_unique<Ty>(_STD forward<Args>(args_)...);
+    }
+
+    /* Shared Memory References */
+
+    template <class Ty, class... Args>
+    smr<Ty> make_smr(Args&&... args_);
+
+    /**/
+
+    template <class Ty>
+    smr<Ty> make_smr(mref<uptr<Ty>> unique_) {
+        auto* const ctrl = new SharedMemoryReferenceCtrlBlock<Ty>(unique_.release());
+        return ctrl->acq();
+    }
+
+    template <class Ty> requires decayed_as<Ty, Ty>
+    smr<Ty> make_smr(mref<ptr<Ty>> ptr_) {
+        auto* const ctrl = new SharedMemoryReferenceCtrlBlock<Ty>(_STD move(ptr_));
+        return ctrl->acq();
+    }
+
+    template <class Ty, class... Args>
+    smr<Ty> make_smr(Args&&... args_) {
+        auto* const ctrl = new SharedMemoryReferenceCtrlBlock<Ty>(new Ty(_STD forward<Args>(args_)...));
+        return ctrl->acq();
     }
 }
