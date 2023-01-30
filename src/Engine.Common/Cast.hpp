7fb77c64874b34a9ff7ea93489932521ec34444f
LtJulius <senator.spartas@googlemail.com>
2023-01-31 00:39:34 +0100

    Convertible smr<Ty_> -> smr<Tx_>
    * added converting move constructor to smr
    * added void cast to transfer type enforce control block
    
    (cherry picked from commit 4180be9ec046936457e069c213ac93e3cf85cb1c)

--- src/Engine.Common/Cast.hpp
+++ src/Engine.Common/Cast.hpp
@@ -1,10 +1,9 @@
 #pragma once
 
 namespace ember {
-
     template <typename From_, typename To_, typename VoidType = void>
     struct void_cast {
-        [[nodiscard]] To_ operator()(From_ value_) const noexcept {
+        [[nodiscard]] constexpr To_ operator()(From_ value_) const noexcept {
             return *reinterpret_cast<To_*>(
                 reinterpret_cast<VoidType*>(&value_)
             );
@@ -19,4 +18,18 @@ namespace ember {
 
     template <typename From_, typename To_>
     struct void_cast<From_&, To_&> { };
+
+    template <typename Ty_, typename Ux_, typename Vy_ = void>
+    [[nodiscard]] constexpr Ty_* _void_cast(Ux_* ptr_) noexcept {
+        return reinterpret_cast<Ty_*>(reinterpret_cast<Vy_*>(ptr_));
+    }
+
+    template <typename Ty_, typename Ux_, typename Vy_ = void>
+    [[nodiscard]] constexpr _STD remove_pointer_t<Ty_>* _void_cast(
+        _STD remove_pointer_t<_STD remove_reference_t<Ux_>>*&& ptr_
+    ) noexcept {
+        using ty = _STD remove_pointer_t<Ty_>;
+        using ux = _STD remove_pointer_t<_STD remove_reference_t<Ux_>>;
+        return reinterpret_cast<ty*>(reinterpret_cast<Vy_*>(_STD forward<ux*>(ptr_)));
+    }
 }
