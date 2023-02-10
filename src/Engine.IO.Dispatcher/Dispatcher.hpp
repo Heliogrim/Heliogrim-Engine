d7e52cf60c2755284e643b119bff413c177246e4
LtJulius <senator.spartas@googlemail.com>
2023-02-10 09:08:21 +0100

    IO Dispatcher
    * added io dispatcher as library to project

---
+++ src/Engine.IO.Dispatcher/Dispatcher.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <Engine.Common/__macro.hpp>
+#include <Engine.Common/Wrapper.hpp>
+#include <Engine.Common/Functional/Function.hpp>
+#include <Engine.Common/Meta/Async.hpp>
+#include <Engine.Scheduler/Fiber/Awaitable.hpp>
+
+namespace ember::engine::io {
+    class Dispatcher;
+}
+
+namespace ember::engine {
+    [[nodiscard]] extern __declspec(restrict) const ptr<io::Dispatcher> _io_ptr() noexcept;
+
+    [[nodiscard]] FORCE_INLINE cref<io::Dispatcher> _io_() {
+        return *_io_ptr();
+    }
+}
+
+namespace ember::engine::io {
+    class Dispatcher {
+    public:
+        using this_type = Dispatcher;
+
+    public:
+        Dispatcher();
+
+        ~Dispatcher() = default;
+
+    public:
+        void operator()(_In_ mref<_STD function<void()>> fnc_, sync_t) const;
+
+        [[nodiscard]] smr<scheduler::fiber::await_signal_type> operator()(
+            _In_ mref<_STD function<void()>> fnc_,
+            async_t
+        ) const;
+    };
+}
