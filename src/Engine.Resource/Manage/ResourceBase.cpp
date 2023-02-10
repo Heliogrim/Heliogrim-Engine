b4ba8b7aebb9ff57a1e8f7368dc0ede0343531cd
LtJulius <senator.spartas@googlemail.com>
2023-02-10 09:09:17 +0100

    Associative Assets, Stream Ops
    * renamed stream loader ops to stream ops to support loading and unloading
    * added associative pointer to indicate asset resource relations

--- src/Engine.Resource/Manage/ResourceBase.cpp
+++ src/Engine.Resource/Manage/ResourceBase.cpp
@@ -8,3 +8,11 @@ ResourceBase::ResourceBase() = default;
 bool ResourceBase::isLoaded() const noexcept {
     return this->loaded() != 0x0ui8;
 }
+
+non_owning_rptr<const EmberObject> ResourceBase::getAssociation() const noexcept {
+    return _association;
+}
+
+void ResourceBase::setAssociation(const non_owning_rptr<const EmberObject> association_) noexcept {
+    _association = association_;
+}
