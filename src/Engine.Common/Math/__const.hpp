e74409161c83aae8c657ab5169101a3196b58652
LtJulius <senator.spartas@googlemail.com>
2023-03-08 12:46:17 +0100

    Transform Migration
    * changed transform to provide location and rotator instead of vectors

--- src/Engine.Common/Math/__const.hpp
+++ src/Engine.Common/Math/__const.hpp
@@ -7,6 +7,7 @@ namespace hg::math {
 
     constexpr float pi_f = 3.1415926535897932384626433832795028841971F;
     constexpr float pih_f = 1.5707963267948966192313216916397514420986F;
+    constexpr float pi2_f = pi_f * 2.F;
     constexpr float ln2_f = 0.69314718055994530941723212145818F;
     constexpr float ln10_f = 2.3025850929940456840179914546844F;
 
