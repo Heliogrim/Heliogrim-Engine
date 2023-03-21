9d2030a616ac475bbfce54fd1ef4fe9719d952e6
LtJulius <senator.spartas@googlemail.com>
2023-03-21 15:13:37 +0100

    Library: Ankerl Dense Map
    * added ankerl's dense map as collection

---
+++ src/Engine.Common/Collection/DenseMap.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <Engine.Common/Wrapper.hpp>
+
+#include <ankerl/unordered_dense.h>
+
+namespace hg {
+    template <
+        class Key_,
+        class Type_,
+        class Hash_ = ankerl::unordered_dense::v3_1_1::hash<Key_>,
+        class Equal_ = _STD equal_to<Key_>,
+        class Alloc_ = _STD allocator<_STD pair<Key_, Type_>>>
+    using DenseMap = ankerl::unordered_dense::map<Key_, Type_, Hash_, Equal_, Alloc_>;
+}
