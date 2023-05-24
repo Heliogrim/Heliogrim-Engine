998a03d03a6e2d38504ac3a18c70d94a7ba08402
LtJulius <senator.spartas@googlemail.com>
2023-05-24 15:40:06 +0200

    Memory Pointer
    Common:
    * added specified types of memory pointers to determine ownership and atomicity
    * added forward declarations for memory pointers
    * added test cases for memory pointers to cover intentional scenarios
    * dropped initialization flagging, cause we don't want to build managed pointers ( there are other managed pointers for those purposes )

--- src/Engine.Common.Test/test.cpp
+++ src/Engine.Common.Test/test.cpp
@@ -14,6 +14,12 @@ TEST(__DummyTest__, Exists) {
 
 namespace Common {
     namespace Memory {
+        struct TestPayload01 {
+            u64 v0;
+            s64 v1;
+            _STD string v2;
+        };
+
         TEST(MemoryPointer, IntegralTyped) {
 
             using type = u64;
@@ -34,6 +40,176 @@ namespace Common {
 
             mp0.destroy();
             ASSERT_FALSE(mp0);
+        }
+
+        TEST(MemoryPointer, MovingOwner) {
+
+            using type = TestPayload01;
+            using mpt = MemoryPointer<type>;
+
+            mpt mp0 {};
+
+            ASSERT_FALSE(mp0);
+
+            mp0.create();
+
+            ASSERT_TRUE(mp0);
+
+            mpt mp1 { _STD move(mp0) };
+
+            ASSERT_FALSE(mp0);
+            ASSERT_TRUE(mp1);
+
+            mpt mp2 {};
+
+            // Required to prevent optimization
+            ASSERT_FALSE(mp2);
+
+            mp2 = _STD move(mp1);
+
+            ASSERT_FALSE(mp1);
+            ASSERT_TRUE(mp2);
+
+            mp2.destroy();
+
+            ASSERT_FALSE(mp2);
+        }
+
+        TEST(MemoryPointer, OwningToNonOwning) {
+
+            using type = TestPayload01;
+            using mpt = MemoryPointer<type>;
+            using nmpt = mpt::non_owning_type;
+
+            mpt mp0 {};
+            nmpt nmp0 {};
+
+            ASSERT_FALSE(mp0);
+            ASSERT_FALSE(nmp0);
+
+            mp0.create();
+            nmp0 = mp0;
+
+            ASSERT_TRUE(mp0);
+            ASSERT_TRUE(nmp0);
+
+            nmpt nmp1 { mp0 };
+
+            ASSERT_TRUE(mp0);
+            ASSERT_TRUE(nmp1);
+
+            nmpt nmp2 {};
+
+            // Required to prevent optimization
+            ASSERT_FALSE(nmp2);
+
+            nmp2 = nmp0;
+
+            ASSERT_TRUE(nmp0);
+            ASSERT_TRUE(nmp2);
+
+            nmp2 = _STD move(nmp0);
+
+            ASSERT_FALSE(nmp0);
+            ASSERT_TRUE(nmp2);
+
+            mp0.destroy();
+        }
+
+        TEST(AtomicMemoryPointer, IntegralTyped) {
+
+            using type = u64;
+            using mpt = AtomicMemoryPointer<type>;
+
+            mpt mp0 {};
+            ASSERT_FALSE(mp0);
+
+            mp0.create();
+
+            ASSERT_TRUE(mp0);
+            ASSERT_EQ(*mp0, 0ui64);
+
+            u64 test = 4379689246ui64;
+            mp0.store(test);
+
+            ASSERT_EQ(*mp0, test);
+
+            mp0.destroy();
+            ASSERT_FALSE(mp0);
+        }
+
+        TEST(AtomicMemoryPointer, MovingOwner) {
+
+            using type = TestPayload01;
+            using mpt = AtomicMemoryPointer<type>;
+
+            mpt mp0 {};
+
+            ASSERT_FALSE(mp0);
+
+            mp0.create();
+
+            ASSERT_TRUE(mp0);
+
+            mpt mp1 { _STD move(mp0) };
+
+            ASSERT_FALSE(mp0);
+            ASSERT_TRUE(mp1);
+
+            mpt mp2 {};
+
+            // Required to prevent optimization
+            ASSERT_FALSE(mp2);
+
+            mp2 = _STD move(mp1);
+
+            ASSERT_FALSE(mp1);
+            ASSERT_TRUE(mp2);
+
+            mp2.destroy();
+
+            ASSERT_FALSE(mp2);
+        }
+
+        TEST(AtomicMemoryPointer, OwningToNonOwning) {
+
+            using type = TestPayload01;
+            using mpt = AtomicMemoryPointer<type>;
+            using nmpt = mpt::non_owning_type;
+
+            mpt mp0 {};
+            nmpt nmp0 {};
+
+            ASSERT_FALSE(mp0);
+            ASSERT_FALSE(nmp0);
+
+            mp0.create();
+            nmp0 = mp0;
+
+            ASSERT_TRUE(mp0);
+            ASSERT_TRUE(nmp0);
+
+            nmpt nmp1 { mp0 };
+
+            ASSERT_TRUE(mp0);
+            ASSERT_TRUE(nmp1);
+
+            nmpt nmp2 {};
+
+            // Required to prevent optimization
+            ASSERT_FALSE(nmp2);
+
+            nmp2 = nmp0;
+
+            ASSERT_TRUE(nmp0);
+            ASSERT_TRUE(nmp2);
+
+            nmp2 = _STD move(nmp0);
+
+            ASSERT_FALSE(nmp0);
+            ASSERT_TRUE(nmp2);
+
+            mp0.destroy();
         }
     }
 
