108bde8add7ee743f6e34b32ba198993f0bd8768
LtJulius <senator.spartas@googlemail.com>
2023-06-14 17:19:41 +0200

    AssocKey Custom  Hash
    Common:
    * added support for custom hasher (trivial struct) within associative keys
    ** will rehash, when copied or moved with changing hasher type
    ** added constexpr to hash of AssocKey < Ty_ >

--- src/Engine.Common/Collection/AssociativeKey.hpp
+++ src/Engine.Common/Collection/AssociativeKey.hpp
@@ -6,12 +6,31 @@
 #include "../Wrapper.hpp"
 
 namespace hg {
-    template <typename KeyType_, class Hasher = _STD hash<KeyType_>>
+    template <typename KeyType_, class Hasher_ = _STD hash<KeyType_>>
     struct AssocKey {
-        using this_type = AssocKey<KeyType_>;
+        using this_type = AssocKey<KeyType_, Hasher_>;
 
         using hash_type = size_t;
         using value_type = KeyType_;
+
+    public:
+        constexpr AssocKey(mref<hash_type> hash_, mref<value_type> key_) :
+            hash(_STD move(hash_)),
+            value(_STD move(key_)) {}
+
+        constexpr AssocKey(cref<hash_type> hash_, cref<value_type> key_) :
+            hash(hash_),
+            value(key_) {}
+
+        template <class HasherTx_ = _STD hash<KeyType_>> requires (not _STD is_same_v<Hasher_, HasherTx_>)
+        constexpr AssocKey(mref<AssocKey<KeyType_, HasherTx_>> other_) :
+            hash(Hasher_ {}.operator()(other_.value)),
+            value(_STD move(other_.value)) {}
+
+        template <class HasherTx_ = _STD hash<KeyType_>> requires _STD is_same_v<Hasher_, HasherTx_>
+        constexpr AssocKey(mref<AssocKey<KeyType_, HasherTx_>> other_) :
+            hash(_STD move(other_.hash)),
+            value(_STD move(other_.value)) {}
 
         hash_type hash { ~0ui64 };
         value_type value;
@@ -40,26 +59,28 @@ namespace hg {
         }
 
     public:
-        static this_type from(mref<value_type> value_) {
+        constexpr static this_type from(mref<value_type> value_) {
             return this_type {
-                Hasher {}.operator()(value_),
+                Hasher_ {}.operator()(value_),
                 _STD move(value_)
             };
         }
 
-        static this_type from(cref<value_type> value_) {
+        constexpr static this_type from(cref<value_type> value_) {
             return this_type {
-                Hasher {}.operator()(value_),
+                Hasher_ {}.operator()(value_),
                 value_
             };
         }
     };
+
+    /**/
 }
 
 namespace std {
     template <typename KeyType_>
     struct hash<::hg::AssocKey<KeyType_>> {
-        [[nodiscard]] size_t operator()(::hg::cref<::hg::AssocKey<KeyType_>> obj_) const noexcept {
+        [[nodiscard]] constexpr size_t operator()(::hg::cref<::hg::AssocKey<KeyType_>> obj_) const noexcept {
             return obj_.hash;
         }
     };
