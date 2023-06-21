bc9e954e531e00c7ec7b0bc0bc3612310e0267c6
LtJulius <senator.spartas@googlemail.com>
2023-06-21 16:18:06 +0200

    GitLab CI
    * reworked ci implementation for linux runners
    // TODO: Find solution for vulkan installation

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,3 +1,27 @@
+# Dependency Resolving
+
+macro(set_dep_policy)
+    cmake_policy(SET CMP0077 NEW)
+    set(CMAKE_POLICY_DEFAULT_CMP0077 NEW)
+
+    if (POLICY CMP0126)
+        cmake_policy(SET CMP0126 NEW)
+        set(CMAKE_POLICY_DEFAULT_CMP0216 NEW)
+    endif ()
+
+    if (POLICY CMP0135)
+        cmake_policy(SET CMP0135 NEW)
+        set(CMAKE_POLICY_DEFAULT_CMP0135 NEW)
+    endif ()
+
+    if (POLICY CMP0148)
+        cmake_policy(SET CMP0148 NEW)
+        set(CMAKE_POLICY_DEFAULT_CMP0148 NEW)
+    endif ()
+endmacro()
+set_dep_policy()
+
+
 # Dependencies
 
 include(FindVulkan)
