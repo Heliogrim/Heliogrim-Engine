256d9fc278cb76d7f2a5fcf77357ec0f38823c07
LtJulius <senator.spartas@googlemail.com>
2023-07-06 17:08:33 +0200

    Pedantic AsImpl
    C++ Language:
    * added as_impl specialization to support multi-alias with strong-typing
    * added missing make include within clone implementation

--- src/Engine.Pedantic/As/As.hpp
+++ src/Engine.Pedantic/As/As.hpp
@@ -3,6 +3,22 @@
 #include <concepts>
 
 namespace hg {
+    template <typename AliasType_, typename SourceType_>
+    struct as_impl;
+
     template <typename Type_>
     concept is_aliasable = _STD false_type::value;
+
+    template <typename AliasType_, typename SourceType_>
+    [[nodiscard]] constexpr AliasType_ as(const SourceType_& val_) {
+        return as_impl<AliasType_, SourceType_> {}(val_);
+    }
+
+    template <typename AliasType_, typename SourceType_> requires
+        (not _STD is_pointer_v<AliasType_>) && (not _STD is_pointer_v<SourceType_>)
+    struct as_impl<const AliasType_&, SourceType_> {
+        [[nodiscard]] constexpr const AliasType_& operator()(const SourceType_& val_) const noexcept {
+            return val_;
+        }
+    };
 }
