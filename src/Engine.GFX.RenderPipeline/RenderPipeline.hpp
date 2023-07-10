3f5ce529286ae27ef8061b2d121b9d925acebb56
LtJulius <senator.spartas@googlemail.com>
2023-07-10 21:21:23 +0200

    Render Pipeline
    Graphics:
    * added project to manage render pipelines and implementations

---
+++ src/Engine.GFX.RenderPipeline/RenderPipeline.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace hg::engine::gfx::render {
+    class RenderPipeline {
+    public:
+        using this_type = RenderPipeline;
+
+    public:
+        RenderPipeline() noexcept = default;
+
+        ~RenderPipeline() noexcept = default;
+    };
+}
