2d3df1cce48cc2b75b067e74591d333fe621438c
LtJulius <senator.spartas@googlemail.com>
2023-07-20 08:50:16 +0200

    Compile Tables
    C++ Language:
    * added constexpr sort to use for compile time generation
    * added helper to provide hash functions specifically for compile time usage
    * added helper to provide relational functions specifically for compile time usage
    * added basic table to hold constexpr data
    * added basic map on top of constexpr table
    * added basic hash map on top of constexpr table
    
    Test:
    * added test to generate constexpr maps
    * added test to check integrity of constexpr maps

---
+++ src/Engine.Reflect/Compile/HashMap.hpp
@@ -0,0 +1,254 @@
+#pragma once
+
+#include "Table.hpp"
+#include "Hash.hpp"
+
+namespace hg {
+    template <typename Type_, size_t Size_, typename Hash_ = CompileHash<Type_>>
+    struct CompileTableHashKey :
+        public Hash_ {
+        using type = CompileTableHashKey<Type_, Size_, Hash_>;
+        using value_type = Type_;
+
+    public:
+        constexpr CompileTableHashKey(const size_t hash_, const value_type& value_) :
+            hash(hash_),
+            data(value_) {}
+
+        constexpr CompileTableHashKey(const value_type& value_) :
+            hash(static_cast<Hash_&>(*this)(value_)),
+            data(value_) {}
+
+        template <typename Ty_ = Type_> requires _STD is_same_v<Ty_, Type_> && _STD is_copy_constructible_v<Ty_>
+        constexpr CompileTableHashKey(const Ty_& other_) noexcept :
+            hash(other_.hash),
+            data(other_.data) {}
+
+        template <typename Ty_ = Type_> requires _STD is_same_v<Ty_, Type_> && _STD is_move_constructible_v<Ty_>
+        constexpr CompileTableHashKey(Ty_&& other_) noexcept :
+            hash(other_.hash),
+            data(_STD move(other_.data)) {}
+
+        constexpr ~CompileTableHashKey() noexcept = default;
+
+    public:
+        size_t hash;
+        value_type data;
+
+    public:
+        template <typename Ty_ = type> requires
+            _STD is_same_v<Ty_, type> && _STD is_copy_assignable_v<typename Ty_::value_type>
+        constexpr type& operator=(const Ty_& other_) noexcept {
+            if (_STD addressof(other_) != this) {
+                hash = other_.hash;
+                data = other_.data;
+            }
+            return *this;
+        }
+
+        template <typename Ty_ = type> requires
+            _STD is_same_v<Ty_, type> && _STD is_move_assignable_v<typename Ty_::value_type>
+        constexpr type& operator=(Ty_&& other_) noexcept {
+            if (_STD addressof(other_) != this) {
+                hash = other_.hash;
+                data = _STD move(other_.data);
+            }
+            return *this;
+        }
+
+    public:
+        constexpr bool operator<(const type& other_) const noexcept {
+            return hash < other_.hash;
+        }
+
+        constexpr bool operator>(const type& other_) const noexcept {
+            return hash > other_.hash;
+        }
+
+        constexpr bool operator<=(const type& other_) const noexcept {
+            return hash <= other_.hash;
+        }
+
+        constexpr bool operator>=(const type& other_) const noexcept {
+            return hash >= other_.hash;
+        }
+
+        constexpr bool operator==(const type& other_) const noexcept {
+            return hash == other_.hash;
+        }
+
+        constexpr bool operator!=(const type& other_) const noexcept {
+            return hash != other_.hash;
+        }
+
+        [[nodiscard]] constexpr _STD strong_ordering operator<=>(const type& other_) const noexcept {
+            return hash <=> other_.hash;
+        }
+    };
+
+    template <
+        typename Key_,
+        typename Value_,
+        size_t Size_,
+        typename Hash_ = CompileHash<Key_>>
+    class CompileHashMap {
+    public:
+        using this_type = CompileHashMap<Key_, Value_, Size_, Hash_>;
+
+        using key_type = typename CompileTableHashKey<Key_, Size_, Hash_>::type;
+        using value_type = Value_;
+
+    private:
+        struct CompileMapComparator {
+            using node_type = CompileTableNode<key_type, value_type>;
+
+            [[nodiscard]] constexpr bool operator()(cref<node_type> left_, cref<node_type> right_) const noexcept {
+                return _STD less {}(left_->first, right_->first);
+            }
+        };
+
+    public:
+        using comparator = CompileMapComparator;
+        using table_type = CompileTable<CompileTableNode<key_type, value_type>, Size_, comparator>;
+
+    public:
+        constexpr CompileHashMap(const typename table_type::node_type (&pairs_)[Size_]) :
+            _table(pairs_) {}
+
+        constexpr CompileHashMap(const _STD pair<key_type, value_type> (&pairs_)[Size_]) :
+            _table(pairs_) {}
+
+    private:
+        table_type _table;
+
+    public:
+        [[nodiscard]] constexpr auto begin() const noexcept {
+            return _table.begin();
+        }
+
+        [[nodiscard]] constexpr auto begin() noexcept {
+            return _table.begin();
+        }
+
+        [[nodiscard]] constexpr auto cbegin() noexcept {
+            return static_cast<const this_type&>(*this).begin();
+        }
+
+        [[nodiscard]] constexpr auto end() const noexcept {
+            return _table.end();
+        }
+
+        [[nodiscard]] constexpr auto end() noexcept {
+            return _table.end();
+        }
+
+        [[nodiscard]] constexpr auto cend() noexcept {
+            return static_cast<const this_type&>(*this).end();
+        }
+
+    public:
+        [[nodiscard]] constexpr auto empty() const noexcept {
+            static_assert(begin() != end());
+            return false;
+        }
+
+        [[nodiscard]] constexpr auto size() const noexcept {
+            return Size_;
+        }
+
+    public:
+        [[nodiscard]] constexpr auto find(const key_type& key_) const noexcept {
+
+            // Threshold where to switch between linear search and binary hash probing
+            constexpr size_t threshold = 56;
+            if constexpr (Size_ < threshold) {
+                return _STD ranges::find_if(
+                    _table,
+                    [key_](cref<typename table_type::node_type> node_) {
+                        return node_->first == key_;
+                    }
+                );
+            } else {
+                // TODO: Rework hashing for constexpr tables
+                return _STD ranges::lower_bound(
+                    _table,
+                    key_,
+                    CompileLess<key_type> {},
+                    [](cref<typename table_type::node_type> node_) {
+                        return node_->first;
+                    }
+                );
+            }
+        }
+
+        [[nodiscard]] constexpr auto find(const typename key_type::value_type& key_) const noexcept {
+            return find(key_type { key_ });
+        }
+
+        [[nodiscard]] constexpr bool contains(const key_type& key_) const noexcept {
+            return find(key_) != end();
+        }
+
+        [[nodiscard]] constexpr bool contains(const typename key_type::value_type& key_) const noexcept {
+            return find(key_) != end();
+        }
+
+        [[nodiscard]] constexpr size_t count(const key_type& key_) const noexcept {
+
+            const auto start = find(key_);
+            auto it = start;
+            const auto end = this->end();
+            for (; it != end && (*it)->first == key_; ++it) {
+                // __noop();
+            }
+
+            return _STD distance(start, it);
+        }
+
+        [[nodiscard]] constexpr size_t count(const typename key_type::value_type& key_) const noexcept {
+            return count(key_type { key_ });
+        }
+
+    public:
+        [[nodiscard]] constexpr auto operator[](const key_type& key_) const noexcept {
+            return find(key_);
+        }
+
+        [[nodiscard]] constexpr auto operator[](const typename key_type::value_type& key_) const noexcept {
+            return find(key_);
+        }
+    };
+
+    /**/
+
+    template <typename Key_, typename Value_, typename Hash_ = CompileHash<Key_>, size_t Size_>
+    [[nodiscard]] constexpr auto make_compile_hash_map(
+        const _STD pair<typename CompileHashMap<Key_, Value_, Size_, Hash_>::key_type, Value_> (&pairs_)[Size_]
+    ) {
+        return CompileHashMap<Key_, Value_, Size_, Hash_> { pairs_ };
+    }
+
+    template <typename Key_, typename Value_, typename Hash_ = CompileHash<Key_>, size_t Size_>
+    [[nodiscard]] constexpr auto make_compile_hash_map(
+        const typename CompileHashMap<Key_, Value_, Size_, Hash_>::table_type::node_type (&nodes_)[Size_]
+    ) {
+        return CompileHashMap<Key_, Value_, Size_, Hash_> { nodes_ };
+    }
+
+    template <typename Key_, typename Value_, typename Hash_ = CompileHash<Key_>, typename... PairTypes_>
+    [[nodiscard]] constexpr auto make_compile_hash_map(
+        const PairTypes_... pairs_
+    ) {
+        using map_type = CompileHashMap<Key_, Value_, sizeof...(pairs_), Hash_>;
+
+        static_assert(
+            sizeof...(pairs_) > 0,
+            "Requires at least one pair to construct compile map via vardiac factoring."
+        );
+
+        _STD pair<typename map_type::key_type, Value_> tmp[sizeof...(pairs_)] {
+            { { pairs_.first }, { pairs_.second } }...
+        };
+        return CompileHashMap<Key_, Value_, sizeof...(pairs_), Hash_> { tmp };
+    }
+}
---
+++ src/Engine.Reflect/Compile/Table.hpp
@@ -0,0 +1,473 @@
+#pragma once
+
+#include <Engine.Common/__macro.hpp>
+#include <Engine.Common/Meta/Concept.hpp>
+
+#include "Relation.hpp"
+#include "Sort.hpp"
+
+namespace hg {
+    template <typename Key_, typename Value_>
+    using CompilePair = std::pair<Key_, Value_>;
+
+    template <typename Key_, typename Value_>
+    struct CompileTableNode {
+    public:
+        using this_type = CompileTableNode<Key_, Value_>;
+
+        using key_type = Key_;
+        using value_type = Value_;
+
+        using mapping = CompilePair<key_type, value_type>;
+
+    public:
+        constexpr CompileTableNode() noexcept = default;
+
+        template <template <typename, typename> typename Type_> requires
+            _STD is_same_v<Type_<Key_, Value_>, mapping>
+        constexpr CompileTableNode(const Type_<Key_, Value_> data_) noexcept :
+            data(data_) {}
+
+        template <template <typename, typename> typename Type_> requires
+            (not _STD is_same_v<Type_<Key_, Value_>, mapping>) &&
+            _STD is_nothrow_convertible_v<Type_<Key_, Value_>, mapping>
+        constexpr CompileTableNode(const Type_<Key_, Value_> data_) noexcept :
+            data { data_ } {}
+
+        constexpr CompileTableNode(const this_type& other_) noexcept :
+            data(other_.data) {}
+
+        constexpr CompileTableNode(this_type&& other_) noexcept :
+            data(_STD move(other_.data)) {}
+
+        constexpr ~CompileTableNode() noexcept = default;
+
+    public:
+        mapping data;
+
+    public:
+        constexpr this_type& operator=(const this_type& other_) noexcept {
+            if (_STD addressof(other_) != this) {
+                data = other_.data;
+            }
+            return *this;
+        }
+
+        constexpr this_type& operator=(this_type&& other_) noexcept {
+            if (_STD addressof(other_) != this) {
+                data = _STD move(other_.data);
+            }
+            return *this;
+        }
+
+    public:
+        constexpr void swap(this_type& other_) noexcept {
+            CompileSwap<key_type> {}(data.first, other_.data.first);
+            CompileSwap<value_type> {}(data.second, other_.data.second);
+        }
+
+    public:
+        constexpr bool operator<(const this_type& other_) const noexcept {
+            return data.first < other_.data.first;
+        }
+
+        constexpr bool operator<(const key_type& other_) const noexcept {
+            return data.first < other_;
+        }
+
+    public:
+        constexpr const auto& operator*() const noexcept {
+            return data;
+        }
+
+        constexpr const auto* operator->() const noexcept {
+            return &data;
+        }
+    };
+
+    /**/
+
+    template <typename NodeType_, size_t Size_, typename Comparator_>
+    class CompileTable {
+    public:
+        using this_type = CompileTable<NodeType_, Size_, Comparator_>;
+
+        using node_type = NodeType_;
+        constexpr static size_t size = Size_;
+
+    private:
+        template <size_t... Idx_>
+        constexpr CompileTable(const node_type (&entries_)[Size_], _STD index_sequence<Idx_...>) noexcept :
+            _data { { entries_[Idx_] }... } {
+            static_assert(sizeof...(Idx_), "Size of index sequence has to match the element count.");
+            //compile_sort(_STD begin(_data), _STD end(_data), Comparator_ {});
+        }
+
+        template <typename Type_, size_t... Idx_> requires _STD is_nothrow_convertible_v<Type_, NodeType_>
+        constexpr CompileTable(const Type_ (&values_)[Size_], _STD index_sequence<Idx_...>) noexcept :
+            _data { (node_type { values_[Idx_] })... } {
+            static_assert(sizeof...(Idx_), "Size of index sequence has to match the element count.");
+            compile_sort(begin(), end(), Comparator_ {});
+        }
+
+    public:
+        constexpr CompileTable(const node_type (&entries_)[Size_]) noexcept :
+            CompileTable(entries_, _STD make_index_sequence<Size_>()) {}
+
+        template <typename Type_> requires _STD is_nothrow_convertible_v<Type_, NodeType_>
+        constexpr CompileTable(const Type_ (&values_)[Size_]) noexcept :
+            CompileTable(values_, _STD make_index_sequence<Size_>()) {}
+
+    private:
+        node_type _data[Size_];
+
+    public:
+        class Iterator;
+        class ConstIterator;
+
+        class Iterator {
+        public:
+            using this_type = Iterator;
+
+            #ifdef __cpp_lib_concepts
+            using iterator_concept = _STD random_access_iterator_tag;
+            #endif
+
+            using iterator_category = _STD random_access_iterator_tag;
+
+            using value_type = node_type;
+            using reference = node_type&;
+            using const_reference = const node_type&;
+
+            using pointer = node_type*;
+            using const_pointer = const node_type*;
+
+            using size_type = _STD size_t;
+            using difference_type = _STD ptrdiff_t;
+
+        public:
+            constexpr Iterator() noexcept = default;
+
+            constexpr Iterator(const pointer cursor_) noexcept :
+                _cursor(cursor_) {}
+
+            constexpr Iterator(this_type&& other_) noexcept = default;
+
+            constexpr Iterator(const this_type& other_) noexcept = default;
+
+            constexpr ~Iterator() noexcept = default;
+
+        private:
+            pointer _cursor;
+
+        public:
+            constexpr this_type& operator=(const this_type& other_) noexcept {
+                if (_STD addressof(other_) != this) {
+                    _cursor = other_._cursor;
+                }
+                return *this;
+            }
+
+            constexpr this_type& operator=(this_type&& other_) noexcept {
+                if (_STD addressof(other_) != this) {
+                    _cursor = other_._cursor;
+                }
+                return *this;
+            }
+
+        public:
+            [[nodiscard]] constexpr const_reference operator*() const noexcept {
+                return *_cursor;
+            }
+
+            [[nodiscard]] constexpr reference operator*() noexcept {
+                return *_cursor;
+            }
+
+            [[nodiscard]] constexpr const_pointer operator->() const noexcept {
+                return _cursor;
+            }
+
+        public:
+            [[nodiscard]] constexpr const_reference operator[](const difference_type offset_) const noexcept {
+                return _cursor[offset_];
+            }
+
+            [[nodiscard]] constexpr reference operator[](const difference_type offset_) noexcept {
+                return _cursor[offset_];
+            }
+
+        public:
+            constexpr this_type& operator++() {
+                ++_cursor;
+                return *this;
+            }
+
+            [[nodiscard]] constexpr this_type operator++(int) {
+                constexpr auto tmp { *this };
+                ++_cursor;
+                return tmp;
+            }
+
+            constexpr this_type& operator+=(difference_type offset_) noexcept {
+                _cursor += offset_;
+                return *this;
+            }
+
+            [[nodiscard]] constexpr this_type operator+(difference_type offset_) const noexcept {
+                this_type tmp { *this };
+                tmp += offset_;
+                return tmp;
+            }
+
+            constexpr this_type& operator--() {
+                ++_cursor;
+                return *this;
+            }
+
+            [[nodiscard]] constexpr this_type operator--(int) {
+                constexpr auto tmp { *this };
+                --_cursor;
+                return tmp;
+            }
+
+            constexpr this_type& operator-=(difference_type offset_) noexcept {
+                _cursor -= offset_;
+                return *this;
+            }
+
+            [[nodiscard]] constexpr this_type operator-(difference_type offset_) const noexcept {
+                this_type tmp { *this };
+                tmp -= offset_;
+                return tmp;
+            }
+
+        public:
+            [[nodiscard]] constexpr difference_type operator-(const this_type& other_) const noexcept {
+                return _cursor - other_._cursor;
+            }
+
+        public:
+            [[nodiscard]] constexpr bool operator==(const this_type& other_) const noexcept {
+                return _cursor == other_._cursor;
+            }
+
+            [[nodiscard]] constexpr bool operator==(const ConstIterator& other_) const noexcept;
+
+            [[nodiscard]] constexpr bool operator!=(const this_type& other_) const noexcept {
+                return _cursor != other_._cursor;
+            }
+
+            [[nodiscard]] constexpr bool operator!=(const ConstIterator& other_) const noexcept;
+
+            [[nodiscard]] constexpr _STD strong_ordering operator<=>(const this_type& other_) const noexcept {
+                return _STD _Unfancy(_cursor) <=> _STD _Unfancy(other_._cursor);
+            }
+
+            [[nodiscard]] constexpr _STD strong_ordering operator<=>(const ConstIterator& other_) const noexcept;
+        };
+
+        class ConstIterator {
+        public:
+            using this_type = ConstIterator;
+
+            #ifdef __cpp_lib_concepts
+            using iterator_concept = _STD random_access_iterator_tag;
+            #endif
+
+            using iterator_category = _STD random_access_iterator_tag;
+
+            using value_type = node_type;
+            using reference = node_type&;
+            using const_reference = const node_type&;
+
+            using pointer = node_type*;
+            using const_pointer = const node_type*;
+
+            using size_type = _STD size_t;
+            using difference_type = _STD ptrdiff_t;
+
+        public:
+            constexpr ConstIterator() noexcept = default;
+
+            constexpr ConstIterator(const const_pointer cursor_) noexcept :
+                _cursor(cursor_) {}
+
+            constexpr ConstIterator(this_type&& other_) noexcept = default;
+
+            constexpr ConstIterator(const this_type& other_) noexcept = default;
+
+            constexpr ~ConstIterator() noexcept = default;
+
+        private:
+            const_pointer _cursor;
+
+        public:
+            constexpr this_type& operator=(const this_type& other_) noexcept {
+                if (_STD addressof(other_) != this) {
+                    _cursor = other_._cursor;
+                }
+                return *this;
+            }
+
+            constexpr this_type& operator=(this_type&& other_) noexcept {
+                if (_STD addressof(other_) != this) {
+                    _cursor = other_._cursor;
+                }
+                return *this;
+            }
+
+        public:
+            [[nodiscard]] constexpr const_reference operator*() const noexcept {
+                return *_cursor;
+            }
+
+            [[nodiscard]] constexpr const_reference operator*() noexcept {
+                return *_cursor;
+            }
+
+            [[nodiscard]] constexpr const_pointer operator->() const noexcept {
+                return _cursor;
+            }
+
+        public:
+            [[nodiscard]] constexpr const_reference operator[](const difference_type offset_) const noexcept {
+                return _cursor[offset_];
+            }
+
+            [[nodiscard]] constexpr const_reference operator[](const difference_type offset_) noexcept {
+                return _cursor[offset_];
+            }
+
+        public:
+            constexpr this_type& operator++() {
+                ++_cursor;
+                return *this;
+            }
+
+            [[nodiscard]] constexpr this_type operator++(int) {
+                constexpr auto tmp { *this };
+                ++_cursor;
+                return tmp;
+            }
+
+            constexpr this_type& operator+=(difference_type offset_) noexcept {
+                _cursor += offset_;
+                return *this;
+            }
+
+            [[nodiscard]] constexpr this_type operator+(difference_type offset_) const noexcept {
+                this_type tmp { *this };
+                tmp += offset_;
+                return tmp;
+            }
+
+            constexpr this_type& operator--() {
+                ++_cursor;
+                return *this;
+            }
+
+            [[nodiscard]] constexpr this_type operator--(int) {
+                constexpr auto tmp { *this };
+                --_cursor;
+                return tmp;
+            }
+
+            constexpr this_type& operator-=(difference_type offset_) noexcept {
+                _cursor -= offset_;
+                return *this;
+            }
+
+            [[nodiscard]] constexpr this_type operator-(difference_type offset_) const noexcept {
+                this_type tmp { *this };
+                tmp -= offset_;
+                return tmp;
+            }
+
+        public:
+            [[nodiscard]] constexpr difference_type operator-(const this_type& other_) const noexcept {
+                return _cursor - other_._cursor;
+            }
+
+        public:
+            [[nodiscard]] constexpr bool operator==(const this_type& other_) const noexcept {
+                return _cursor == other_._cursor;
+            }
+
+            [[nodiscard]] constexpr bool operator==(const Iterator& other_) const noexcept;
+
+            [[nodiscard]] constexpr bool operator!=(const this_type& other_) const noexcept {
+                return _cursor != other_._cursor;
+            }
+
+            [[nodiscard]] constexpr bool operator!=(const Iterator& other_) const noexcept;
+
+            [[nodiscard]] constexpr _STD strong_ordering operator<=>(const this_type& other_) const noexcept {
+                return _STD _Unfancy(_cursor) <=> _STD _Unfancy(other_._cursor);
+            }
+
+            [[nodiscard]] constexpr _STD strong_ordering operator<=>(const Iterator& other_) const noexcept;
+        };
+
+    public:
+        [[nodiscard]] constexpr ConstIterator begin() const noexcept {
+            return ConstIterator { _data };
+        }
+
+        [[nodiscard]] constexpr Iterator begin() noexcept {
+            return Iterator { _data };
+        }
+
+        [[nodiscard]] constexpr ConstIterator end() const noexcept {
+            return ConstIterator { _data + Size_ };
+        }
+
+        [[nodiscard]] constexpr Iterator end() noexcept {
+            return Iterator { _data + Size_ };
+        }
+    };
+
+    /**/
+
+    template <typename NodeType_, size_t Size_, typename Comparator_>
+    constexpr bool CompileTable<NodeType_, Size_, Comparator_>::Iterator::operator==(
+        const ConstIterator& other_
+    ) const noexcept {
+        return _cursor == &(*other_);
+    }
+
+    template <typename NodeType_, size_t Size_, typename Comparator_>
+    constexpr bool CompileTable<NodeType_, Size_, Comparator_>::Iterator::operator!=(
+        const ConstIterator& other_
+    ) const noexcept {
+        return _cursor != &(*other_);
+    }
+
+    template <typename NodeType_, size_t Size_, typename Comparator_>
+    constexpr _STD strong_ordering CompileTable<NodeType_, Size_, Comparator_>::Iterator::operator<=>(
+        const ConstIterator& other_
+    ) const noexcept {
+        return _cursor <=> &(*other_);
+    }
+
+    template <typename NodeType_, size_t Size_, typename Comparator_>
+    constexpr bool CompileTable<NodeType_, Size_, Comparator_>::ConstIterator::operator==(
+        const Iterator& other_
+    ) const noexcept {
+        return _cursor == &(*other_);
+    }
+
+    template <typename NodeType_, size_t Size_, typename Comparator_>
+    constexpr bool CompileTable<NodeType_, Size_, Comparator_>::ConstIterator::operator!=(
+        const Iterator& other_
+    ) const noexcept {
+        return _cursor != &(*other_);
+    }
+
+    template <typename NodeType_, size_t Size_, typename Comparator_>
+    constexpr _STD strong_ordering CompileTable<NodeType_, Size_, Comparator_>::ConstIterator::operator<=>(
+        const Iterator& other_
+    ) const noexcept {
+        return _cursor <=> &(*other_);
+    }
+}
