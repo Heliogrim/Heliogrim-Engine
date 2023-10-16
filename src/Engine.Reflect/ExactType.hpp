ab65a6a771310fab8660bfe2b643e153dba7b5c7
LtJulius <senator.spartas@googlemail.com>
2023-10-16 16:56:15 +0200

    Reflect Queries
    C++ Language:
    * added type check query to reflect interface
    * added exact type check query to reflect interface

---
+++ src/Engine.Reflect/ExactType.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <Engine.Common/Wrapper.hpp>
+#include "Inherit/Concept.hpp"
+#include "Meta/MetaClass.hpp"
+
+namespace hg {
+    template <typename Type_>
+    struct ExactTypeFn;
+
+    template <typename Type_> requires (ClassHasMeta<Type_>)
+    struct ExactTypeFn<Type_> {
+        template <typename Arg_> requires ClassHasMeta<_STD remove_cvref_t<Arg_>>
+        [[nodiscard]] constexpr bool operator()(Arg_&& arg_) noexcept {
+            return arg_.getMetaClass()->template exact<Type_>();
+        }
+    };
+
+    template <typename Type_> requires (not ClassHasMeta<Type_>)
+    struct ExactTypeFn<Type_> {
+        static_assert(not ClassHasMeta<Type_>);
+    };
+
+    template <ClassHasMeta Type_>
+    static constexpr ExactTypeFn<Type_> ExactType {};
+}
---
+++ src/Engine.Reflect/IsType.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <Engine.Common/Wrapper.hpp>
+#include "Inherit/Concept.hpp"
+#include "Meta/MetaClass.hpp"
+
+namespace hg {
+    template <typename Type_>
+    struct IsTypeFn;
+
+    template <typename Type_> requires (ClassHasMeta<Type_>)
+    struct IsTypeFn<Type_> {
+        template <typename Arg_> requires ClassHasMeta<_STD remove_cvref_t<Arg_>>
+        [[nodiscard]] constexpr bool operator()(Arg_&& arg_) const noexcept {
+            return arg_.getMetaClass()->template is<Type_>();
+        }
+    };
+
+    template <typename Type_> requires (not ClassHasMeta<Type_>)
+    struct IsTypeFn<Type_> {
+        static_assert(not ClassHasMeta<Type_>);
+    };
+
+    template <ClassHasMeta Type_>
+    static constexpr IsTypeFn<Type_> IsType {};
+}
