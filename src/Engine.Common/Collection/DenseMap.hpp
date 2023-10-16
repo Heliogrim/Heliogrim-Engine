e2ee5a99235fefafc650257d427538e19469834c
LtJulius <senator.spartas@googlemail.com>
2023-10-16 16:47:59 +0200

    Update Collection
    Common:
    * updated dense map alias using ankerl
    * updated dense set alias using ankerl
    * updated set aliases using ankerl

--- src/Engine.Common/Collection/DenseMap.hpp
+++ src/Engine.Common/Collection/DenseMap.hpp
@@ -8,7 +8,7 @@ namespace hg {
     template <
         class Key_,
         class Type_,
-        class Hash_ = ankerl::unordered_dense::v3_1_1::hash<Key_>,
+        class Hash_ = ankerl::unordered_dense::v4_1_2::hash<Key_>,
         class Equal_ = _STD equal_to<Key_>,
         class Alloc_ = _STD allocator<_STD pair<Key_, Type_>>>
     using DenseMap = ankerl::unordered_dense::map<Key_, Type_, Hash_, Equal_, Alloc_>;
