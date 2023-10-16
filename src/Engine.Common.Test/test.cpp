9f56bc7d352d65a2cd518fb14edc418c606bb003
LtJulius <senator.spartas@googlemail.com>
2023-10-16 16:50:41 +0200

    Inline Auto Array
    Common:
    * removed dummy alias for inline auto-array
    * added custom implementation for inline auto-array
    * start experimental inline-vector implementation
    * added test cases to cover inline auto-array implementation

--- src/Engine.Common.Test/test.cpp
+++ src/Engine.Common.Test/test.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 
 #include <Engine.Common/String.hpp>
+#include <Engine.Common/Collection/InlineAutoArray.hpp>
 #include <Engine.Common/Collection/ProxyVector.hpp>
 #include <Engine.Common/Math/Transformation.hpp>
 #include <Engine.Common/Math/Vector.hpp>
@@ -624,6 +625,116 @@ namespace Common {
                 EXPECT_EQ(vector.at<type>(3), 4);
                 EXPECT_EQ(vector.at<type>(4), 9);
             }
+        }
+    }
+
+    namespace Collection {
+        TEST(InlineAutoArray, ZeroSize) {
+            //InlineAutoArray<size_t, 0> tmp {};
+            // Result: Will not compile due to min capacity constraint
+        }
+
+        TEST(InlineAutoArray, SingleSize) {
+            InlineAutoArray<size_t, 1> tmp {};
+
+            EXPECT_EQ(tmp.size(), 0);
+            tmp.emplace_back(0);
+            EXPECT_EQ(tmp.size(), 1);
+        }
+
+        TEST(InlineAutoArray, InlineSize) {
+            InlineAutoArray<size_t, 4> tmp {};
+
+            EXPECT_EQ(tmp.size(), 0);
+            tmp.emplace_back(0);
+            tmp.emplace_back(1);
+            tmp.emplace_back(2);
+            EXPECT_EQ(tmp.size(), 3);
+        }
+
+        TEST(InlineAutoArray, OverflowSize) {
+            InlineAutoArray<size_t, 4> tmp {};
+
+            EXPECT_EQ(tmp.size(), 0);
+
+            tmp.emplace_back(0);
+            tmp.emplace_back(1);
+            tmp.emplace_back(2);
+            tmp.emplace_back(3);
+            tmp.emplace_back(4);
+            tmp.emplace_back(5);
+
+            EXPECT_EQ(tmp.size(), 6);
+        }
+
+        TEST(InlineAutoArray, PopInlineBack) {
+
+            InlineAutoArray<size_t, 4> tmp {};
+
+            tmp.emplace_back(0);
+            tmp.emplace_back(1);
+
+            EXPECT_NE(*tmp.begin(), *(--tmp.end()));
+
+            tmp.pop_back();
+
+            EXPECT_EQ(tmp.size(), 1);
+            EXPECT_EQ(*tmp.begin(), *(--tmp.end()));
+        }
+
+        TEST(InlineAutoArray, PopExternalBack) {
+
+            InlineAutoArray<size_t, 1> tmp {};
+
+            tmp.emplace_back(0);
+            tmp.emplace_back(1);
+            tmp.emplace_back(2);
+
+            EXPECT_NE(*tmp.begin(), *(--tmp.end()));
+
+            tmp.pop_back();
+
+            EXPECT_EQ(tmp.size(), 2);
+            EXPECT_NE(*tmp.begin(), *(--tmp.end()));
+            EXPECT_EQ(*(++tmp.begin()), *(--tmp.end()));
+        }
+
+        TEST(InlineAutoArray, PopMixedBack) {
+
+            InlineAutoArray<size_t, 2> tmp {};
+
+            tmp.emplace_back(0);
+            tmp.emplace_back(1);
+            tmp.emplace_back(20);
+            tmp.emplace_back(21);
+
+            EXPECT_NE(*tmp.begin(), *(--tmp.end()));
+
+            tmp.pop_back();
+            tmp.pop_back();
+
+            tmp.pop_back();
+            tmp.pop_back();
+
+            EXPECT_EQ(tmp.size(), 0);
+        }
+
+        TEST(InlineAutoArray, Iterate) {
+
+            InlineAutoArray<size_t, 8> tmp {};
+
+            for (size_t i = 0; i < 0xF; ++i) {
+                tmp.emplace_back(i);
+            }
+
+            EXPECT_EQ(tmp.size(), 0xF);
+
+            size_t c { 0 };
+            for (const auto& entry : tmp) {
+                EXPECT_EQ(entry, c++);
+            }
+
+            EXPECT_EQ(c, 0xF);
         }
     }
 }
---
+++ src/Engine.Common/Collection/InlineVector.hpp
@@ -0,0 +1,231 @@
+#pragma once
+
+#include <cassert>
+#include <memory>
+
+#include "InlineArray.hpp"
+#include "../Wrapper.hpp"
+
+namespace hg {
+    template <class Type_, size_t Capacity_ = 1, typename Allocator_ = _STD allocator<Type_>>
+    class InlineVector {
+    public:
+        using this_type = InlineVector<Type_, Capacity_, Allocator_>;
+
+        static constexpr auto element_align = alignof(Type_);
+        static constexpr auto element_size = sizeof(Type_);
+
+        static constexpr auto inline_byte_size = element_size * Capacity_;
+
+        using size_type = size_t;
+        using difference_type = ptrdiff_t;
+
+        using allocator_type = Allocator_;
+        using allocator_traits = _STD allocator_traits<allocator_type>;
+
+    public:
+        constexpr InlineVector() noexcept;
+
+        constexpr ~InlineVector() noexcept {
+            tidy();
+        }
+
+    private:
+        allocator_type _alloc;
+        allocator_traits _traits;
+
+        _::byte _inlineStorage[inline_byte_size];
+        ptr<Type_> _inlineEnd;
+
+        _::byte _externalStorage;
+        ptr<Type_> _externalLast;
+        ptr<Type_> _externalEnd;
+
+    protected:
+        constexpr void tidy() {
+
+            /* Destroy inline allocated objects */
+
+            _STD _Destroy_range(inline_begin(), _inlineEnd, _alloc);
+
+            /* Destroy external allocated objects */
+
+            _STD _Destroy_range(external_begin(), _externalLast, _alloc);
+            _traits.deallocate(_alloc, _externalStorage, _STD distance(external_begin(), _externalEnd));
+
+            _externalStorage = _externalEnd = nullptr;
+        }
+
+        [[nodiscard]] constexpr ptr<Type_> inline_begin() const noexcept {
+            return _inlineStorage;
+        }
+
+        [[nodiscard]] constexpr ptr<Type_> inline_last() const noexcept {
+            return _inlineEnd != _inlineStorage ? _inlineEnd - 1 : _inlineEnd;
+        }
+
+        [[nodiscard]] constexpr size_type inline_size() const noexcept {
+            return _STD distance(inline_begin(), _inlineEnd);
+        }
+
+        [[nodiscard]] constexpr ptr<Type_> external_begin() const noexcept {
+            return _externalStorage;
+        }
+
+        [[nodiscard]] constexpr size_type external_capacity() const noexcept {
+            return _STD distance(external_begin(), _externalEnd);
+        }
+
+        [[nodiscard]] constexpr size_t external_size() const noexcept {
+            return _STD distance(external_begin(), _externalLast);
+        }
+
+    protected:
+        constexpr bool inline_emplace_back(auto&&... args_) {
+
+            const auto diff = inline_size();
+            if (diff >= Capacity_) {
+                return false;
+            }
+
+            _traits.construct(_alloc, _inlineEnd, _STD forward<decltype(args_)>(args_)...);
+            return ++_inlineEnd;
+        }
+
+        constexpr void inline_pop_back() {
+            assert(_inlineEnd != _inlineStorage);
+            _traits.destroy(_alloc, --_inlineEnd);
+        }
+
+        constexpr void external_emplace_back(auto&&... args_) {
+            _traits.construct(_alloc, _externalLast, _STD forward<decltype(args_)>(args_)...);
+            ++_externalLast;
+        }
+
+        constexpr void _Xchange_external(const ptr<Type_> newPt_, const size_type newSize_, const size_type newCap_) {
+
+            if (external_begin()) {
+                _STD _Destroy_range(external_begin(), _externalLast, _alloc);
+                _alloc.deallocate(_externalStorage);
+            }
+
+            _externalStorage = newPt_;
+            _externalLast = newPt_ + newSize_;
+            _externalEnd = newPt_ + newCap_;
+        }
+
+        constexpr void external_realloc_emplace(auto&&... args_) {
+
+            const auto oldSize = external_size();
+
+            auto [newPt_, newCap_] = _traits.allocate_at_least(_alloc, external_capacity() + 1);
+
+            if (_externalLast == _externalEnd) {
+                if constexpr (_STD is_nothrow_move_constructible_v<Type_> || not _STD is_copy_constructible_v<Type_>) {
+                    _STD _Uninitialized_move(external_begin(), _externalLast, newPt_, _alloc);
+                } else {
+                    _STD _Uninitialized_copy(external_begin(), _externalLast, newPt_, _alloc);
+                }
+            }
+
+            _traits.construct(_alloc, newPt_ + oldSize, _STD forward<decltype(args_)>(args_)...);
+            _Xchange_external(newPt_, oldSize + 1, newCap_);
+        }
+
+        [[nodiscard]] constexpr bool external_pop_back() {
+
+            if (_externalLast != _externalStorage) {
+                _traits.destroy(_alloc, --_externalLast);
+                return true;
+            }
+
+            return false;
+        }
+
+    public:
+        [[nodiscard]] size_type size() const noexcept {
+            return inline_size() + external_size();
+        }
+
+        [[nodiscard]] size_type capacity() const noexcept {
+            return (Capacity_ - inline_size()) + (external_capacity() - external_size());
+        }
+
+        [[nodiscard]] constexpr auto begin() const noexcept {
+            // Warning:
+            // TODO:
+            return inline_begin();
+        }
+
+        [[nodiscard]] constexpr auto begin() noexcept {
+            // Warning:
+            // TODO:
+            return inline_begin();
+        }
+
+        [[nodiscard]] constexpr auto cbegin() noexcept {
+            // Warning:
+            // TODO:
+            return inline_begin();
+        }
+
+        [[nodiscard]] constexpr auto end() const noexcept {
+            // Warning:
+            // TODO:
+            return _inlineEnd;
+        }
+
+        [[nodiscard]] constexpr auto end() noexcept {
+            // Warning:
+            // TODO:
+            return _inlineEnd;
+        }
+
+        [[nodiscard]] constexpr auto cend() noexcept {
+            // Warning:
+            // TODO:
+            return _inlineEnd;
+        }
+
+    public:
+        void emplace_back(auto&&... val_) {
+
+            if (inline_emplace_back(val_)) {
+                return;
+            }
+
+            if (external_capacity() > external_size()) {
+                external_emplace_back(_STD forward<decltype(val_)>(val_)...);
+                return;
+            }
+
+            external_realloc_emplace(_STD forward<decltype(val_)>(val_)...);
+        }
+
+        void pop_back() {
+
+            if (external_pop_back()) {
+                return;
+            }
+
+            inline_pop_back();
+        }
+
+        template <typename Iter_>
+        constexpr Iter_ erase(const Iter_& where_) noexcept;
+
+        constexpr void clear() {
+            tidy();
+        }
+
+        constexpr void reserve(size_type reserved_) {
+            if (reserved_ > size()) {
+                // TODO:
+            }
+        }
+
+        constexpr void resize(size_t newSize_);
+
+        constexpr void resize(size_t newSize_, auto&&... args_);
+    };
+}
