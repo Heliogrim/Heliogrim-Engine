45e508541e511173f4ef33f2d541e66a43edf3af
LtJulius <senator.spartas@googlemail.com>
2023-10-22 22:29:01 +0200

    Vulkan Acceleration Pass
    Acceleration:
    * added missing attribute to pass vulkan graphics and compute pipeline along this specialized acceleration passes

--- src/Engine.GFX.Acc/Pass/VkComputePass.hpp
+++ src/Engine.GFX.Acc/Pass/VkComputePass.hpp
@@ -1,13 +1,14 @@
 #pragma once
 
 #include <Engine.Common/Meta/Constexpr.hpp>
+#include <Engine.GFX/API/__vkFwd.hpp>
 #include <Engine.Reflect/CompileTypeId.hpp>
 
 #include "ComputePass.hpp"
 
 namespace hg::engine::gfx::acc {
     class VkComputePass final :
-        public ComputePass {
+        public InheritMeta<VkComputePass, ComputePass> {
     public:
         using this_type = VkComputePass;
 
@@ -16,5 +17,22 @@ namespace hg::engine::gfx::acc {
 
     public:
         ~VkComputePass() override = default;
+
+    private:
+        void tidy();
+
+    private:
+    public:
+        _::VkComputePipelineLayout _vkPipeLayout;
+        _::VkComputePipeline _vkPipe;
+
+    public:
+        void setVkPipeLayout(mref<_::VkComputePipelineLayout> vkPipeLayout_);
+
+        void setVkPipe(mref<_::VkComputePipeline> vkPipe_);
+
+        // Warning: Temporary
+    public:
+        Vector<_::VkDescriptorSetLayout> _vkDescLayouts;
     };
 }
