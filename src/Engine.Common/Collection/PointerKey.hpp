de67c658309056d375d6532af7b78a4bb36d97fe
LtJulius <senator.spartas@googlemail.com>
2023-10-30 14:53:13 +0100

    Pointer to Key
    Common:
    * added helper to use pointers and managed pointers a associative keys within data structures

---
+++ src/Engine.Common/Collection/PointerKey.hpp
@@ -0,0 +1,81 @@
+#pragma once
+#include <type_traits>
+
+#include "../Wrapper.hpp"
+
+namespace hg {
+    template <typename Ty_, typename StorageType_>
+    struct NonOwningMemoryPointer;
+
+    template <typename Type_>
+    class SharedMemoryReference;
+}
+
+namespace hg {
+    template <typename PointerType_>
+    struct PointerKey {
+        using this_type = PointerKey<PointerType_>;
+
+        using raw_type = _STD remove_all_extents_t<PointerType_>;
+        using pointer_type = ptr<raw_type>;
+
+    public:
+        constexpr PointerKey() noexcept :
+            value(nullptr) {}
+
+        template <typename Type_> requires
+            _STD is_same_v<Type_, PointerType_> &&
+            _STD is_nothrow_convertible_v<Type_, pointer_type>
+        constexpr PointerKey(Type_&& value_) noexcept :
+            value(static_cast<pointer_type>(value_)) {}
+
+        template <typename Type_, typename StorageType_>
+        constexpr PointerKey(NonOwningMemoryPointer<Type_, StorageType_>&& value_) noexcept :
+            value(static_cast<pointer_type>(value_.get())) {}
+
+        template <typename Type_>
+        constexpr PointerKey(SharedMemoryReference<Type_>&& value_) noexcept :
+            value(static_cast<pointer_type>(value_.get())) {}
+
+        constexpr PointerKey(cref<this_type> other_) noexcept :
+            value(other_.value) {}
+
+        constexpr PointerKey(mref<this_type> other_) noexcept :
+            value(other_.value) {}
+
+        constexpr ~PointerKey() noexcept = default;
+
+        ref<this_type> operator=(cref<this_type> other_) noexcept {
+            value = other_.value;
+            return *this;
+        }
+
+        ref<this_type> operator=(mref<this_type> other_) noexcept {
+            value = other_.value;
+            return *this;
+        }
+
+        [[nodiscard]] constexpr bool operator==(cref<this_type> other_) const noexcept {
+            return value == other_.value;
+        }
+
+        [[nodiscard]] constexpr bool operator!=(cref<this_type> other_) const noexcept {
+            return value == other_.value;
+        }
+
+        [[nodiscard]] constexpr _STD strong_ordering operator<=>(cref<this_type> other_) const noexcept {
+            return value <=> other_.value;
+        }
+
+        _STD nullptr_t value;
+    };
+}
+
+namespace std {
+    template <typename Type_>
+    struct hash<::hg::PointerKey<Type_>> {
+        [[nodiscard]] constexpr size_t operator()(auto&& value_) const noexcept {
+            return reinterpret_cast<ptrdiff_t>(value_.value);
+        }
+    };
+}
