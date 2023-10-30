9120cd6cddb0b4b7afc0a45beb21ab6a2ee1cfac
LtJulius <senator.spartas@googlemail.com>
2023-10-30 15:34:07 +0100

    Render Graph Compile Migration
    Graphics:
    * updated compiler visitor to corresponding intention of the new compile node and component system

--- src/Engine.GFX.RenderGraph.Compile/CompileVisitor.cpp
+++ src/Engine.GFX.RenderGraph.Compile/CompileVisitor.cpp
@@ -3,36 +3,27 @@
 #include <Engine.Common/Memory/MemoryPointer.hpp>
 #include <Engine.GFX.RenderGraph/RuntimeGraph.hpp>
 #include <Engine.GFX.RenderGraph/Builder/Builder.hpp>
-#include <Engine.GFX.RenderGraph/Component/Compile/Subpass/CompileSubpassAccelComponent.hpp>
-#include <Engine.GFX.RenderGraph/Component/Compile/Subpass/CompileSubpassMaterialAccelComponent.hpp>
-#include <Engine.GFX.RenderGraph/Component/Compile/Subpass/CompileSubpassMultiAccelComponent.hpp>
-#include <Engine.GFX.RenderGraph/Component/Compile/Subpass/CompileSubpassSingleAccelComponent.hpp>
-#include <Engine.GFX.RenderGraph/Component/Subpass/SubpassSceneMeshInvocationComponent.hpp>
 #include <Engine.Reflect/TypeSwitch.hpp>
 
 /**/
 #include <Engine.GFX.Acc.Compile/VkEffectCompiler.hpp>
 #include <Engine.GFX.Acc.Compile/Spec/SimpleEffectSpecification.hpp>
-#include <Engine.GFX.RenderGraph/Component/Subpass/SubpassMaterialAccelComponent.hpp>
+#include <Engine.GFX.RenderGraph/Node/CompileNode.hpp>
 #include <Engine.GFX.RenderGraph/Node/Node.hpp>
-#include <Engine.GFX.RenderGraph/Node/AnchorNode.hpp>
-#include <Engine.GFX.RenderGraph/Node/BarrierNode.hpp>
-#include <Engine.GFX.RenderGraph/Node/ConvergeNode.hpp>
-#include <Engine.GFX.RenderGraph/Node/DivergeNode.hpp>
-#include <Engine.GFX.RenderGraph/Node/ProviderNode.hpp>
-#include <Engine.GFX.RenderGraph/Node/SelectorNode.hpp>
-#include <Engine.GFX.RenderGraph/Node/Compile/CompileNode.hpp>
 #include <Engine.GFX.RenderGraph/Node/Compile/CompileSubpassNode.hpp>
+#include <Engine.GFX.RenderGraph/Node/Runtime/AnchorNode.hpp>
+#include <Engine.GFX.RenderGraph/Node/Runtime/BarrierNode.hpp>
+#include <Engine.GFX.RenderGraph/Node/Runtime/ConvergeNode.hpp>
+#include <Engine.GFX.RenderGraph/Node/Runtime/DivergeNode.hpp>
+#include <Engine.GFX.RenderGraph/Node/Runtime/ProviderNode.hpp>
+#include <Engine.GFX.RenderGraph/Node/Runtime/SelectorNode.hpp>
+#include <Engine.GFX.RenderGraph/Pass/CompilePassContext.hpp>
 #include <Engine.Pedantic/Clone/Clone.hpp>
 #include <Engine.Reflect/Cast.hpp>
 /**/
 
 using namespace hg::engine::gfx::render::graph;
 using namespace hg;
-
-/* Temporary Helper */
-template <typename CompileNodeType_, typename RuntimeNodeType_>
-void process(cref<CompileNodeType_> compile_, _Inout_ ref<RuntimeNodeType_> runtime_);
 
 /**/
 
@@ -127,94 +118,18 @@ void CompileVisitor::operator()(cref<SubpassNode> node_) {
 }
 
 void CompileVisitor::operator()(cref<CompileNode> node_) {
-    this->operator()(static_cast<cref<Node>>(node_));
-}
 
-void CompileVisitor::operator()(cref<CompileSubpassNode> node_) {
+    const CompilePassContext context {};
+    auto compiled = node_.compile(context);
 
-    const auto csc = node_.getSubpassComponent();
-    const auto cac = node_.getSubpassAcceleration();
-
-    auto sequence = switchType<
-        CompileSubpassAccelComponent
-    >(
-        cac.get(),
-        [](CompileSubpassSingleAccelComponent*) {
-            // TODO:
-            auto tmp = make_smr<SubpassNode>(graph::SubpassAccelMode::eSingle).into<Node>();
-            return _STD make_pair(tmp, tmp);
-        },
-        [](CompileSubpassMultiAccelComponent*) {
-            // TODO:
-            auto tmp = make_smr<SubpassNode>(graph::SubpassAccelMode::eMulti).into<Node>();
-            return _STD make_pair(tmp, tmp);
-        },
-        [](CompileSubpassMaterialAccelComponent*) {
-            // TODO:
-            auto tmp = make_smr<SubpassNode>(graph::SubpassAccelMode::eMaterial).into<Node>();
-            return _STD make_pair(tmp, tmp);
-        }
-    );
-
-    Builder::insertSubGraph(
-        _STD as_const(*_target).begin(),
-        _target->begin()->getNext().get(),
-        _STD move(sequence.first),
-        _STD move(sequence.second),
-        _target
-    );
-
-    Visitor::operator()(node_);
-}
-
-/**/
-
-template <>
-void process<CompileSubpassNode, SubpassNode>(cref<CompileSubpassNode> compile_, _Inout_ ref<SubpassNode> runtime_) {
-
-    auto* compileAccelComponent = Cast<CompileSubpassMaterialAccelComponent>(compile_.getSubpassAcceleration().get());
-    auto* runtimeAccelComponent = Cast<SubpassMaterialAccelComponent>(runtime_.getSubpassAcceleration().get());
-    if (compileAccelComponent == nullptr || runtimeAccelComponent == nullptr) {
-        return;
-    }
-
-    /**/
-
-    auto aec = engine::gfx::acc::makeVkAccCompiler();
-
-    auto profile = make_smr<engine::gfx::acc::EffectProfile>(
-        engine::gfx::acc::EffectProfile {
-            ._name = "Default-Test-Profile",
-            ._definitions = {}
-        }
-    );
-
-    engine::gfx::acc::SimpleEffectSpecification spec {};
-    Vector<smr<const engine::gfx::acc::Symbol>> targetSymbols {};
-
-    /**/
-
-    for (const auto& resource : compileAccelComponent->getMaterials()) {
-
-        const auto guard = resource->acquire(engine::resource::ResourceUsageFlag::eRead);
-        const auto prototype = guard->getPrototype();
-        const auto& effects = prototype->getAccelerationEffects();
-
-        assert(effects.size() == 1);
-        const auto& effect = effects.front();
-
-        /**/
-
-        auto pass = aec->compile(
-            engine::gfx::acc::EffectCompileRequest {
-                .effect = effect,
-                .profile = profile,
-                .spec = spec
-            }
+    if (compiled.operator bool()) {
+        Builder::insertNode(
+            _STD as_const(*_target).begin(),
+            _target->begin()->getNext().get(),
+            make_smr(_STD move(compiled)),
+            _target
         );
-
-        /**/
-
-        runtimeAccelComponent->addMaterial(clone(resource));
     }
+
+    this->operator()(static_cast<cref<Node>>(node_));
 }
