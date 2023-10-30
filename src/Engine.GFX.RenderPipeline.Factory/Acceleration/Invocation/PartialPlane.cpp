cdffff7fd5f7909d52bc225252fea27bad1092df
LtJulius <senator.spartas@googlemail.com>
2023-10-30 15:09:35 +0100

    Removed Render Pipeline Factory
    Graphics:
    * removed factory project for render pipelines
    * removed source code for render pipeline factories

--- src/Engine.GFX.RenderPipeline.Factory/Acceleration/Invocation/PartialPlane.cpp
+++
@@ -1,8 +0,0 @@
-#include "PartialPlane.hpp"
-
-using namespace hg::engine::gfx::render::pipeline;
-using namespace hg;
-
-smr<PlaneMeshIG> PartialPlane::operator()(mref<smr<graph::SubpassNode>> node_) const noexcept {
-    return make_smr<PlaneMeshIG>();
-}
--- src/Engine.GFX.RenderPipeline.Factory/Acceleration/Invocation/PartialProcedural.cpp
+++
@@ -1,8 +0,0 @@
-#include "PartialProcedural.hpp"
-
-using namespace hg::engine::gfx::render::pipeline;
-using namespace hg;
-
-smr<ProcMeshIG> PartialProcedural::operator()(mref<smr<graph::SubpassNode>> node_) const noexcept {
-    return make_smr<ProcMeshIG>();
-}
