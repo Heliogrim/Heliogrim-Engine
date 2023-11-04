e03424b135a8d5fe0b39d96e403a0388a53fb6f3
LtJulius <senator.spartas@googlemail.com>
2023-11-04 17:54:50 +0100

    Cleanup Editor Renderer
    Editor Graphics:
    * dropped previous specialized renderer setup of editor system

--- src/Editor.GFX/Renderer/EdRevRenderer.hpp
+++ src/Editor.GFX/Renderer/EdRevRenderer.hpp
@@ -1,26 +1,16 @@
 #pragma once
 
-#include <Engine.GFX.Glow.3D/Renderer/RevRenderer.hpp>
+#include <Engine.GFX.Render/Renderer.hpp>
 
 namespace hg::editor::gfx {
     class EdRevRenderer :
-        public ::hg::engine::gfx::glow::render::RevRenderer {
+        public ::hg::engine::gfx::render::Renderer {
     public:
         using this_type = EdRevRenderer;
-        using underlying_type = ::hg::engine::gfx::glow::render::RevRenderer;
-        using base_type = Renderer_Deprecated;
 
     public:
-        EdRevRenderer();
+        EdRevRenderer() noexcept;
 
-        ~EdRevRenderer() override = default;
-
-    public:
-        void setup(cref<sptr<engine::gfx::Device>> device_) override;
-
-        void destroy() override;
-
-    protected:
-        void registerStages() override;
+        ~EdRevRenderer() noexcept override = default;
     };
 }
