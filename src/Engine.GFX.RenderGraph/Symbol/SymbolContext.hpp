25862c2f636823b4bdbe8f29309be4fb647529d5
LtJulius <senator.spartas@googlemail.com>
2023-11-18 01:42:51 +0100

    Symbol Context Impl
    Graphics:
    * added implementation of symbol context to store and query symbolized resources
    * added experimental data carrier to symbolized resources to carry barrier dependencies

--- src/Engine.GFX.RenderGraph/Symbol/SymbolContext.hpp
+++ src/Engine.GFX.RenderGraph/Symbol/SymbolContext.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <map>
 #include <Engine.Common/Memory/MemoryPointer.hpp>
 #include <Engine.GFX.Acc/Symbol/Symbol.hpp>
 
@@ -10,9 +11,29 @@ namespace hg::engine::gfx::render::graph {
         using this_type = SymbolContext;
 
     public:
-        SymbolContext() noexcept = default;
+        SymbolContext() noexcept;
 
-        ~SymbolContext() noexcept = default;
+        SymbolContext(mref<this_type>) = delete;
+
+        SymbolContext(cref<this_type>) = delete;
+
+        ~SymbolContext() noexcept;
+
+    private:
+        void tidy();
+
+    private:
+        nmpt<this_type> _parent;
+
+    public:
+        [[nodiscard]] bool isRoot() const noexcept;
+
+        [[nodiscard]] nmpt<const this_type> root() const noexcept;
+
+        [[nodiscard]] nmpt<this_type> parent() const noexcept;
+
+    private:
+        std::map<smr<const acc::Symbol>, nmpt<SymbolizedResource>> _resources;
 
     public:
         void exposeSymbol(mref<smr<const acc::Symbol>> symbol_, nmpt<SymbolizedResource> resource_);
@@ -20,5 +41,30 @@ namespace hg::engine::gfx::render::graph {
         [[nodiscard]] nmpt<SymbolizedResource> exportSymbol(mref<smr<const acc::Symbol>> symbol_);
 
         [[nodiscard]] nmpt<SymbolizedResource> importSymbol(mref<smr<const acc::Symbol>> symbol_);
+
+    public:
+        [[nodiscard]] _Success_(return != nullptr) nmpt<SymbolizedResource> getExportSymbol(
+            mref<smr<const acc::Symbol>> symbol_
+        ) const;
+
+        [[nodiscard]] _Success_(return != nullptr) nmpt<SymbolizedResource> getImportSymbol(
+            mref<smr<const acc::Symbol>> symbol_
+        ) const;
+
+    public:
+        bool eraseExposedSymbol(mref<smr<const acc::Symbol>> symbol_);
+
+        bool eraseExposedSymbol(mref<nmpt<SymbolizedResource>> resource_);
+
+        [[nodiscard]] bool eraseExportSymbol(mref<smr<const acc::Symbol>> symbol_);
+
+        [[nodiscard]] bool eraseExportSymbol(mref<nmpt<SymbolizedResource>> resource_);
+
+        [[nodiscard]] bool eraseImportSymbol(mref<smr<const acc::Symbol>> symbol_);
+
+        [[nodiscard]] bool eraseImportSymbol(mref<nmpt<SymbolizedResource>> resource_);
+
+    public:
+        [[nodiscard]] bool isShadowing(mref<smr<const acc::Symbol>> symbol_);
     };
 }
--- src/Engine.GFX.RenderGraph/Symbol/SymbolizedResource.hpp
+++ src/Engine.GFX.RenderGraph/Symbol/SymbolizedResource.hpp
@@ -1,10 +1,167 @@
 #pragma once
+#include <concepts>
+#include <type_traits>
 #include <Engine.Common/Concurrent/SharedMemoryReference.hpp>
+#include <Engine.Common/Memory/MemoryPointer.hpp>
 #include <Engine.GFX.Acc/Symbol/Symbol.hpp>
+#include <Engine.Reflect/IsType.hpp>
+
+namespace hg::engine::gfx::render::graph {
+    template <typename Validator_, typename Type_>
+    concept HasValueValidation = requires(const Validator_& valid_, const nmpt<Type_> data_) {
+        { valid_.isValidObject(data_) } -> _STD same_as<bool>;
+    };
+}
 
 namespace hg::engine::gfx::render::graph {
     struct SymbolizedResource {
-        smr<const acc::Symbol> symbol;
-        void* data;
+        bool owned : 1/* Whether the resource is NOT owned by the context */ = false;
+        bool transient : 1/* Currently unused */ = false;
+
+        smr<const acc::Symbol> symbol = nullptr;
+        void* data = nullptr;
+
+        // Warning: Temporary -> May be offloaded into a transition chaining layer
+        Vector<void*> barriers;
+
+        // TODO: Maybe we want to store the subject computed type identifier to extend dynamic type checking
+
+    public:
+        [[nodiscard]] bool empty() const noexcept {
+            return data == nullptr;
+        }
+
+        [[nodiscard]] ptr<void> loadDirect() const noexcept {
+            return data;
+        }
+
+        void storeDirect(const ptr<void> data_) noexcept {
+            data = data_;
+        }
+
+        template <typename Type_, typename Validation_ = _STD void_t<Type_>, typename Proj_ = _STD identity> requires
+            (not _STD is_void_v<Type_>)
+        [[nodiscard]] bool valid() const noexcept {
+
+            if (empty()) {
+                return _STD is_same_v<Type_, nullptr_t>;
+            }
+
+            if constexpr (_STD is_void_v<Validation_>) {
+                return not _STD is_same_v<Type_, nullptr_t>;
+            }
+
+            if constexpr (/* Type strengthening */not _STD is_void_v<Validation_>) {
+
+                if (not IsType<Validation_>(*symbol->description)) {
+                    return false;
+                }
+
+                if constexpr (HasValueValidation<Validation_, Type_>) {
+
+                    constexpr static Proj_ proj {};
+                    return static_cast<Validation_&>(*symbol->description).isValidObject(
+                        { proj(*static_cast<Type_*>(data)) }
+                    );
+                }
+
+                return true;
+            }
+
+            _STD unreachable();
+        }
+
+        template <typename Type_, typename Validation_ = _STD void_t<Type_>, typename Proj_ = _STD identity> requires
+            _STD is_move_constructible_v<Type_> && (not _STD is_void_v<Type_>) && (not _STD is_reference_v<Type_>)
+        ref<Type_> create(mref<Type_> data_) noexcept(_STD is_nothrow_move_constructible_v<Type_>) {
+
+            assert(empty() && "Tried to create at already initialized symbolized resource.");
+
+            if constexpr (not _STD is_void_v<Validation_>) {
+
+                if (not IsType<Validation_>(*symbol->description)) {
+                    return false;
+                }
+
+                if constexpr (HasValueValidation<Validation_, Type_>) {
+
+                    constexpr static Proj_ proj {};
+                    if (static_cast<Validation_&>(*symbol->description).isValidObject({ proj(data_) })) {
+                        return false;
+                    }
+                }
+
+            }
+
+            data = new Type_(_STD move(data_));
+            return load<Type_>();
+        }
+
+        template <typename Type_> requires _STD is_void_v<Type_> && (not _STD is_reference_v<Type_>)
+        nmpt<Type_> create(mref<Type_> data_) noexcept(_STD is_nothrow_move_constructible_v<Type_>) {
+
+            assert(empty() && "Tried to create at already initialized symbolized resource.");
+
+            data = new Type_(_STD move(data_));
+            return nmpt<Type_> { static_cast<Type_*>(data) };
+        }
+
+        template <typename Type_, typename Validation_ = _STD void_t<Type_>> requires (not _STD is_void_v<Type_>)
+        [[nodiscard]] ref<Type_> load() const noexcept {
+
+            assert(not empty() && "Tried to load from uninitialized symbolized resource.");
+
+            if constexpr (not _STD is_void_v<Validation_>) {
+                if (not IsType<Validation_>(*symbol->description)) {
+                    return *static_cast<Type_*>(nullptr);
+                }
+            }
+
+            return *static_cast<Type_*>(data);
+        }
+
+        template <typename Type_, typename Validation_ = _STD void_t<Type_>, typename Proj_ = _STD identity> requires
+            (not _STD is_void_v<Type_>)
+        bool store(mref<Type_> data_) noexcept {
+
+            assert(not empty() && "Tried to store to uninitialized symbolized resource.");
+
+            if constexpr (not _STD is_void_v<Validation_>) {
+
+                if (not IsType<Validation_>(*symbol->description)) {
+                    return false;
+                }
+
+                if constexpr (HasValueValidation<Validation_, Type_>) {
+
+                    constexpr static Proj_ proj {};
+                    if (static_cast<Validation_&>(*symbol->description).isValidObject({ proj(data_) })) {
+                        return false;
+                    }
+                }
+            }
+
+            *static_cast<Type_*>(data) = _STD move(data_);
+            return true;
+        }
+
+        template <typename Type_, typename Validation_ = _STD void_t<Type_>> requires (not _STD is_void_v<Type_>)
+        bool destroy() noexcept(_STD is_nothrow_destructible_v<Type_>) {
+
+            if constexpr (not _STD is_void_v<Validation_>) {
+                if (not IsType<Validation_>(*symbol->description)) {
+                    return false;
+                }
+            }
+
+            // TODO: Maybe we want to use reflect cast to strengthen the dynamic type checking
+
+            if (not empty()) {
+                delete static_cast<Type_*>(data);
+                data = nullptr;
+            }
+
+            return empty();
+        }
     };
 }
