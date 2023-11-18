84a85eb4bcdad275a6a327e37f1ec4e08ebe6f3b
LtJulius <senator.spartas@googlemail.com>
2023-11-18 02:26:14 +0100

    Accel Comand Buffers
    Accel:
    * added helper functions to recode interleaved command buffer state
    * implemented functions to generate binding commands for compute and graphics pipelines
    * implemented functions to generate binding commands for render passes
    * renamed pass bindings

--- src/Engine.GFX.Acc/Command/CommandBuffer.hpp
+++ src/Engine.GFX.Acc/Command/CommandBuffer.hpp
@@ -1,11 +1,67 @@
 #pragma once
+#include <Engine.Common/Memory/MemoryPointer.hpp>
 #include <Engine.GFX/Command/CommandBuffer.hpp>
 
+#include "CommandBufferStructs.hpp"
+
 namespace hg::engine::gfx::acc {
-    //class CommandBuffer { };
-    using CommandBuffer = ::hg::engine::gfx::CommandBuffer;
+    class ComputePass;
+    class GraphicsPass;
+    class ComputePipeline;
+    class GraphicsPipeline;
 }
 
 namespace hg::engine::gfx {
-    using AccelerationCommandBuffer = ::hg::engine::gfx::acc::CommandBuffer;
+    class AccelCommandBuffer :
+        public ::hg::engine::gfx::CommandBuffer {
+    public:
+        using this_type = AccelCommandBuffer;
+        using ::hg::engine::gfx::CommandBuffer::CommandBuffer;
+
+    public:
+        AccelCommandBuffer(mref<::hg::engine::gfx::CommandBuffer> commandBuffer_) noexcept;
+
+        constexpr ~AccelCommandBuffer() noexcept = default;
+
+    public:
+        [[nodiscard]] bool initialized() const noexcept {
+            return _initialized;
+        }
+
+        [[nodiscard]] bool valid() const noexcept {
+            return _valid;
+        }
+
+        [[nodiscard]] bool recording() const noexcept {
+            return _recording;
+        }
+
+        [[nodiscard]] bool isFaf() const noexcept {
+            return _faf;
+        }
+
+        [[nodiscard]] bool root() const noexcept {
+            return _root;
+        }
+
+    public:
+        /**
+         * Used for internal submission to native queues
+         *
+         * @details Will release control of submission to external handled instance.
+         *  Therefore this is primarily used from natives batches, as they are controlling
+         *  the combined submission of multiple command buffers at once.
+         */
+        void commit();
+
+    public:
+        void bindComputePass(const nmpt<const acc::ComputePass> computePass_);
+
+        void bindRenderPass(mref<acc::BindRenderPassStruct> bindRenderPass_);
+
+    public:
+        void bindPipeline(const nmpt<const acc::ComputePipeline> computePipeline_);
+
+        void bindPipeline(const nmpt<const acc::GraphicsPipeline> graphicsPipeline_);
+    };
 }
