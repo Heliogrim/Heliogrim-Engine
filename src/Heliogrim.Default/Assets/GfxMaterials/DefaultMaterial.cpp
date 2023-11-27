69fac38aebea03eabd43ed66a89848c39456cfe4
LtJulius <senator.spartas@googlemail.com>
2023-11-28 00:38:30 +0100

    Heliogrim Default Sky
    Assets:
    * updated factory invocation for heliogrim assets
    * added skybox prototype asset to heliogrim

--- src/Heliogrim.Default/Assets/GfxMaterials/DefaultMaterial.cpp
+++ src/Heliogrim.Default/Assets/GfxMaterials/DefaultMaterial.cpp
@@ -1,9 +1,12 @@
 #include "DefaultMaterial.hpp"
 
+#include <Engine.Pedantic/Clone/Clone.hpp>
+
 using namespace hg::game::assets::material;
 using namespace hg;
 
 DefaultMaterial::DefaultMaterial() :
     GfxMaterialAsset(
-        DefaultMaterial::auto_guid<DefaultMaterial>()
+        DefaultMaterial::auto_guid<DefaultMaterial>(),
+        clone(invalid_asset_guid)
     ) {}
--- src/Heliogrim.Default/Assets/GfxMaterials/DefaultSkybox.cpp
+++ src/Heliogrim.Default/Assets/GfxMaterials/DefaultSkybox.cpp
@@ -1,9 +1,14 @@
 #include "DefaultSkybox.hpp"
 
+#include <Engine.Pedantic/Clone/Clone.hpp>
+
+#include "DefaultSkyboxPrototype.hpp"
+
 using namespace hg::game::assets::material;
 using namespace hg;
 
 DefaultSkybox::DefaultSkybox() :
     GfxMaterialAsset(
-        DefaultSkybox::unstable_auto_guid()
+        DefaultSkybox::unstable_auto_guid(),
+        clone(DefaultSkyboxPrototype::unstable_auto_guid())
     ) {}
