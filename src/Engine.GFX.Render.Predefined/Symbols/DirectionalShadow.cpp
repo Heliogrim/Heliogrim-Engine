155f16f65dbdf364fb2660012e9d3daf4566c9d0
LtJulius <senator.spartas@googlemail.com>
2024-01-02 12:22:29 +0100

    Predefined Brdf & Shadow Symbols
    Render:
    * added predefined brdf prefilter symbol
    * added directional shadow symbol

--- src/Engine.GFX.Render.Predefined/Symbols/DirectionalShadow.cpp
+++ src/Engine.GFX.Render.Predefined/Symbols/DirectionalShadow.cpp
@@ -1,21 +1,21 @@
-#include "BrdfPrefilter.hpp"
+#include "DirectionalShadow.hpp"
 
 #include <Engine.Common/Make.hpp>
 
-#include "../Descriptions/BrdfPrefilterBuffer.hpp"
+#include "../Descriptions/DepthBuffer.hpp"
 
 using namespace hg::engine::render;
 using namespace hg;
 
-smr<const graph::Symbol> engine::render::makeBrdfPrefilterSymbol() {
+smr<const graph::Symbol> engine::render::makeDirectionalShadowSymbol() {
     static auto obj = make_smr<graph::Symbol>(
         graph::SymbolFlagBits::eUndefined,
         graph::SymbolScope {
             .type = graph::SymbolScopeType::eGlobal,
             .layer = ""
         },
-        "brdf::pref",
-        makeBrdfPrefilterBufferDescription()
+        "shadow::directional",
+        makeDepthBufferDescription()
     );
     return obj;
 }
