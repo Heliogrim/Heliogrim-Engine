85c3e375c906c505490a54e8de3cf0a2b29a5b41
LtJulius <senator.spartas@googlemail.com>
2024-01-20 14:35:03 +0100

    Preview Scene
    General:
    * added preview scene composition

---
+++ src/Engine.Scene.Preview/PreviewScene.cpp
@@ -0,0 +1,22 @@
+#include "PreviewScene.hpp"
+
+#include <Engine.Reflect/Cast.hpp>
+#include <Heliogrim/ActorComponent.hpp>
+#include <Heliogrim/SceneComponent.hpp>
+
+using namespace hg::engine::scene;
+using namespace hg;
+
+void PreviewScene::add(const ptr<ActorComponent> component_) {
+    if (auto* const sceneComponent = Cast<SceneComponent>(component_)) {
+        Scene::add(sceneComponent);
+    }
+}
+
+void PreviewScene::add(cref<ComponentHierarchy> hierarchy_) {
+    std::unreachable();
+}
+
+nmpt<IComponentRegisterContext> PreviewScene::registerContext() noexcept {
+    return this;
+}
---
+++ src/Engine.Scene.Preview/PreviewScene.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <Engine.Render.Scene/RenderSceneSystem.hpp>
+#include <Engine.Scene/Scene.hpp>
+#include <Heliogrim/IComponentRegisterContext.hpp>
+
+namespace hg::engine::scene {
+    class PreviewScene :
+        public Scene<render::RenderSceneSystem>,
+        public IComponentRegisterContext {
+    public:
+        using this_type = PreviewScene;
+
+    public:
+        void add(const ptr<ActorComponent> component_) override;
+
+        void add(cref<ComponentHierarchy> hierarchy_) override;
+
+    public:
+        [[nodiscard]] nmpt<IComponentRegisterContext> registerContext() noexcept override;
+    };
+}
