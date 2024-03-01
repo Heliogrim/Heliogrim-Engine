1e85f08feaacd879d8f69a649911120bb9f47cd8
LtJulius <admin@neogc.net>
2024-03-01 12:33:14 +0000

    CMake Library and Executable Functions

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,31 +1,33 @@
 # Dependency Resolving
 
 macro(set_dep_policy)
-    cmake_policy(SET CMP0077 NEW)
-    set(CMAKE_POLICY_DEFAULT_CMP0077 NEW)
+	cmake_policy(SET CMP0077 NEW)
+	set(CMAKE_POLICY_DEFAULT_CMP0077 NEW)
 
-    if (POLICY CMP0126)
-        cmake_policy(SET CMP0126 NEW)
-        set(CMAKE_POLICY_DEFAULT_CMP0216 NEW)
-    endif ()
+	if (POLICY CMP0126)
+		cmake_policy(SET CMP0126 NEW)
+		set(CMAKE_POLICY_DEFAULT_CMP0216 NEW)
+	endif ()
 
-    if (POLICY CMP0135)
-        cmake_policy(SET CMP0135 NEW)
-        set(CMAKE_POLICY_DEFAULT_CMP0135 NEW)
-    endif ()
+	if (POLICY CMP0135)
+		cmake_policy(SET CMP0135 NEW)
+		set(CMAKE_POLICY_DEFAULT_CMP0135 NEW)
+	endif ()
 
-    if (POLICY CMP0148)
-        cmake_policy(SET CMP0148 NEW)
-        set(CMAKE_POLICY_DEFAULT_CMP0148 NEW)
-    endif ()
+	if (POLICY CMP0148)
+		cmake_policy(SET CMP0148 NEW)
+		set(CMAKE_POLICY_DEFAULT_CMP0148 NEW)
+	endif ()
 endmacro()
 set_dep_policy()
 
 
 # Dependencies
 
+include(CTest)
+
 include(FindVulkan)
 
 if (NOT ${Vulkan_FOUND})
-    message(FATAL_ERROR "Vulkan not found. Please specify include directory and library path.")
+	message(FATAL_ERROR "Vulkan not found. Please specify include directory and library path.")
 endif ()
--- cmake/MakeTestProject.cmake
+++ cmake/MakeTestProject.cmake
@@ -1,86 +1,101 @@
 
 function(__prepare_link_libs libs out)
-    list(TRANSFORM libs STRIP)
-    set("${out}" ${libs} PARENT_SCOPE)
+	list(TRANSFORM libs STRIP)
+	set("${out}" ${libs} PARENT_SCOPE)
 endfunction()
 
-function(make_test_project name link_libraries)
-    if (NOT DEFINED name)
-        message(FATAL_ERROR "Target `${name}` is required parameter name to generate setup.")
-        return()
-    endif ()
+function(make_test_project)
 
-    message(STATUS "Setup library project for target `${name}`.")
-    name_to_target(target "${name}")
+	set(__single_var_options TEST_NAME SOURCE)
+	set(__multi_var_options LIBRARIES)
+	cmake_parse_arguments(make_lib_vars "" "${__single_var_options}" "${__multi_var_options}" ${ARGN})
 
-    list(LENGTH link_libraries link_libraries_size)
-    if (link_libraries_size GREATER 0)
-        __prepare_link_libs(${link_libraries} link_libraries)
-        list(LENGTH link_libraries link_libraries_size)
-    endif ()
-    message(STATUS "Using `${link_libraries_size}` libraries at linker.")
+	if (NOT DEFINED make_lib_vars_TEST_NAME)
+		message(FATAL_ERROR "Target `${make_lib_vars_TEST_NAME}` is required parameter name to generate setup.")
+		return()
+	endif ()
 
-    # Discover Sources
-    get_src_path(proj_src_dir)
-    set(source_directory "${proj_src_dir}/${name}")
+	message(STATUS "Setup library project for target `${make_lib_vars_TEST_NAME}`.")
+	name_to_target(target "${make_lib_vars_TEST_NAME}")
 
-    file(GLOB_RECURSE header_files CONFIGURE_DEPENDS ${source_directory}/*.hpp ${source_directory}/*.h ${source_directory}/*.hh)
-    file(GLOB_RECURSE source_files CONFIGURE_DEPENDS ${source_directory}/*.cpp ${source_directory}/*.c ${source_directory}/*.cc)
+	list(LENGTH make_lib_vars_LIBRARIES link_libraries_size)
+	if (link_libraries_size GREATER 0)
+		__prepare_link_libs(${make_lib_vars_LIBRARIES} make_lib_vars_LIBRARIES)
+		list(LENGTH make_lib_vars_LIBRARIES link_libraries_size)
+	endif ()
+	message(STATUS "Using `${link_libraries_size}` libraries at linker.")
 
-    # Sources
-    set(headers ${header_files})
-    set(sources ${source_files})
+	# Discover Sources
+	get_src_path(proj_src_dir)
 
-    # Executable
-    add_executable(${target} EXCLUDE_FROM_ALL ${sources} ${headers})
-    add_executable(${PROJECT_NAME}::${name} ALIAS ${target})
+	if (DEFINED make_lib_vars_SOURCE)
+		set(source_directory "${proj_src_dir}/${make_lib_vars_SOURCE}")
+	else ()
+		set(source_directory "${proj_src_dir}/${make_lib_vars_TEST_NAME}")
+	endif ()
 
-    # Project Options
-    set_target_properties(${target} PROPERTIES ${DEFAULT_PROJECT_OPTIONS} FOLDER "${IDE_FOLDER}")
+	file(GLOB_RECURSE header_files CONFIGURE_DEPENDS ${source_directory}/*.hpp ${source_directory}/*.h ${source_directory}/*.hh)
+	file(GLOB_RECURSE source_files CONFIGURE_DEPENDS ${source_directory}/*.cpp ${source_directory}/*.c ${source_directory}/*.cc)
 
-    # Include Directories
-    target_include_directories(
-            ${target}
-            PRIVATE
-            ${DEFAULT_INCLUDE_DIRECTORIES}
-            ${TEST_INCLUDE_DIRECTORIES}
-            ${proj_src_dir}
-    )
+	# Sources
+	set(headers ${header_files})
+	set(sources ${source_files})
 
-    # Libraries
-    target_link_libraries(
-            ${target}
-            PRIVATE
-            ${DEFAULT_LIBRARIES}
-            ${TEST_LIBRARIES}
-            ${link_libraries}
-    )
+	# Executable
+	add_executable(${target} EXCLUDE_FROM_ALL ${sources} ${headers})
+	add_executable(${PROJECT_NAME}::${make_lib_vars_TEST_NAME} ALIAS ${target})
 
-    # Compile
-    target_compile_definitions(
-            ${target}
-            PRIVATE
-            ${DEFAULT_COMPILE_DEFINITIONS}
-    )
+	# Project Options
+	set_target_properties(${target} PROPERTIES ${DEFAULT_PROJECT_OPTIONS} FOLDER "${IDE_FOLDER}")
 
-    # Compile Options
-    target_compile_options(
-            ${target}
-            PRIVATE
-            ${DEFAULT_COMPILE_OPTIONS}
-    )
+	# Include Directories
+	target_include_directories(
+			${target}
+			PRIVATE
+			${DEFAULT_INCLUDE_DIRECTORIES}
+			${TEST_INCLUDE_DIRECTORIES}
+			${proj_src_dir}
+	)
 
-    # Linker Options
-    target_link_options(
-            ${target}
-            PRIVATE
-            ${DEFAULT_LINKER_OPTIONS}
-    )
+	# Libraries
+	target_link_libraries(
+			${target}
+			PRIVATE
+			${DEFAULT_LIBRARIES}
+			${TEST_LIBRARIES}
+			${link_libraries}
+	)
 
-    # Target Health
-    # Deployment
-    get_dist_path(proj_dist_path)
-    set_target_properties(${target} PROPERTIES ARCHIVE_OUTPUT_DIRECTORY ${proj_dist_path}/$<CONFIG>)
-    set_target_properties(${target} PROPERTIES LIBRARY_OUTPUT_DIRECTORY ${proj_dist_path}/$<CONFIG>)
-    set_target_properties(${target} PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${proj_dist_path}/$<CONFIG>)
+	# Compile
+	target_compile_definitions(
+			${target}
+			PRIVATE
+			${DEFAULT_COMPILE_DEFINITIONS}
+	)
+
+	# Compile Options
+	target_compile_options(
+			${target}
+			PRIVATE
+			${DEFAULT_COMPILE_OPTIONS}
+	)
+
+	# Linker Options
+	target_link_options(
+			${target}
+			PRIVATE
+			${DEFAULT_LINKER_OPTIONS}
+	)
+
+	# Target Health
+	# Deployment
+	get_dist_path(proj_dist_path)
+	set_target_properties(${target} PROPERTIES ARCHIVE_OUTPUT_DIRECTORY ${proj_dist_path}/$<CONFIG>)
+	set_target_properties(${target} PROPERTIES LIBRARY_OUTPUT_DIRECTORY ${proj_dist_path}/$<CONFIG>)
+	set_target_properties(${target} PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${proj_dist_path}/$<CONFIG>)
+
+	# Register Tests
+	include(GoogleTest)
+	gtest_discover_tests(${target} DISCOVERY_MODE PRE_TEST)
+
 endfunction()
