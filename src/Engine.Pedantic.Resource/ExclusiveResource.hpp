a4017a6046de7372f059072b72c3258ae9f2541f
LtJulius <senator.spartas@googlemail.com>
2024-03-06 19:37:09 +0100

    Pedantic Resource, Exclusive Resources
    General:
    build: added new cmake project related to c++ language resource handling
    build: added new cmake test project corresponding to the new resource project
    feat: added new project to manage resources and there related access control
    feat: added virtual base for resource objects
    feat: added templated base object for resources
    feat: added common functions for resources to work with at base template
    feat: added exclusive resource specialization to control managed value access
    feat: added two distinct access modes to enable shared access for future work
    feat: added conditional moving operations to resource wrapper in regard to movability of the underlying managed type cascaded by the storage holder
    feat: added RAII wrapper for accessor to automatically manage locking and unlocking of exclusive access
    test: added test checking the internal value changes with and without try-mutation verification
    test: added test checking exclusive behaviour, preventing additional access after first acquisition
    test: added verification test related to moving and lifecycle behaviour of internally stored objects via storage holder
    test: added verification test related to return values of apply and tryApply functions to work correctly

---
+++ src/Engine.Pedantic.Resource/ExclusiveResource.hpp
@@ -0,0 +1,141 @@
+#pragma once
+#include <Engine.Scheduler/Helper/Wait.hpp>
+
+#include "Resource.hpp"
+#include "ResourceStorage.hpp"
+
+namespace hg {
+	template <typename ManagedType_, typename StorageType_ = ResourceStorage<ManagedType_>>
+	class ExclusiveResource;
+
+	/**/
+
+	template <typename ManagedType_, typename StorageType_ = ResourceStorage<ManagedType_>, typename... Args_>
+	[[nodiscard]] ExclusiveResource<ManagedType_, StorageType_> make_exclusive_resource(Args_&&... args_);
+
+	/**/
+
+	template <typename ManagedType_, typename StorageType_>
+	class ExclusiveResource final :
+		public Resource<ManagedType_> {
+	public:
+		template <typename ManagedType_, typename StorageType_, typename... Args_>
+		friend ExclusiveResource<ManagedType_, StorageType_> make_exclusive_resource(Args_&&... args_);
+
+	public:
+		using this_type = ExclusiveResource<ManagedType_, StorageType_>;
+		using base_type = Resource<ManagedType_>;
+
+		using storage_type = StorageType_;
+		using typename base_type::readonly_accessor_type;
+		using typename base_type::readwrite_accessor_type;
+
+	public:
+		constexpr ExclusiveResource() noexcept requires std::is_default_constructible_v<storage_type> :
+			Resource<ManagedType_>(),
+			_storage() {}
+
+		template <typename... Args_>
+		constexpr explicit ExclusiveResource(Args_&&... args_) noexcept
+			requires std::is_constructible_v<typename storage_type::managed_type, Args_...> :
+			Resource<ManagedType_>(),
+			_storage({ std::forward<Args_>(args_)... }) {}
+
+		ExclusiveResource(ExclusiveResource&& other_) noexcept
+			requires (not std::is_const_v<typename storage_type::managed_type>) &&
+			std::is_nothrow_move_constructible_v<typename storage_type::managed_type> :
+			Resource<ManagedType_>(),
+			_storage(storage_type::from(other_.acquireReadWrite())) {}
+
+		ExclusiveResource(ExclusiveResource&& other_) noexcept
+			requires (not std::is_const_v<typename storage_type::managed_type>) &&
+			(not std::is_nothrow_move_constructible_v<typename storage_type::managed_type>) &&
+			std::is_default_constructible_v<typename storage_type::managed_type> &&
+			std::is_move_assignable_v<typename storage_type::managed_type> :
+			Resource<ManagedType_>(),
+			_storage() {
+			other_.apply(
+				[this](ref<typename storage_type::managed_type> obj_) {
+					_storage.getValueReference() = std::move(obj_);
+				}
+			);
+		}
+
+		constexpr ~ExclusiveResource() noexcept override = default;
+
+	public:
+		[[nodiscard]] constexpr bool isShared() const noexcept override {
+			return false;
+		}
+
+	private:
+		storage_type _storage;
+		mutable std::atomic_flag _lck;
+
+	private:
+		FORCE_INLINE void acquireLock() const noexcept {
+			while (_lck.test_and_set(std::memory_order::release)) {
+				::hg::engine::scheduler::waitOnAtomic(_lck, true);
+			}
+		}
+
+		FORCE_INLINE bool tryAcquireLock() const noexcept {
+			return not _lck.test_and_set(std::memory_order::release);
+		}
+
+		FORCE_INLINE void releaseLock() const noexcept {
+			_lck.clear(std::memory_order::release);
+			_lck.notify_one();
+		}
+
+	public:
+		[[nodiscard]] readonly_accessor_type acquireReadonly() const noexcept override {
+			acquireLock();
+			return readonly_accessor_type { *this, _storage.getConstValueReference() };
+		}
+
+		[[nodiscard]] readwrite_accessor_type acquireReadWrite() const noexcept override {
+			acquireLock();
+			return readwrite_accessor_type { *this, _storage.getValueReference() };
+		}
+
+		[[nodiscard]] tl::optional<readonly_accessor_type> tryAcquireReadonly() const noexcept override {
+			return tryAcquireLock() ?
+				       tl::make_optional<readonly_accessor_type>(*this, _storage.getConstValueReference()) :
+				       tl::nullopt;
+		}
+
+		[[nodiscard]] tl::optional<readwrite_accessor_type> tryAcquireReadWrite() const noexcept override {
+			return tryAcquireLock() ?
+				       tl::make_optional<readwrite_accessor_type>(*this, _storage.getValueReference()) :
+				       tl::nullopt;
+		}
+
+	protected:
+		void release([[maybe_unused]] ResourceAccessMode mode_) const noexcept override {
+			releaseLock();
+		}
+	};
+
+	/**/
+
+	template <typename ManagedType_, typename StorageType_, typename... Args_>
+	[[nodiscard]] ExclusiveResource<ManagedType_, StorageType_> make_exclusive_resource(Args_&&... args_) {
+		using type = ExclusiveResource<ManagedType_, StorageType_>;
+		return type { std::forward<Args_>(args_)... };
+	}
+
+	template <typename DeducedType_>
+	struct sanitize_resource_type {
+		using type = std::remove_cvref_t<DeducedType_>;
+	};
+
+	template <
+		typename DeducedType_,
+		typename StorageType_ = ResourceStorage<typename sanitize_resource_type<DeducedType_>::type>>
+	[[nodiscard]] ExclusiveResource<typename sanitize_resource_type<DeducedType_>::type, StorageType_>
+	make_exclusive_resource(DeducedType_&& initial_) {
+		using type = ExclusiveResource<typename sanitize_resource_type<DeducedType_>::type, StorageType_>;
+		return type { std::forward<DeducedType_>(initial_) };
+	}
+}
