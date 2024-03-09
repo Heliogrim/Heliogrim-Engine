9f008de450c6ee474bbc7f4dd7c293bf7b5f371d
LtJulius <senator.spartas@googlemail.com>
2024-03-09 21:06:16 +0100

    Minor Fixes
    General:
    * corrected seed of murmur hash to use 64 bit value
    * corrected suffix type of asset guid hash using murmur
    * corrected suffix type of type id hash using murmur
    
    Input:
    * added missing break statement to input event handler

--- src/Engine.Common/Hash/Murmur3.hpp
+++ src/Engine.Common/Hash/Murmur3.hpp
@@ -4,42 +4,42 @@
 #include "../__macro.hpp"
 
 namespace hg::hash {
-    /**
-     * Murmur Hash 3 (x86/64 Platform) (32Bit Output)
-     *
-     * @author Julius
-     * @date 12.11.2020
-     *
-     * @param 		   data_ The data.
-     * @param 		   length_ The length.
-     * @param 		   seed_ The seed.
-     * @param [in,out] dst_ If non-null, destination for the.
-     */
-    void murmur3_x86_32(IN const void* data_, IN const u32 length_, IN const u32 seed_, OUT u32* dst_);
+	/**
+	 * Murmur Hash 3 (x86/64 Platform) (32Bit Output)
+	 *
+	 * @author Julius
+	 * @date 12.11.2020
+	 *
+	 * @param 		   data_ The data.
+	 * @param 		   length_ The length.
+	 * @param 		   seed_ The seed.
+	 * @param [in,out] dst_ If non-null, destination for the.
+	 */
+	void murmur3_x86_32(IN const void* data_, IN const u32 length_, IN const u32 seed_, OUT u32* dst_);
 
-    /**
-     * Murmur Hash 3 (x64 Platform) (64Bit Output)
-     *
-     * @author Julius
-     * @date 12.11.2020
-     *
-     * @param          data_ The data.
-     * @param          length_ The length.
-     * @param          seed_ The seed.
-     * @param [out]    dst_ Destination for the hash value.
-     */
-    void murmur3_x64_64(IN const void* data_, IN const u32 length_, IN const u32 seed_, OUT u64& dst_);
+	/**
+	 * Murmur Hash 3 (x64 Platform) (64Bit Output)
+	 *
+	 * @author Julius
+	 * @date 12.11.2020
+	 *
+	 * @param          data_ The data.
+	 * @param          length_ The length.
+	 * @param          seed_ The seed.
+	 * @param [out]    dst_ Destination for the hash value.
+	 */
+	void murmur3_x64_64(IN const void* data_, IN const u32 length_, IN const u32 seed_, OUT u64& dst_);
 
-    /**
-     * Murmur Hash 3 (x64 Platform) (128Bit Output)
-     *
-     * @author Julius
-     * @date 12.11.2020
-     *
-     * @param 		   data_ The data.
-     * @param 		   length_ The length.
-     * @param 		   seed_ The seed.
-     * @param [in,out] dst_ If non-null, destination for the.
-     */
-    void murmur3_x64_128(IN const void* data_, IN const u32 length_, IN const u32 seed_, OUT void* dst_);
+	/**
+	 * Murmur Hash 3 (x64 Platform) (128Bit Output)
+	 *
+	 * @author Julius
+	 * @date 12.11.2020
+	 *
+	 * @param 		   data_ The data.
+	 * @param 		   length_ The length.
+	 * @param 		   seed_ The seed.
+	 * @param [in,out] dst_ If non-null, destination for the.
+	 */
+	void murmur3_x64_128(IN const void* data_, IN const u32 length_, IN const u64 seed_, OUT void* dst_);
 }
--- src/Engine.Input/Input.cpp
+++ src/Engine.Input/Input.cpp
@@ -22,23 +22,23 @@ using namespace hg::engine;
 using namespace hg;
 
 Input::Input(const non_owning_rptr<Engine> engine_) noexcept :
-    CoreModule(engine_),
-    _dragDropReceiver(nullptr),
-    _dragDropSender(nullptr) {}
+	CoreModule(engine_),
+	_dragDropReceiver(nullptr),
+	_dragDropSender(nullptr) {}
 
 Input::~Input() {
-    Input::destroy();
+	Input::destroy();
 }
 
 void Input::setup() {
-    _dragDropSender = new Win32DragDropSender();
-    _dragDropSender->setup();
+	_dragDropSender = new Win32DragDropSender();
+	_dragDropSender->setup();
 
-    /**
-     * Scheduling Pipelines
-     */
-    auto inputPipeline = make_uptr<schedule::InputPipeline>();
-    _engine->getScheduler()->getCompositePipeline()->addPipeline(std::move(inputPipeline));
+	/**
+	 * Scheduling Pipelines
+	 */
+	auto inputPipeline = make_uptr<schedule::InputPipeline>();
+	_engine->getScheduler()->getCompositePipeline()->addPipeline(std::move(inputPipeline));
 }
 
 void Input::start() {}
@@ -46,127 +46,128 @@ void Input::start() {}
 void Input::stop() {}
 
 void Input::destroy() {
-    if (_dragDropReceiver) {
-        _dragDropReceiver->destroy();
+	if (_dragDropReceiver) {
+		_dragDropReceiver->destroy();
 
-        delete _dragDropReceiver;
-        _dragDropReceiver = nullptr;
-    }
+		delete _dragDropReceiver;
+		_dragDropReceiver = nullptr;
+	}
 
-    if (_dragDropSender) {
-        _dragDropSender->destroy();
+	if (_dragDropSender) {
+		_dragDropSender->destroy();
 
-        delete _dragDropSender;
-        _dragDropSender = nullptr;
-    }
+		delete _dragDropSender;
+		_dragDropSender = nullptr;
+	}
 }
 
 void Input::tick() {
 
-    std::unique_lock<std::mutex> lck { _bufferMtx };
+	std::unique_lock<std::mutex> lck { _bufferMtx };
 
-    for (const auto& entry : _buffered) {
+	for (const auto& entry : _buffered) {
 
-        switch (entry.first.data) {
-            case event::MouseMoveEvent::typeId.data: {
-                _emitter.emit<event::MouseMoveEvent>(static_cast<ptr<event::MouseMoveEvent>>(entry.second.get()));
-                break;
-            }
-            case event::MouseWheelEvent::typeId.data: {
-                _emitter.emit<event::MouseWheelEvent>(static_cast<ptr<event::MouseWheelEvent>>(entry.second.get()));
-                break;
-            }
-            case event::MouseButtonEvent::typeId.data: {
-                _emitter.emit<event::MouseButtonEvent>(static_cast<ptr<event::MouseButtonEvent>>(entry.second.get()));
-                break;
-            }
-            case event::DragDropEvent::typeId.data: {
-                _emitter.emit<event::DragDropEvent>(static_cast<ptr<event::DragDropEvent>>(entry.second.get()));
-                break;
-            }
-            case event::KeyboardEvent::typeId.data: {
-                _emitter.emit<event::KeyboardEvent>(static_cast<ptr<event::KeyboardEvent>>(entry.second.get()));
-                break;
-            }
-            case platform::PlatformResizeEvent::typeId.data: {
+		switch (entry.first.data) {
+			case event::MouseMoveEvent::typeId.data: {
+				_emitter.emit<event::MouseMoveEvent>(static_cast<ptr<event::MouseMoveEvent>>(entry.second.get()));
+				break;
+			}
+			case event::MouseWheelEvent::typeId.data: {
+				_emitter.emit<event::MouseWheelEvent>(static_cast<ptr<event::MouseWheelEvent>>(entry.second.get()));
+				break;
+			}
+			case event::MouseButtonEvent::typeId.data: {
+				_emitter.emit<event::MouseButtonEvent>(static_cast<ptr<event::MouseButtonEvent>>(entry.second.get()));
+				break;
+			}
+			case event::DragDropEvent::typeId.data: {
+				_emitter.emit<event::DragDropEvent>(static_cast<ptr<event::DragDropEvent>>(entry.second.get()));
+				break;
+			}
+			case event::KeyboardEvent::typeId.data: {
+				_emitter.emit<event::KeyboardEvent>(static_cast<ptr<event::KeyboardEvent>>(entry.second.get()));
+				break;
+			}
+			case platform::PlatformResizeEvent::typeId.data: {
 
-                auto* const event = static_cast<
-                    const ptr<platform::PlatformResizeEvent>>(entry.second.get());
-                event->getWindow()->resizeEmitter().emit(event);
+				auto* const event = static_cast<
+					const ptr<platform::PlatformResizeEvent>>(entry.second.get());
+				event->getWindow()->resizeEmitter().emit(event);
 
-                _emitter.emit<platform::PlatformResizeEvent>(
-                    *event
-                );
-            }
-            default: {}
-        }
+				_emitter.emit<platform::PlatformResizeEvent>(
+					*event
+				);
+				break;
+			}
+			default: {}
+		}
 
-    }
+	}
 
-    _buffered.clear();
+	_buffered.clear();
 }
 
 cref<GlobalEventEmitter> Input::emitter() const noexcept {
-    return _emitter;
+	return _emitter;
 }
 
 ref<GlobalEventEmitter> Input::emitter() noexcept {
-    return _emitter;
+	return _emitter;
 }
 
 bool Input::tryBufferEvent(const event_type_id eventType_, mref<uptr<Event>> event_) {
 
-    std::unique_lock<std::mutex> lck { _bufferMtx, std::defer_lock };
-    if (not lck.try_lock()) {
-        return false;
-    }
+	std::unique_lock<std::mutex> lck { _bufferMtx, std::defer_lock };
+	if (not lck.try_lock()) {
+		return false;
+	}
 
-    _buffered.emplace_back(eventType_, std::move(event_));
-    return true;
+	_buffered.emplace_back(eventType_, std::move(event_));
+	return true;
 }
 
 void Input::bufferEvent(const event_type_id eventType_, mref<uptr<Event>> event_) {
-    std::unique_lock<std::mutex> lck { _bufferMtx };
-    _buffered.emplace_back(eventType_, std::move(event_));
+	std::unique_lock<std::mutex> lck { _bufferMtx };
+	_buffered.emplace_back(eventType_, std::move(event_));
 }
 
 const ptr<input::DragDropSender> Input::dragDropSender() const noexcept {
-    return _dragDropSender;
+	return _dragDropSender;
 }
 
 void Input::captureWindow(const non_owning_rptr<platform::NativeWindow> nativeWindow_) {
-    assert(not _dragDropReceiver);
+	assert(not _dragDropReceiver);
 
-    #ifdef WIN32
-    auto* sdlWnd = static_cast<ptr<platform::Win32Window>>(nativeWindow_)->sdl();
-    SDL_SysWMinfo info {};
-    SDL_VERSION(&info.version);
-    SDL_GetWindowWMInfo(sdlWnd, &info);
+	#ifdef WIN32
+	auto* sdlWnd = static_cast<ptr<platform::Win32Window>>(nativeWindow_)->sdl();
+	SDL_SysWMinfo info {};
+	SDL_VERSION(&info.version);
+	SDL_GetWindowWMInfo(sdlWnd, &info);
 
-    const HWND hwnd = info.info.win.window;
+	const HWND hwnd = info.info.win.window;
 
-    _dragDropReceiver = new Win32DragDropReceiver(hwnd);
-    _dragDropReceiver->setup();
+	_dragDropReceiver = new Win32DragDropReceiver(hwnd);
+	_dragDropReceiver->setup();
 
-    /**/
+	/**/
 
-    _dragDropReceiver->setOnDrop(
-        [this](mref<uptr<event::DragDropEvent>> event_) {
-            // Attention: This function callback is actual external thread context
-            // Attention: Not allowed to use default/fiber control flow
-            bufferEvent(event::DragDropEvent::typeId, std::move(event_));
-            return false;
-        }
-    );
-    #else
-    #endif
+	_dragDropReceiver->setOnDrop(
+		[this](mref<uptr<event::DragDropEvent>> event_) {
+			// Attention: This function callback is actual external thread context
+			// Attention: Not allowed to use default/fiber control flow
+			bufferEvent(event::DragDropEvent::typeId, std::move(event_));
+			return false;
+		}
+	);
+	#else
+	#endif
 }
 
 void Input::releaseWindow(const non_owning_rptr<platform::NativeWindow> nativeWindow_) {
-    if (_dragDropReceiver) {
-        _dragDropReceiver->destroy();
+	if (_dragDropReceiver) {
+		_dragDropReceiver->destroy();
 
-        delete _dragDropReceiver;
-        _dragDropReceiver = nullptr;
-    }
+		delete _dragDropReceiver;
+		_dragDropReceiver = nullptr;
+	}
 }
