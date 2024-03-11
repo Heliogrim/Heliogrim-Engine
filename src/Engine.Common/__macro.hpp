60bf3642ba38296c598836a167f52cdc4cba8971
LtJulius <admin@neogc.net>
2024-03-11 14:54:09 +0000

    Asserts

--- src/Engine.Common/__macro.hpp
+++ src/Engine.Common/__macro.hpp
@@ -132,17 +132,6 @@
 #define ALIGNED(struct_, alignment_) __declspec(align(alignment_)) struct_
 #endif
 
-// Assume (Optimization <-> Progress with caution)
-#ifndef assume
-#ifdef ENV_MSVC
-#define assume(expr) __assume(expr);
-#elif defined(ENV_GCC)
-#define assume(expr)
-#elif defined(ENV_CLANG)
-#define assume(expr) __builtin_assume(expr);
-#endif
-#endif
-
 // Comparison
 #ifndef MAX
 #define MAX(a,b) (((a) > (b)) ? (a) : (b))
