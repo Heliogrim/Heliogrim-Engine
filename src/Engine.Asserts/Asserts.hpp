faa7b9b3156c72da11f7f2a70dba165e9fefc013
LtJulius <senator.spartas@googlemail.com>
2024-03-11 23:46:12 +0100

    Assert Convertible
    General:
    * extended debug and runtime asserts with direct convertible overload

--- src/Engine.Asserts/Asserts.hpp
+++ src/Engine.Asserts/Asserts.hpp
@@ -26,6 +26,15 @@ namespace hg {
 		}
 	}
 
+	template <typename Expr_>
+		requires (not std::is_invocable_v<Expr_>) &&
+		std::is_nothrow_convertible_v<Expr_, bool>
+	void assertrt(Expr_&& expr_) noexcept {
+		if (not static_cast<bool>(std::forward<Expr_>(expr_))) {
+			::hg::panic();
+		}
+	}
+
 	#if defined(_DEBUG) && not defined(NDEBUG)
 
 	template <typename Expr_>
@@ -48,6 +57,16 @@ namespace hg {
 		}
 	}
 
+	template <typename Expr_>
+		requires (not std::is_invocable_v<Expr_>) &&
+		std::is_nothrow_convertible_v<Expr_, bool>
+	void assertd(Expr_&& expr_) noexcept {
+		if (not static_cast<bool>(std::forward<Expr_>(expr_))) {
+			::hg::breakpoint();
+			::hg::panic();
+		}
+	}
+
 	#else
 
 	template <typename Expr_>
@@ -60,6 +79,13 @@ namespace hg {
 		requires (not std::is_invocable_r_v<bool, Expr_>) &&
 		std::is_invocable_v<Expr_> &&
 		std::is_convertible_v<std::invoke_result_t<Expr_>, bool>
+	void assertd(Expr_&& expr_) noexcept {
+		// noop();
+	}
+
+	template <typename Expr_>
+		requires (not std::is_invocable_v<Expr_>) &&
+		std::is_nothrow_convertible_v<Expr_, bool>
 	void assertd(Expr_&& expr_) noexcept {
 		// noop();
 	}
