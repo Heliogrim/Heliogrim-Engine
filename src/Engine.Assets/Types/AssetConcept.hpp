5d41fac74897bb73fe720db93f6346f2aff12257
LtJulius <senator.spartas@googlemail.com>
2024-03-25 13:56:08 +0100

    MSVC Clang CMake Fixes
    General:
    * added compiler flags to suppress warnings of intentionally used reserved identifier prefixes
    * added missing inline qualifiers for global templated variables

--- src/Engine.Assets/Types/AssetConcept.hpp
+++ src/Engine.Assets/Types/AssetConcept.hpp
@@ -5,35 +5,35 @@
 #include "Asset.hpp"
 
 namespace hg::engine::assets {
-    /**
-     * The Concept for assets
-     */
-    template <class Ty>
-    concept IsAsset = requires(const Ty obj) {
-        { obj.get_guid() } -> std::same_as<asset_guid>;
-    } && hg::HasType<Ty, asset_type_id>;
+	/**
+	 * The Concept for assets
+	 */
+	template <class Ty>
+	concept IsAsset = requires(const Ty obj) {
+		{ obj.get_guid() } -> std::same_as<asset_guid>;
+	} && hg::HasType<Ty, asset_type_id>;
 
-    template <class Type_ = void>
-    constexpr bool _has_data_layout = true;
+	template <class Type_ = void>
+	constexpr inline bool _has_data_layout = true;
 
-    template <>
-    constexpr bool _has_data_layout<void> = false;
+	template <>
+	constexpr inline bool _has_data_layout<void> = false;
 
-    template <class Type_>
-    concept HasDataLayout = _has_data_layout<typename Type_::layout_type>;
+	template <class Type_>
+	concept HasDataLayout = _has_data_layout<typename Type_::layout_type>;
 
-    template <IsAsset Type_>
-    struct MetaLoaderAsset {
-        using type = Type_;
-        inline static constexpr bool value = false;
-    };
+	template <IsAsset Type_>
+	struct MetaLoaderAsset {
+		using type = Type_;
+		inline static constexpr bool value = false;
+	};
 
-    template <IsAsset Type_>
-    struct MetaStreamLoaderAsset {
-        using type = Type_;
-        inline static constexpr bool value = false;
-    };
+	template <IsAsset Type_>
+	struct MetaStreamLoaderAsset {
+		using type = Type_;
+		inline static constexpr bool value = false;
+	};
 
-    template <class Type_>
-    concept IsStreamableAsset = IsAsset<Type_> && MetaStreamLoaderAsset<Type_>::value;
+	template <class Type_>
+	concept IsStreamableAsset = IsAsset<Type_> && MetaStreamLoaderAsset<Type_>::value;
 }
