65fa41a5fbb700e89efeca0c8b8f87c0813f9680
LtJulius <senator.spartas@googlemail.com>
2024-04-22 21:43:12 +0200

    CMake Re-Configure Optimization
    General:
    * added derivat of FetchContent_Declare to add custom "caching" behaviour preventing expensive source checks
    * added some patches to optimize sub-project setups
    * added cache condition to the source_group mapping helper, preventing source_group generator for unknown or unsupported generator targets
    * added cmake policy to choose location lookups for python packages
    * added default override for git remote update functions when using cmake's ExternalProject

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,29 +1,38 @@
 # Dependency Resolving
 
 macro(set_dep_policy)
-	cmake_policy(SET CMP0048 NEW)
-	set(CMAKE_POLICY_DEFAULT_CMP0048 NEW)
+    cmake_policy(SET CMP0048 NEW)
+    set(CMAKE_POLICY_DEFAULT_CMP0048 NEW)
 
-	cmake_policy(SET CMP0077 NEW)
-	set(CMAKE_POLICY_DEFAULT_CMP0077 NEW)
+    cmake_policy(SET CMP0077 NEW)
+    set(CMAKE_POLICY_DEFAULT_CMP0077 NEW)
 
-	if (POLICY CMP0126)
-		cmake_policy(SET CMP0126 NEW)
-		set(CMAKE_POLICY_DEFAULT_CMP0216 NEW)
-	endif ()
+    # Modules FindPython3, FindPython2 and FindPython use LOCATION for lookup strategy.
+    # https://cmake.org/cmake/help/latest/policy/CMP0094.html
+    cmake_policy(SET CMP0094 NEW)
+    set(CMAKE_POLICY_DEFAULT_CMP0094 NEW)
 
-	if (POLICY CMP0135)
-		cmake_policy(SET CMP0135 NEW)
-		set(CMAKE_POLICY_DEFAULT_CMP0135 NEW)
-	endif ()
+    if (POLICY CMP0126)
+        cmake_policy(SET CMP0126 NEW)
+        set(CMAKE_POLICY_DEFAULT_CMP0216 NEW)
+    endif ()
 
-	if (POLICY CMP0148)
-		cmake_policy(SET CMP0148 NEW)
-		set(CMAKE_POLICY_DEFAULT_CMP0148 NEW)
-	endif ()
+    if (POLICY CMP0135)
+        cmake_policy(SET CMP0135 NEW)
+        set(CMAKE_POLICY_DEFAULT_CMP0135 NEW)
+    endif ()
+
+    if (POLICY CMP0148)
+        cmake_policy(SET CMP0148 NEW)
+        set(CMAKE_POLICY_DEFAULT_CMP0148 NEW)
+    endif ()
 endmacro()
 set_dep_policy()
 
+# Configure
+if (NOT DEFINED CMAKE_EP_GIT_REMOTE_UPDATE_STRATEGY)
+    set(CMAKE_EP_GIT_REMOTE_UPDATE_STRATEGY REBASE_CHECKOUT)
+endif ()
 
 # Dependencies
 
@@ -32,5 +41,5 @@ include(CTest)
 include(FindVulkan)
 
 if (NOT ${Vulkan_FOUND})
-	message(FATAL_ERROR "Vulkan not found. Please specify include directory and library path.")
+    message(FATAL_ERROR "Vulkan not found. Please specify include directory and library path.")
 endif ()
