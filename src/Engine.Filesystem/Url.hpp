ffe1abe26c9cb9f99e656692ce88012e2d7f4854
LtJulius <senator.spartas@googlemail.com>
2024-06-06 00:30:31 +0200

    Constexpr fs::Url
    General:
    feat: updated default constructor of fs::Url to support constexpr expressions

--- src/Engine.Filesystem/Url.hpp
+++ src/Engine.Filesystem/Url.hpp
@@ -7,53 +7,53 @@
 #include <Engine.Filesystem/Path.hpp>
 
 namespace hg::engine::fs {
-    class Url {
-    public:
-        ACCESS_LAYOUT
-        ACCESS_STRUCTURE
+	class Url {
+	public:
+		ACCESS_LAYOUT
+		ACCESS_STRUCTURE
 
-    public:
-        using this_type = Url;
+	public:
+		using this_type = Url;
 
-    public:
-        Url();
+	public:
+		constexpr Url() noexcept = default;
 
-        Url(cref<this_type> other_) = default;
+		Url(cref<this_type> other_) = default;
 
-        Url(mref<this_type> other_) noexcept = default;
+		Url(mref<this_type> other_) noexcept = default;
 
-        Url(mref<Path> path_);
+		Url(mref<Path> path_);
 
-        Url(string_view scheme_, mref<Path> path_);
+		Url(string_view scheme_, mref<Path> path_);
 
-        ~Url();
+		~Url();
 
-    public:
-        ref<this_type> operator=(cref<this_type> other_) = default;
+	public:
+		ref<this_type> operator=(cref<this_type> other_) = default;
 
-        ref<this_type> operator=(mref<this_type> other_) noexcept = default;
+		ref<this_type> operator=(mref<this_type> other_) noexcept = default;
 
-    private:
-        string _scheme;
-        Path _path;
+	private:
+		string _scheme;
+		Path _path;
 
-    public:
-        [[nodiscard]] bool hasScheme() const noexcept;
+	public:
+		[[nodiscard]] bool hasScheme() const noexcept;
 
-        [[nodiscard]] const string_view scheme() const noexcept;
+		[[nodiscard]] const string_view scheme() const noexcept;
 
-        ref<this_type> setScheme(string_view scheme_);
+		ref<this_type> setScheme(string_view scheme_);
 
-        [[nodiscard]] bool hasPath() const noexcept;
+		[[nodiscard]] bool hasPath() const noexcept;
 
-        [[nodiscard]] cref<Path> path() const noexcept;
+		[[nodiscard]] cref<Path> path() const noexcept;
 
-        ref<this_type> setPath(mref<Path> path_);
+		ref<this_type> setPath(mref<Path> path_);
 
-    public:
-        [[nodiscard]] bool empty() const noexcept;
+	public:
+		[[nodiscard]] bool empty() const noexcept;
 
-    public:
-        [[nodiscard]] string encode() const noexcept;
-    };
+	public:
+		[[nodiscard]] string encode() const noexcept;
+	};
 }
