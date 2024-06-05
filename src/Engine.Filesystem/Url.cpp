078ef5d2e142bf5feb5a7c76678ead28672071cb
LtJulius <senator.spartas@googlemail.com>
2024-06-06 00:30:31 +0200

    Constexpr fs::Url
    General:
    feat: updated default constructor of fs::Url to support constexpr expressions

--- src/Engine.Filesystem/Url.cpp
+++ src/Engine.Filesystem/Url.cpp
@@ -3,53 +3,49 @@
 using namespace hg::engine::fs;
 using namespace hg;
 
-Url::Url() :
-    _scheme(),
-    _path() {}
-
 Url::Url(mref<Path> path_) :
-    _scheme(),
-    _path(std::move(path_)) {}
+	_scheme(),
+	_path(std::move(path_)) {}
 
 Url::Url(string_view scheme_, mref<Path> path_) :
-    _scheme(scheme_),
-    _path(std::move(path_)) {}
+	_scheme(scheme_),
+	_path(std::move(path_)) {}
 
 Url::~Url() = default;
 
 bool Url::hasScheme() const noexcept {
-    return not _scheme.empty();
+	return not _scheme.empty();
 }
 
 const string_view Url::scheme() const noexcept {
-    return _scheme;
+	return _scheme;
 }
 
 ref<Url::this_type> Url::setScheme(string_view scheme_) {
-    _scheme = scheme_;
-    return *this;
+	_scheme = scheme_;
+	return *this;
 }
 
 bool Url::hasPath() const noexcept {
-    return not _path.empty();
+	return not _path.empty();
 }
 
 cref<Path> Url::path() const noexcept {
-    return _path;
+	return _path;
 }
 
 ref<Url::this_type> Url::setPath(mref<Path> path_) {
-    _path = std::move(path_);
-    return *this;
+	_path = std::move(path_);
+	return *this;
 }
 
 bool Url::empty() const noexcept {
-    return _scheme.empty() && _path.empty();
+	return _scheme.empty() && _path.empty();
 }
 
 string Url::encode() const noexcept {
-    if (not _scheme.empty()) {
-        return _scheme + "://" + _path.string();
-    }
-    return _path.string();
+	if (not _scheme.empty()) {
+		return _scheme + "://" + _path.string();
+	}
+	return _path.string();
 }
