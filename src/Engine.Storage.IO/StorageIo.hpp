7c82d58dd190b16a45dc9cd710a6ed5dd5255ec8
LtJulius <senator.spartas@googlemail.com>
2024-06-15 14:16:01 +0200

    Storage IO Base
    General:
    feat: removed dummy functions used to generate storage input and output streams
    feat: removed storage input and output stream dummy objects
    feat: added a templated dummy object to generate one layer of indirection before accessing internal resource objects
    feat: added factory functions to grant access to internal io-handled resources based on the provided storage object

--- src/Engine.Storage.IO/StorageIo.hpp
+++ src/Engine.Storage.IO/StorageIo.hpp
@@ -2,15 +2,40 @@
 
 #include <Engine.Common/Wrapper.hpp>
 #include <Engine.Common/Memory/MemoryPointer.hpp>
+#include <Engine.Storage/IStorage.hpp>
+
+#include "Io.hpp"
+
+namespace hg {
+	template <typename ManagedType_>
+	class ResourceAccessor;
+}
 
 namespace hg::engine {
 	class Engine;
+}
+
+namespace hg::engine::resource {
+	class MemoryBuffer;
+	class LocalFile;
+	class Package;
+}
+
+namespace hg::engine::serialization {
+	class Archive;
 }
 
 namespace hg::engine::storage {
 	class IStorage;
 	class StorageInputStream;
 	class StorageOutputStream;
+}
+
+namespace hg::engine::storage::system {
+	class ArchiveStorage;
+	class LocalFileStorage;
+	class MemoryStorage;
+	class PackageStorage;
 }
 
 namespace hg::engine::storage {
@@ -31,8 +56,36 @@ namespace hg::engine::storage {
 		nmpt<Engine> _engine;
 
 	public:
-		[[nodiscard]] uptr<StorageInputStream> makeInputStream(_In_ mref<IStorage> storage_);
+		[[nodiscard]] uptr<ResourceAccessor<Io<resource::MemoryBuffer>>, decltype([](auto*) {})> accessReadWrite(
+			_In_ mref<Arci<system::MemoryStorage>> storage_
+		);
 
-		[[nodiscard]] uptr<StorageOutputStream> makeOutputStream(_In_ mref<IStorage> storage_);
+		[[nodiscard]] uptr<ResourceAccessor<Io<const resource::MemoryBuffer>>, decltype([](auto*) {})> accessReadonly(
+			_In_ mref<Arci<system::MemoryStorage>> storage_
+		);
+
+		[[nodiscard]] uptr<ResourceAccessor<Io<resource::LocalFile>>, decltype([](auto*) {})> accessReadWrite(
+			_In_ mref<Arci<system::LocalFileStorage>> storage_
+		);
+
+		[[nodiscard]] uptr<ResourceAccessor<Io<const resource::LocalFile>>, decltype([](auto*) {})> accessReadonly(
+			_In_ mref<Arci<system::LocalFileStorage>> storage_
+		);
+
+		[[nodiscard]] uptr<ResourceAccessor<Io<serialization::Archive>>, decltype([](auto*) {})> accessReadWrite(
+			_In_ mref<Arci<system::ArchiveStorage>> storage_
+		);
+
+		[[nodiscard]] uptr<ResourceAccessor<Io<const serialization::Archive>>, decltype([](auto*) {})> accessReadonly(
+			_In_ mref<Arci<system::ArchiveStorage>> storage_
+		);
+
+		[[nodiscard]] uptr<ResourceAccessor<Io<resource::Package>>, decltype([](auto*) {})> accessReadWrite(
+			_In_ mref<Arci<system::PackageStorage>> storage_
+		);
+
+		[[nodiscard]] uptr<ResourceAccessor<Io<const resource::Package>>, decltype([](auto*) {})> accessReadonly(
+			_In_ mref<Arci<system::PackageStorage>> storage_
+		);
 	};
 }
