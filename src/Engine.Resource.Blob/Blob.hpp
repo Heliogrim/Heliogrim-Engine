682f79479b6292f64616da1ddbfe0875f7c866f0
LtJulius <senator.spartas@googlemail.com>
2024-07-13 21:28:40 +0200

    Blob resource
    General:
    feat: added new cmake module to manage blob resource related functionality
    feat: added base class for blob objects
    feat: added naive implementation of blob using fixed size memory as backing storage
    feat: added native implementation of blob using filestream operations as backing storage

---
+++ src/Engine.Resource.Blob/Blob.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <span>
+
+#include <Engine.Common/Sal.hpp>
+#include <Engine.Common/Types.hpp>
+#include <Engine.Common/Wrapper.hpp>
+
+namespace hg::engine::resource {
+	class __declspec(novtable) Blob {
+	public:
+		constexpr virtual ~Blob() noexcept = default;
+
+	public:
+		virtual std::span<_::byte> read(_In_ streamoff offset_, _In_ mref<std::span<_::byte>> dst_) const = 0;
+
+		virtual std::span<_::byte> write(_In_ streamoff offset_, _In_ mref<std::span<_::byte>> src_) = 0;
+
+	public:
+		// Question: Which attributes and functions should be exposed by blob.
+		[[nodiscard]] constexpr virtual streamsize size() const noexcept {
+			return streamsize { -1LL };
+		};
+	};
+}
