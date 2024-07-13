77b1709e6a20ba082f30c8bf2db9390b48d95fe1
LtJulius <senator.spartas@googlemail.com>
2024-07-13 21:23:40 +0200

    Engine config init
    Editor:
    feat: added boot functions to initialize configs
    feat: added boot functions to initialize runtime configs
    feat: added support for windows environments to query for local appdata directories to store cache and user specific data

---
+++ src/Editor.Main/Boot/ConfigInit.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <Engine.Common/Sal.hpp>
+#include <Engine.Common/Wrapper.hpp>
+
+namespace hg::engine {
+	class Config;
+}
+
+namespace hg::editor::cfg {
+	class EditorProvider;
+}
+
+namespace hg::engine::cfg {
+	class ProjectProvider;
+}
+
+namespace hg::editor::boot {
+	void initProjectConfig(_In_ ref<engine::Config> cfg_, _Inout_ ref<engine::cfg::ProjectProvider> provider_);
+
+	void initEditorConfig(_In_ ref<engine::Config> cfg_, _Inout_ ref<cfg::EditorProvider> provider_);
+}
