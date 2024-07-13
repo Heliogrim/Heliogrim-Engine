747cd7968cb895484629e857c6383814a41b4b32
LtJulius <senator.spartas@googlemail.com>
2024-07-13 22:22:15 +0200

    Engine storage init
    General:
    feat: added boot functions to initialize storage
    feat: added boot functions to initialize runtime storage

---
+++ src/Editor.Main/Boot/StorageInit.cpp
@@ -0,0 +1,60 @@
+#include "StorageInit.hpp"
+
+#include <Engine.Common/Make.hpp>
+#include <Engine.Config/Config.hpp>
+#include <Engine.Config/Enums.hpp>
+#include <Engine.Storage/IStorageRegistry.hpp>
+#include <Engine.Storage/StorageModule.hpp>
+#include <Engine.Storage/Provider/LocalFileSystemProvider.hpp>
+#include <Engine.Storage/Repository/CacheFileSystemRepository.hpp>
+#include <Engine.Storage/Repository/EditorReadOnlyRepository.hpp>
+#include <Engine.Storage/Repository/ProjectFileSystemRepository.hpp>
+
+using namespace hg;
+
+void editor::boot::initStorage(cref<engine::Config> cfg_, ref<engine::StorageModule> module_) {
+
+	auto lfsOwned = make_uptr<engine::storage::system::LocalFileSystemProvider>();
+	auto lfs = lfsOwned.get();
+
+	/**/
+
+	auto& registry = *module_.getRegistry();
+	registry.addProvider(std::move(lfsOwned));
+
+	/**/
+
+	{
+		auto cachePath = cfg_.getTyped<String>(engine::cfg::ProjectConfigProperty::eLocalCachePath);
+		::hg::assertrt(cachePath.has_value() && cachePath.value().has_value());
+
+		registry.addRepository(lfs->makeCacheRepository(cachePath->value()));
+	}
+
+	/**/
+
+	{
+		auto projectPath = cfg_.getTyped<String>(engine::cfg::ProjectConfigProperty::eLocalBasePath);
+		::hg::assertrt(projectPath.has_value() && projectPath.value().has_value());
+
+		registry.addRepository(lfs->makeProjectRepository(projectPath->value()));
+	}
+
+	/**/
+
+	{
+		auto cachePath = cfg_.getTyped<String>(engine::cfg::EditorConfigProperty::eLocalCachePath);
+		::hg::assertrt(cachePath.has_value() && cachePath.value().has_value());
+
+		registry.addRepository(lfs->makeCacheRepository(cachePath->value()));
+	}
+
+	/**/
+
+	{
+		auto editorPath = cfg_.getTyped<String>(engine::cfg::EditorConfigProperty::eLocalEditorPath);
+		::hg::assertrt(editorPath.has_value() && editorPath.value().has_value());
+
+		registry.addRepository(lfs->makeEditorRepository(editorPath->value()));
+	}
+}
---
+++ src/Editor.Main/Boot/StorageInit.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <Engine.Common/Sal.hpp>
+#include <Engine.Common/Wrapper.hpp>
+
+namespace hg::engine {
+	class Config;
+	class StorageModule;
+}
+
+namespace hg::editor::boot {
+	void initStorage(_In_ cref<engine::Config> cfg_, _In_ ref<engine::StorageModule> module_);
+}
