4f95cf8905a132c86fa38deed48f574ff264fd89
LtJulius <senator.spartas@googlemail.com>
2024-07-15 22:45:14 +0200

    Added warning helper macros
    General:
    feat: added macro header to build helpers suppressing specific groups of assertions and warnings to enable low-level access without raising too much logging noise

---
+++ src/Engine.Common/__warning.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+/* Warning: General tool which will suppress all warnings...*/
+
+#if __clang__
+#define START_SUPPRESS_WARNINGS \
+_Pragma("clang diagnostic push") \
+_Pragma("clang diagnostic ignored \"-Wall\"") \
+_Pragma("clang diagnostic ignored \"-Weverything\"")
+#define STOP_SUPPRESS_WARNINGS \
+_Pragma("clang diagnostic pop")
+#elif defined(_MSC_VER)
+#define START_SUPPRESS_WARNINGS \
+__pragma(warning(push, 0))
+#define STOP_SUPPRESS_WARNINGS \
+__pragma(warning(pop))
+#else
+#define START_SUPPRESS_WARNINGS
+#define STOP_SUPPRESS_WARNINGS
+#endif
+
+/* Purposely bound warning helpers... */
+
+#if __clang__
+#define BEGIN_REGION_UNCHECKED_LLVL \
+_Pragma("clang diagnostic push") \
+_Pragma("clang diagnostic ignored \"-Wunsafe-buffer-usage\"")
+#define END_REGION_UNCHECKED_LLVL \
+_Pragma("clang diagnostic pop")
+#elif defined(_MSC_VER)
+#define BEGIN_REGION_UNCHECKED_LLVL \
+__pragma(warning(push)) \
+__pragma(warning(disable: 26481))
+#define END_REGION_UNCHECKED_LLVL \
+__pragma(warning(pop))
+#else
+#define BEGIN_REGION_UNCHECKED_LLVL
+#define END_REGION_UNCHECKED_LLVL
+#endif
+
+#define GUARD_UNCHECKED_LLVL(expr) \
+BEGIN_REGION_UNCHECKED_LLVL \
+expr \
+END_REGION_UNCHECKED_LLVL
