e90b4e170600dd046137b13e040cb6ea7eaacf0c
LtJulius <senator.spartas@googlemail.com>
2024-07-22 22:03:37 +0200

    Added default mesh definitions and resolver
    General:
    feat: added static default mesh guids and forwarding resolvers

---
+++ src/Editor.Assets.Default/Meshes/Default.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <Engine.Assets/Types/Asset.hpp>
+#include <Engine.Assets/Types/AssetConcept.hpp>
+#include <Engine.Assets/Types/Geometry/StaticGeometry.hpp>
+#include <Engine.Core/Engine.hpp>
+#include <Engine.Reflect/Cast.hpp>
+
+namespace hg::editor::assets::meshes {
+	/**/
+
+	[[nodiscard]] extern nmpt<engine::assets::Asset> resolveRuntimeAsset(
+		_In_ ref<engine::Engine> engine_,
+		_In_ cref<asset_guid> guid_
+	);
+
+	template <engine::assets::IsAsset AssetType_>
+	[[nodiscard]] nmpt<AssetType_> resolveRuntimeAsset(cref<asset_guid> guid_) {
+		// TODO: Make type safe casts...
+		return static_cast<ptr<AssetType_>>(resolveRuntimeAsset(*engine::Engine::getEngine(), guid_).get());
+	}
+
+	/**/
+
+	// @formatter:off
+	constexpr auto default_sphere_guid = asset_guid { 2327095154, 11665, 18946, 4311640585827641743uLL };
+	inline auto get_default_sphere() { return resolveRuntimeAsset<engine::assets::StaticGeometry>(default_sphere_guid); }
+	// @formatter:on
+}
