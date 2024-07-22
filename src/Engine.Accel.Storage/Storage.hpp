70eda076081b8e742dfdf9ca1f76f53fc2f8a0e0
LtJulius <senator.spartas@googlemail.com>
2024-07-22 21:27:30 +0200

    Naive acceleration effect state hashes
    General:
    feat: added naive implementation of hash functions to sign acceleraction effects and related components
    feat: changed the permutation set to be immutable

--- src/Engine.Accel.Storage/Storage.hpp
+++ src/Engine.Accel.Storage/Storage.hpp
@@ -8,106 +8,106 @@
 #include "HashedState.hpp"
 
 namespace hg::engine::accel {
-    class EffectProfile;
-    class EffectSpecification;
-    class AccelerationPipeline;
-    class Stage;
-    class AccelerationEffect;
+	class EffectProfile;
+	class EffectSpecification;
+	class AccelerationPipeline;
+	class Stage;
+	class AccelerationEffect;
 }
 
 namespace hg::engine::accel {
-    struct Permutation {
-        nmpt<const EffectProfile> profile;
-        nmpt<const EffectSpecification> specification;
-    };
+	struct Permutation {
+		const nmpt<const EffectProfile> profile;
+		const nmpt<const EffectSpecification> specification;
+	};
 }
 
 namespace std {
-    template <typename Type_>
-    struct hash<::hg::engine::accel::HashedState<Type_>> {
-        [[nodiscard]] constexpr size_t operator()(
-            const ::hg::engine::accel::HashedState<Type_>& value_
-        ) const noexcept {
-            return value_.value;
-        }
-    };
+	template <typename Type_>
+	struct hash<::hg::engine::accel::HashedState<Type_>> {
+		[[nodiscard]] constexpr size_t operator()(
+			const ::hg::engine::accel::HashedState<Type_>& value_
+		) const noexcept {
+			return value_.value;
+		}
+	};
 }
 
 namespace hg::engine::accel {
-    class __declspec(novtable) AccelStorage {
-    public:
-        using this_type = AccelStorage;
+	class __declspec(novtable) AccelStorage {
+	public:
+		using this_type = AccelStorage;
 
-    public:
-        constexpr AccelStorage() noexcept = default;
+	public:
+		constexpr AccelStorage() noexcept = default;
 
-        constexpr virtual ~AccelStorage() noexcept = default;
+		constexpr virtual ~AccelStorage() noexcept = default;
 
-    public:
-        [[nodiscard]] virtual bool hasAccelStage(
-            cref<HashedState<Stage>> hashedState_
-        ) noexcept = 0;
+	public:
+		[[nodiscard]] virtual bool hasAccelStage(
+			cref<HashedState<Stage>> hashedState_
+		) noexcept = 0;
 
-        [[nodiscard]] virtual bool hasAccelStage(
-            cref<smr<const Stage>> stage_
-        ) noexcept = 0;
+		[[nodiscard]] virtual bool hasAccelStage(
+			cref<smr<const Stage>> stage_
+		) noexcept = 0;
 
-        [[nodiscard]] virtual smr<const Stage> getAccelStage(
-            cref<HashedState<Stage>> hashedState_
-        ) noexcept = 0;
+		[[nodiscard]] virtual smr<const Stage> getAccelStage(
+			cref<HashedState<Stage>> hashedState_
+		) noexcept = 0;
 
-        [[nodiscard]] virtual smr<const Stage> insertAccelStage(
-            cref<HashedState<Stage>> hashedState_,
-            mref<smr<const Stage>> stage_
-        ) noexcept = 0;
+		[[nodiscard]] virtual smr<const Stage> insertAccelStage(
+			cref<HashedState<Stage>> hashedState_,
+			mref<smr<const Stage>> stage_
+		) noexcept = 0;
 
-    public:
-        [[nodiscard]] virtual bool hasAccelEffect(
-            cref<HashedState<AccelerationEffect>> hashedState_
-        ) noexcept = 0;
+	public:
+		[[nodiscard]] virtual bool hasAccelEffect(
+			cref<HashedState<AccelerationEffect>> hashedState_
+		) noexcept = 0;
 
-        [[nodiscard]] virtual bool hasAccelEffect(
-            cref<Guid> effectGuid_
-        ) noexcept = 0;
+		[[nodiscard]] virtual bool hasAccelEffect(
+			cref<Guid> effectGuid_
+		) noexcept = 0;
 
-        [[nodiscard]] virtual bool hasAccelEffect(
-            cref<smr<const AccelerationEffect>> effect_
-        ) noexcept = 0;
+		[[nodiscard]] virtual bool hasAccelEffect(
+			cref<smr<const AccelerationEffect>> effect_
+		) noexcept = 0;
 
-        [[nodiscard]] virtual smr<const AccelerationEffect> getAccelEffect(
-            cref<Guid> effectGuid_
-        ) noexcept = 0;
+		[[nodiscard]] virtual smr<const AccelerationEffect> getAccelEffect(
+			cref<Guid> effectGuid_
+		) noexcept = 0;
 
-        [[nodiscard]] virtual smr<const AccelerationEffect> getAccelEffect(
-            cref<HashedState<AccelerationEffect>> hashedState_
-        ) noexcept = 0;
+		[[nodiscard]] virtual smr<const AccelerationEffect> getAccelEffect(
+			cref<HashedState<AccelerationEffect>> hashedState_
+		) noexcept = 0;
 
-        [[nodiscard]] virtual smr<const AccelerationEffect> insertAccelEffect(
-            cref<HashedState<AccelerationEffect>> hashedState_,
-            mref<smr<const AccelerationEffect>> effect_
-        ) noexcept = 0;
+		[[nodiscard]] virtual smr<const AccelerationEffect> insertAccelEffect(
+			cref<HashedState<AccelerationEffect>> hashedState_,
+			mref<smr<const AccelerationEffect>> effect_
+		) noexcept = 0;
 
-    public:
-        [[nodiscard]] virtual bool hasAccelPipeline(
-            cref<HashedState<AccelerationPipeline>> hashedState_
-        ) noexcept = 0;
+	public:
+		[[nodiscard]] virtual bool hasAccelPipeline(
+			cref<HashedState<AccelerationPipeline>> hashedState_
+		) noexcept = 0;
 
-        [[nodiscard]] virtual bool hasAccelPipeline(
-            cref<smr<const AccelerationPipeline>> pipeline_
-        ) noexcept = 0;
+		[[nodiscard]] virtual bool hasAccelPipeline(
+			cref<smr<const AccelerationPipeline>> pipeline_
+		) noexcept = 0;
 
-        [[nodiscard]] virtual smr<const AccelerationPipeline> getAccelPipeline(
-            cref<HashedState<AccelerationPipeline>> hashedPipeline_
-        ) noexcept = 0;
+		[[nodiscard]] virtual smr<const AccelerationPipeline> getAccelPipeline(
+			cref<HashedState<AccelerationPipeline>> hashedPipeline_
+		) noexcept = 0;
 
-        [[nodiscard]] virtual smr<const AccelerationPipeline> getAccelPipeline(
-            cref<HashedState<Permutation>> hashedPermutation_,
-            cref<HashedState<AccelerationEffect>> hashedEffect_
-        ) noexcept = 0;
+		[[nodiscard]] virtual smr<const AccelerationPipeline> getAccelPipeline(
+			cref<HashedState<Permutation>> hashedPermutation_,
+			cref<HashedState<AccelerationEffect>> hashedEffect_
+		) noexcept = 0;
 
-        [[nodiscard]] virtual smr<const AccelerationPipeline> insertAccelPipeline(
-            cref<Permutation> permutation_,
-            mref<smr<const AccelerationPipeline>> pipeline_
-        ) noexcept = 0;
-    };
+		[[nodiscard]] virtual smr<const AccelerationPipeline> insertAccelPipeline(
+			cref<Permutation> permutation_,
+			mref<smr<const AccelerationPipeline>> pipeline_
+		) noexcept = 0;
+	};
 }
