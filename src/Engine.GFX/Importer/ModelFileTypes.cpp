6ac4a1020918cca20308932d20f4574c7dc81b9b
LtJulius <senator.spartas@googlemail.com>
2024-07-22 21:49:51 +0200

    Added model file type list
    General:
    feat: added authored list of model file types to iterate

--- src/Engine.GFX/Importer/ModelFileTypes.cpp
+++ src/Engine.GFX/Importer/ModelFileTypes.cpp
@@ -1,7 +1,18 @@
 #include "ModelFileTypes.hpp"
 
+#include <array>
+
 using namespace hg::engine::gfx;
 using namespace hg;
 
 const engine::res::FileTypeId ModelFileType::Fbx { "fbx"_typeId, ".fbx" };
 const engine::res::FileTypeId ModelFileType::Obj { "obj"_typeId, ".obj" };
+
+static auto modelFileTypes = std::array {
+	ModelFileType::Fbx,
+	ModelFileType::Obj
+};
+
+std::span<const engine::res::FileTypeId> ModelFileType::asSpan() noexcept {
+	return modelFileTypes;
+}
