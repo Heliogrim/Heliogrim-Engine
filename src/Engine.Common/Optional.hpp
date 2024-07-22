02f3e950d9dc3e489f71d9976ebeea0384f512cf
LtJulius <senator.spartas@googlemail.com>
2024-07-22 21:19:44 +0200

    Added experimental optional class extending tl::optional
    General:
    feat: added a new optional class using the tl::optional implementation as extendable base
    feat: added factory types and functions to generate optional objects

--- src/Engine.Common/Optional.hpp
+++ src/Engine.Common/Optional.hpp
@@ -5,4 +5,30 @@
 namespace hg {
 	template <typename Ty_>
 	using Optional = tl::optional<Ty_>;
+
+	template <class Ty_>
+	class Opt :
+		public tl::optional<Ty_> {
+	public:
+		using this_type = Opt<Ty_>;
+		using underlying_type = tl::optional<Ty_>;
+
+	public:
+		using underlying_type::optional;
+
+		template <typename Uy_>
+		constexpr Opt(Uy_&& other_) noexcept :
+			underlying_type(std::forward<Uy_>(other_)) {}
+	};
+
+	template <typename Ty_>
+	Opt(Ty_&&) -> Opt<Ty_>;
+
+	/**/
+
+	constexpr static auto None = tl::nullopt;
+
+	[[nodiscard]] constexpr static decltype(auto) Some(auto&& args_) noexcept {
+		return Opt(std::forward<decltype(args_)>(args_));
+	}
 }
