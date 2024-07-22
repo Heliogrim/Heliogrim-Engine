b52e3ef6eb3a823d808b849d7ee32fc90a054b33
LtJulius <senator.spartas@googlemail.com>
2024-07-22 21:49:34 +0200

    Added image file type list
    General:
    feat: added authored list of image file types to iterate

--- src/Engine.GFX/Importer/ImageFileTypes.cpp
+++ src/Engine.GFX/Importer/ImageFileTypes.cpp
@@ -1,5 +1,7 @@
 #include "ImageFileTypes.hpp"
 
+#include <array>
+
 using namespace hg::engine::gfx;
 using namespace hg;
 
@@ -9,3 +11,16 @@ const engine::res::FileTypeId ImageFileType::Jpeg { "jpeg"_typeId, ".jpg" };
 const engine::res::FileTypeId ImageFileType::Ktx { "ktx"_typeId, ".ktx" };
 const engine::res::FileTypeId ImageFileType::Ktx2 { "ktx2"_typeId, ".ktx2" };
 const engine::res::FileTypeId ImageFileType::Png { "png"_typeId, ".png" };
+
+static auto imageFileTypes = std::array {
+	ImageFileType::Exr,
+	ImageFileType::Hdr,
+	ImageFileType::Jpeg,
+	ImageFileType::Ktx,
+	ImageFileType::Ktx2,
+	ImageFileType::Png,
+};
+
+std::span<const engine::res::FileTypeId> ImageFileType::asSpan() noexcept {
+	return imageFileTypes;
+}
