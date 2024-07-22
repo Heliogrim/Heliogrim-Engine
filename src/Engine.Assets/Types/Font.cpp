d3c92d6fa8c7f95fffc718f5b338ab545b010f48
LtJulius <senator.spartas@googlemail.com>
2024-07-22 21:33:24 +0200

    Added font serialization support
    General:
    feat: added font structure serialization support

--- src/Engine.Assets/Types/Font.cpp
+++ src/Engine.Assets/Types/Font.cpp
@@ -3,13 +3,16 @@
 using namespace hg::engine::assets;
 using namespace hg;
 
+Font::Font() :
+	InheritMeta(clone(invalid_asset_guid), Font::typeId) {}
+
 Font::Font(mref<asset_guid> guid_) :
-    InheritMeta(std::move(guid_), Font::typeId) {}
+	InheritMeta(std::move(guid_), Font::typeId) {}
 
 Font::Font(mref<asset_guid> guid_, mref<Vector<fs::Url>> sources_) :
-    InheritMeta(std::move(guid_), Font::typeId),
-    _sources(std::move(sources_)) {}
+	InheritMeta(std::move(guid_), Font::typeId),
+	_sources(std::move(sources_)) {}
 
 cref<Vector<fs::Url>> Font::sources() const noexcept {
-    return _sources;
+	return _sources;
 }
