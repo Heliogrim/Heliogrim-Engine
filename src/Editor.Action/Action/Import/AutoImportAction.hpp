a4448618bc0decdf149a5383a490196b295d50c9
LtJulius <senator.spartas@googlemail.com>
2024-07-22 21:58:18 +0200

    Added editor AutoImportAction
    General:
    feat: added headless import action which will run upon editor booting to import required assets

---
+++ src/Editor.Action/Action/Import/AutoImportAction.hpp
@@ -0,0 +1,57 @@
+#pragma once
+#include <Engine.Assets/Types/Asset.hpp>
+#include <Engine.Common/Collection/Vector.hpp>
+#include <Engine.Common/Managed/Rc.hpp>
+#include <Engine.Storage/Url/FileUrl.hpp>
+
+#include "ImportAction.hpp"
+
+namespace hg::editor {
+	class AutoImportAction :
+		public ImportAction {
+	public:
+		AutoImportAction() noexcept;
+
+		AutoImportAction(_In_ mref<engine::storage::FileUrl> importFile_) noexcept;
+
+		~AutoImportAction() override;
+
+	private:
+		engine::storage::FileUrl _importFile;
+		Vector<Arci<engine::assets::Asset>> _generatedAssets;
+
+	public:
+		[[nodiscard]] engine::storage::FileUrl getImportFile() const noexcept;
+
+		[[nodiscard]] cref<decltype(_generatedAssets)> generatedAssets() const noexcept;
+
+	private:
+		std::atomic_flag _running;
+		std::atomic_flag _finished;
+		bool _failed;
+
+	private:
+		void setRunning();
+
+		void setFinished();
+
+	public:
+		[[nodiscard]] bool isReversible() const noexcept override;
+
+		[[nodiscard]] bool isPending() const noexcept override;
+
+		[[nodiscard]] bool isRunning() const noexcept override;
+
+		[[nodiscard]] bool isFinished() const noexcept override;
+
+	public:
+		void apply() override;
+
+		void reverse() override;
+
+	public:
+		[[nodiscard]] operator ptr<await_signal_sub_type>() const noexcept override;
+
+		[[nodiscard]] bool failed() const noexcept override;
+	};
+}
