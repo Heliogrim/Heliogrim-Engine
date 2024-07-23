c6e810308ec750fcf351058fbc41bd997b2fce81
LtJulius <senator.spartas@googlemail.com>
2024-07-23 15:02:54 +0200

    Added portable unfancy and memory ranged destroy function
    General:
    feat: added unfancy free function to access the address of an possible smart pointer type
    feat: added a ranges destroy using a pointer range and an associated allocator

---
+++ src/Engine.Common/Memory/Destroy.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Address.hpp"
+
+namespace hg {
+	/* Note: This is a straight copy from the msvc xmemory header... */
+
+	template <class Allocator_, typename ValueType_>
+	concept AllocIsDefault = std::is_same_v<std::allocator<ValueType_>, Allocator_>;
+
+	template <class Allocator_, typename PointerType_>
+	concept AllocHasDestroy = requires(Allocator_& alloc_) {
+		{ alloc_.destory(std::declval<PointerType_>()) };
+	};
+
+	template <class Allocator_>
+	constexpr void destroy_range(
+		typename std::allocator_traits<Allocator_>::pointer first_,
+		const typename std::allocator_traits<Allocator_>::pointer last_,
+		Allocator_& alloc_
+	) noexcept {
+		using value_type = typename Allocator_::value_type;
+		if constexpr (not(
+			std::is_trivially_destructible_v<value_type> &&
+			(AllocIsDefault<Allocator_, value_type> || not AllocHasDestroy<Allocator_, value_type*>)
+		)) {
+			for (; first_ != last_; ++first_) {
+				std::allocator_traits<Allocator_>::destroy(alloc_, ::hg::unfancy(first_));
+			}
+		}
+	}
+}
