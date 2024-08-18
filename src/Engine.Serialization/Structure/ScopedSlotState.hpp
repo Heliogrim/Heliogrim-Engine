8764146ca7eae85b137d6b654125322e735c4f21
LtJulius <senator.spartas@googlemail.com>
2024-08-18 17:05:19 +0200

    Fix missing type_traits within structured serialization
    General:
    fix: added missing type_trait header

--- src/Engine.Serialization/Structure/ScopedSlotState.hpp
+++ src/Engine.Serialization/Structure/ScopedSlotState.hpp
@@ -1,25 +1,25 @@
 #pragma once
 
+#include <Engine.Common/Flag.hpp>
 #include <Engine.Common/Types.hpp>
 #include <Engine.Common/Wrapper.hpp>
-#include <Engine.Common/Flag.hpp>
 
 namespace hg::engine::serialization {
-    struct StructureSlotState;
-    class StructureSlotBase;
+	struct StructureSlotState;
+	class StructureSlotBase;
 }
 
 namespace hg::engine::serialization {
-    enum class ScopedSlotStateFlag : bool {
-        eUndefined = false,
-        /**/
-        eClean = eUndefined,
-        eDirty = true
-    };
+	enum class ScopedSlotStateFlag : bool {
+		eUndefined = false,
+		/**/
+		eClean = eUndefined,
+		eDirty = true
+	};
 
-    typedef Flag<ScopedSlotStateFlag, bool> ScopedSlotStateFlags;
+	typedef Flag<ScopedSlotStateFlag, bool> ScopedSlotStateFlags;
 
-    struct ScopedSlotState {
-        ScopedSlotStateFlags flags;
-    };
+	struct ScopedSlotState {
+		ScopedSlotStateFlags flags;
+	};
 }
