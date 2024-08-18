404c788ab8f86b042a6d5f5c308d26bb0cb00bde
LtJulius <senator.spartas@googlemail.com>
2024-08-18 17:03:42 +0200

    Workaround for missing std::atomic<std::shared_ptr<T>> support
    General:
    feat: replaced usage of atomic shared instance of a work set with a uniquely handled instance

--- src/Engine.Scheduler/Process/Schedule.cpp
+++ src/Engine.Scheduler/Process/Schedule.cpp
@@ -1,6 +1,6 @@
 #include "Schedule.hpp"
 
-#include <cassert>
+#include <Engine.Asserts/Asserts.hpp>
 #include <Engine.Common/Make.hpp>
 #include <Engine.Logging/Logger.hpp>
 
@@ -10,101 +10,74 @@ using namespace hg::engine::scheduler;
 using namespace hg;
 
 Schedule::Schedule() :
-    _workSets() {}
+	_workSets() {}
 
 Schedule::~Schedule() {
-    tidy();
+	tidy();
 }
 
 void Schedule::setup() {
-
-    for (auto&& atomic : _workSets) {
-
-        auto nextSet = make_sptr<FifoWorkSet>();
-        auto workSet = atomic.exchange(std::move(nextSet), std::memory_order_acq_rel);
-
-        assert(not workSet);
-    }
-
+	for (auto& workSet : _workSets) {
+		::hg::assertd(not workSet);
+		workSet = make_uptr<FifoWorkSet>();
+	}
 }
 
 void Schedule::tick() {
-    transfer(task::TaskMask::eHigher, task::TaskMask::eCritical);
-    transfer(task::TaskMask::eNormal, task::TaskMask::eHigher);
-    transfer(task::TaskMask::eLower, task::TaskMask::eNormal);
+	transfer(task::TaskMask::eHigher, task::TaskMask::eCritical);
+	transfer(task::TaskMask::eNormal, task::TaskMask::eHigher);
+	transfer(task::TaskMask::eLower, task::TaskMask::eNormal);
 }
 
 void Schedule::tidy() {
-
-    for (auto&& atomic : _workSets) {
-
-        auto workSet = atomic.exchange(nullptr, std::memory_order_acq_rel);
-        if (not workSet) {
-            continue;
-        }
-
-        if (workSet.use_count() != 1) {
-            IM_CORE_ERRORF(
-                "Schedule is cleaning up but encountered shared usage of workset `{:x}` with `{}` references.",
-                reinterpret_cast<uintptr_t>(workSet.get()),
-                workSet.use_count()
-            );
-        }
-        workSet.reset();
-    }
-
+	for (auto&& workSet : _workSets) {
+		workSet.reset();
+	}
 }
 
 u64 Schedule::maskToPriority(const task::TaskMask mask_) const noexcept {
-    if (mask_ & task::TaskMask::eCritical) {
-        return 0uLL;
+	if (mask_ & task::TaskMask::eCritical) {
+		return 0uLL;
 
-    } else if (mask_ & task::TaskMask::eHigher) {
-        return 1uLL;
+	} else if (mask_ & task::TaskMask::eHigher) {
+		return 1uLL;
 
-    } else if (mask_ & task::TaskMask::eNormal) {
-        return 2uLL;
+	} else if (mask_ & task::TaskMask::eNormal) {
+		return 2uLL;
 
-    } else if (mask_ & task::TaskMask::eLower) {
-        return 3uLL;
+	} else if (mask_ & task::TaskMask::eLower) {
+		return 3uLL;
 
-    } else {
-        // Default to normal priority
-        return 2uLL;
-    }
+	} else {
+		// Default to normal priority
+		return 2uLL;
+	}
 }
 
 void Schedule::transfer(const task::TaskMask src_, const task::TaskMask dst_) {
 
-    const auto src = maskToPriority(src_);
-    const auto dst = maskToPriority(dst_);
+	const auto src = maskToPriority(src_);
+	const auto dst = maskToPriority(dst_);
 
-    const auto srcSet = _workSets[src].load(std::memory_order_consume);
-    const auto dstSet = _workSets[dst].load(std::memory_order_consume);
-
-    srcSet->transfer(dstSet.get());
+	_workSets[src]->transfer(_workSets[dst].get());
 }
 
 bool Schedule::push(const non_owning_rptr<const task::TaskDelegate> task_) {
 
-    const auto mask = task_->mask();
-    const u64 priorityIdx = maskToPriority(mask);
+	const auto mask = task_->mask();
+	const u64 priorityIdx = maskToPriority(mask);
 
-    const auto workSet = _workSets[priorityIdx].load(std::memory_order_consume);
-    return workSet->push(task_);
+	return _workSets[priorityIdx]->push(task_);
 }
 
 bool Schedule::pop(const task::TaskMask mask_, ref<non_owning_rptr<const task::TaskDelegate>> task_) {
 
-    u64 priorityIdx = maskToPriority(mask_);
-    for (; priorityIdx < priority_count; ++priorityIdx) {
+	u64 priorityIdx = maskToPriority(mask_);
+	for (; priorityIdx < priority_count; ++priorityIdx) {
+		if (_workSets[priorityIdx]->pop(mask_, task_)) {
+			return true;
+		}
+	}
 
-        const auto workSet = _workSets[priorityIdx].load(std::memory_order_consume);
-        if (workSet->pop(mask_, task_)) {
-            return true;
-        }
-
-    }
-
-    return false;
+	return false;
 }
