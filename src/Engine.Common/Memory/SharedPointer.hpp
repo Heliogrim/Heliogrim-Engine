385c4e19ea658130b2f9e4cb645e288454693066
LtJulius <senator.spartas@googlemail.com>
2024-08-26 16:20:17 +0200

    Added std::shared_ptr and std::unique_ptr typename alias
    General:
    feat: added new aliases for shared_ptr and unique_ptr to match naming convention

--- src/Engine.Common/Memory/SharedPointer.hpp
+++ src/Engine.Common/Memory/SharedPointer.hpp
@@ -6,19 +6,29 @@
 #include "../__macro.hpp"
 
 namespace hg {
-    template <typename To_, typename From_>
-    struct into_impl<std::shared_ptr<To_>, std::shared_ptr<From_>> {
-        [[nodiscard]] constexpr std::shared_ptr<To_> operator()(std::shared_ptr<From_>&& obj_) noexcept {
-            return std::shared_ptr<To_> { std::move(obj_) };
-        }
-    };
+	template <typename Ty_>
+	using SharedPtr = std::shared_ptr<Ty_>;
 
-    /**/
+	template <typename Ty_>
+	using WeakPtr = std::weak_ptr<Ty_>;
+}
 
-    template <typename To_, typename From_>
-    struct into_impl<std::shared_ptr<To_>, std::unique_ptr<From_>> {
-        [[nodiscard]] constexpr std::shared_ptr<To_> operator()(std::unique_ptr<From_>&& obj_) noexcept {
-            return std::shared_ptr<To_> { std::move(obj_) };
-        }
-    };
+/**/
+
+namespace hg {
+	template <typename To_, typename From_>
+	struct into_impl<std::shared_ptr<To_>, std::shared_ptr<From_>> {
+		[[nodiscard]] constexpr std::shared_ptr<To_> operator()(std::shared_ptr<From_>&& obj_) noexcept {
+			return std::shared_ptr<To_> { std::move(obj_) };
+		}
+	};
+
+	/**/
+
+	template <typename To_, typename From_>
+	struct into_impl<std::shared_ptr<To_>, std::unique_ptr<From_>> {
+		[[nodiscard]] constexpr std::shared_ptr<To_> operator()(std::unique_ptr<From_>&& obj_) noexcept {
+			return std::shared_ptr<To_> { std::move(obj_) };
+		}
+	};
 }
--- src/Engine.Common/Memory/UniquePointer.hpp
+++ src/Engine.Common/Memory/UniquePointer.hpp
@@ -6,10 +6,17 @@
 #include "../__macro.hpp"
 
 namespace hg {
-    template <typename To_, typename From_>
-    struct into_impl<std::unique_ptr<To_>, std::unique_ptr<From_>> {
-        [[nodiscard]] constexpr std::unique_ptr<To_> operator()(std::unique_ptr<From_>&& obj_) noexcept {
-            return std::unique_ptr<To_> { std::move(obj_) };
-        }
-    };
+	template <typename Ty_, class Dy_ = ::std::default_delete<Ty_>>
+	using UniquePtr = std::unique_ptr<Ty_, Dy_>;
+}
+
+/**/
+
+namespace hg {
+	template <typename To_, typename From_>
+	struct into_impl<std::unique_ptr<To_>, std::unique_ptr<From_>> {
+		[[nodiscard]] constexpr std::unique_ptr<To_> operator()(std::unique_ptr<From_>&& obj_) noexcept {
+			return std::unique_ptr<To_> { std::move(obj_) };
+		}
+	};
 }
