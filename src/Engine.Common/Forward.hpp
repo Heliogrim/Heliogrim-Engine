fec0c17eabe23f93669d6e1e8926573b3467eeca
LtJulius <senator.spartas@googlemail.com>
2024-09-28 15:44:49 +0200

    fix: warning for ill-formed signature when using intrinsic attribute macro

--- src/Engine.Common/Forward.hpp
+++ src/Engine.Common/Forward.hpp
@@ -5,12 +5,12 @@
 
 namespace hg {
 	template <typename Type_>
-	[[nodiscard]] macro_attr_intr constexpr Type_&& forward(::hg::meta::remove_reference_t<Type_>& ref_) noexcept {
+	[[nodiscard, macro_attr_intr]] constexpr Type_&& forward(::hg::meta::remove_reference_t<Type_>& ref_) noexcept {
 		return static_cast<Type_&&>(ref_);
 	}
 
 	template <typename Type_>
-	[[nodiscard]] macro_attr_intr constexpr Type_&& forward(::hg::meta::remove_reference_t<Type_>&& ref_) noexcept {
+	[[nodiscard, macro_attr_intr]] constexpr Type_&& forward(::hg::meta::remove_reference_t<Type_>&& ref_) noexcept {
 		return static_cast<Type_&&>(ref_);
 	}
 }
