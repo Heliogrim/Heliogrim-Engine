c048fcd489bf1fff988e0b12f89d849bc05dd702
LtJulius <senator.spartas@googlemail.com>
2024-09-28 15:59:27 +0200

    feat: added result<T, E> with expected<T> and unexpected<E> helper types

---
+++ src/Engine.Common/Expected.hpp
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <tl/expected.hpp>
+
+namespace hg {
+	/**/
+	template <typename Type_>
+	struct Expected;
+
+	template <typename Error_>
+	struct Unexpected;
+
+	/**/
+
+	template <typename Type_, typename Error_>
+	class Result :
+		public tl::expected<Type_, Error_> {
+	public:
+		using this_type = Result<Type_, Error_>;
+		using underlying_type = tl::expected<Type_, Error_>;
+
+	public:
+		using underlying_type::expected;
+
+	public:
+		constexpr Result(Expected<Type_>&& expected_) noexcept :// NOLINT(*-explicit-constructor)
+			underlying_type(std::move(expected_.value)) {}
+
+		constexpr Result(Unexpected<Error_>&& unexpected_) noexcept:// NOLINT(*-explicit-constructor)
+			underlying_type(::tl::unexpected { std::move(unexpected_.error) }) {}
+
+	public:
+		constexpr static decltype(auto) expected(auto&&... args_) noexcept
+			requires std::is_constructible_v<this_type, decltype(args_)...> {
+			return this_type { std::forward<decltype(args_)>(args_)... };
+		}
+
+		constexpr static decltype(auto) unexpected(auto&&... error_) noexcept
+			requires std::is_constructible_v<Error_, decltype(error_)...> {
+			return this_type { ::tl::make_unexpected(std::forward<decltype(error_)>(error_)...) };
+		}
+	};
+
+	/**/
+
+	template <typename Type_>
+	struct Expected {
+		Type_ value;
+
+		constexpr explicit Expected(auto&&... args_) noexcept :
+			value(std::forward<decltype(args_)>(args_)...) {}
+	};
+
+	template <>
+	struct Expected<void> {
+		constexpr explicit Expected(auto&&...) noexcept {}
+	};
+
+	template <typename Error_>
+	struct Unexpected {
+		Error_ error;
+
+		constexpr explicit Unexpected(auto&&... args_) noexcept :
+			error(std::forward<decltype(args_)>(args_)...) {}
+	};
+
+	/**/
+
+	template <typename Type_>
+	Expected(Type_&&) -> Expected<Type_>;
+
+	template <typename Error_>
+	Unexpected(Error_&&) -> Unexpected<std::decay_t<Error_>>;
+}
