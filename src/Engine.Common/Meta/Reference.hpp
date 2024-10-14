6e39fe9386b1414a502d024d8423a59cd8bb23b8
LtJulius <senator.spartas@googlemail.com>
2024-10-14 23:35:15 +0200

    refactor: renamed meta reference struct 'remove' -> 'drop'

--- src/Engine.Common/Meta/Reference.hpp
+++ src/Engine.Common/Meta/Reference.hpp
@@ -2,20 +2,23 @@
 
 namespace hg::meta {
 	template <typename Type_>
-	struct remove_ref {
+	struct drop_ref {
 		using type = Type_;
 	};
 
 	template <typename Type_>
-	struct remove_ref<Type_&> {
+	struct drop_ref<Type_&> {
 		using type = Type_;
 	};
 
 	template <typename Type_>
-	struct remove_ref<Type_&&> {
+	struct drop_ref<Type_&&> {
 		using type = Type_;
 	};
 
 	template <typename Type_>
-	using remove_reference_t = typename remove_ref<Type_>::type;
+	using remove_reference_t = typename drop_ref<Type_>::type;
+
+	template <typename Type_>
+	using drop_ref_t = typename drop_ref<Type_>::type;
 }
