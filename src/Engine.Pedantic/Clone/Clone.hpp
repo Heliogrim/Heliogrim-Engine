bb89da0488c1dd936fbecf1e84bb69b8e5f0bbc0
LtJulius <senator.spartas@googlemail.com>
2024-10-14 23:47:32 +0200

    feat: added clonable concept

--- src/Engine.Pedantic/Clone/Clone.hpp
+++ src/Engine.Pedantic/Clone/Clone.hpp
@@ -4,21 +4,30 @@
 #include <type_traits>
 #include <Engine.Common/Make.hpp>
 #include <Engine.Common/Wrapper.hpp>
+#include <Engine.Common/Meta/Type.hpp>
 
 namespace hg {
 	template <typename Type_>
 	struct clone_impl;
 
-	template <typename Type_>
-	concept is_clonable = std::false_type::value;
-
-	/*
-	[[nodiscard]] auto clone(const auto& val_) {
-	    return val_;
-	}
-	*/
+	/**/
 
 	template <typename Type_>
+	struct is_clonable {
+		constexpr static auto value = std::is_copy_constructible_v<Type_>;
+	};
+
+	template <typename Type_, typename Dy_>
+	struct is_clonable<UniquePtr<Type_, Dy_>> {
+		constexpr static auto value = std::is_copy_constructible_v<Type_>;
+	};
+
+	template <typename Type_>
+	concept Clonable = CompleteType<Type_> && is_clonable<Type_>::value;
+
+	/**/
+
+	template <Clonable Type_>
 	[[nodiscard]] constexpr Type_ clone(const Type_& val_) {
 		return clone_impl<Type_> {}(val_);
 	}
