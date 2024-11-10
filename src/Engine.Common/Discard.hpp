201198a07829a2fb398b54497edb9aa3c4c9c31f
LtJulius <senator.spartas@googlemail.com>
2024-11-10 17:25:27 +0100

    fix: corrected call operator signature for discarding

--- src/Engine.Common/Discard.hpp
+++ src/Engine.Common/Discard.hpp
@@ -1,10 +1,12 @@
 #pragma once
 
+#include <type_traits>
+
 namespace hg {
 	namespace {
 		struct DiscardFn {
 			template <typename Type_> requires std::is_move_constructible_v<Type_> && std::is_destructible_v<Type_>
-			[[maybe_unused]] constexpr DiscardFn& operator=([[maybe_unused]] Type_&& obj_) noexcept {
+			[[maybe_unused]] constexpr const DiscardFn& operator=([[maybe_unused]] Type_&& obj_) const noexcept {
 				[[maybe_unused]] Type_ disposed { std::forward<Type_>(obj_) };
 				return *this;
 			}
