39b9a9b1f50e599e703f8ef698b5995ce9e93842
LtJulius <senator.spartas@googlemail.com>
2024-11-29 22:17:11 +0100

    feat(reflow): added anchor to host a sub-tree

---
+++ src/Engine.Reflow/Widget/Layer/Anchor.cpp
@@ -0,0 +1,62 @@
+#include "Anchor.hpp"
+
+#include <Engine.Asserts/Asserts.hpp>
+#include <Engine.Common/Make.hpp>
+#include <Engine.Common/Math/Coordinates.hpp>
+
+#include "Window/Window.hpp"
+
+using namespace hg::engine::reflow;
+using namespace hg;
+
+Anchor::Anchor() = default;
+
+Anchor::~Anchor() {
+	if (_host) { hide(); }
+};
+
+string Anchor::getTag() const noexcept {
+	return std::format(R"(:Anchor <{:#x}>)", reinterpret_cast<u64>(this));
+}
+
+void Anchor::show(sptr<Widget> content_) {
+	::hg::assertrt(not _host);
+
+	auto root = this->root();
+	::hg::assertrt(static_cast<bool>(root));
+
+	auto window = std::dynamic_pointer_cast<Window, Widget>(root);
+	::hg::assertrt(static_cast<bool>(window));
+
+	_host = make_sptr<Host>(content_);
+	[[maybe_unused]] auto layer = window->requestLayer(_host);
+}
+
+void Anchor::hide() {
+	::hg::assertrt(static_cast<bool>(_host));
+
+	auto root = this->root();
+	::hg::assertrt(static_cast<bool>(root));
+
+	auto window = std::dynamic_pointer_cast<Window, Widget>(root);
+	::hg::assertrt(static_cast<bool>(window));
+
+	window->dropLayer(_host.get());
+	_host.reset();
+}
+
+const ptr<const NullChildren> Anchor::children() const {
+	return get_null_children();
+}
+
+void Anchor::render(ptr<ReflowCommandBuffer> cmd_) {
+	// __noop();
+}
+
+math::vec2 Anchor::prefetchDesiredSize(cref<ReflowState> state_, float scale_) const {
+	return math::vec2_zero;
+}
+
+void Anchor::applyLayout(ref<ReflowState> state_, mref<LayoutContext> ctx_) {
+	// __noop();
+}
---
+++ src/Engine.Reflow/Widget/Layer/Anchor.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "Host.hpp"
+#include "../Widget.hpp"
+
+namespace hg::engine::reflow {
+	class Anchor final :
+		public Widget {
+	public:
+		using this_type = Anchor;
+
+	public:
+		Anchor();
+
+		~Anchor() override;
+
+	public:
+		[[nodiscard]] string getTag() const noexcept override;
+
+	private:
+		sptr<Host> _host;
+
+	public:
+		void show(_Inout_ sptr<Widget> content_);
+
+		void hide();
+
+	public:
+		[[nodiscard]] const ptr<const NullChildren> children() const override;
+
+		void render(ptr<ReflowCommandBuffer> cmd_) override;
+
+	public:
+		math::vec2 prefetchDesiredSize(cref<ReflowState> state_, float scale_) const override;
+
+		void applyLayout(ref<ReflowState> state_, mref<LayoutContext> ctx_) override;
+	};
+}
