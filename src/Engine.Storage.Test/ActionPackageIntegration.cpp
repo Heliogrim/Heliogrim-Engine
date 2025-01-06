d54ac6585616b105cf8f80cfd96b463811ce8e53
LtJulius <senator.spartas@googlemail.com>
2025-01-06 23:33:56 +0100

    test(storage): split package integration test into stepping functions

--- src/Engine.Storage.Test/ActionPackageIntegration.cpp
+++ src/Engine.Storage.Test/ActionPackageIntegration.cpp
@@ -25,6 +25,7 @@
 #include <Engine.Storage.System/StorageSystem.hpp>
 
 #include "Utils.hpp"
+#include "Attribute/PackageGuid.hpp"
 
 /**/
 
@@ -66,6 +67,250 @@ namespace StorageModule {
 		ASSERT_NE(packStorage, nullptr);
 	}
 
+	static auto insertionTest(
+		IStorageRegistry& registry_,
+		const PackageGuid& packGuid_,
+		const ArchiveGuid& initialArchiveGuid_,
+		auto& initialArchiveData_,
+		const StorageSystem& system_
+	) {
+		auto packStore = registry_.findStorageByUrl(PackageUrl { clone(packGuid_) });
+		ASSERT_NE(packStore, nullptr);
+
+		auto initialArchive = BufferArchive {};
+		initialArchive.reserve(initialArchiveData_.size());
+		initialArchive.serializeBytes(
+			const_cast<char*>(initialArchiveData_.data()),
+			initialArchiveData_.size(),
+			ArchiveStreamMode::eStore
+		);
+		initialArchive.setType(ArchiveType::eRaw);
+
+		system_.mutate(
+			::hg::move(packStore).into<PackageStorage>(),
+			[&initialArchive, &initialGuid = initialArchiveGuid_](_Inout_ ref<ReadWritePackage> rwp_) -> bool {
+				auto& linked = rwp_.getLinker().add({ .type = ArchiveType::eUndefined, .guid = initialGuid });
+				linked.changes.emplace_back(
+					package::ArchiveDeltaAdd {
+						.where = streamoff {},
+						.size = static_cast<streamsize>(initialArchive.size()),
+						.data = initialArchive
+					}
+				);
+				return true;
+			}
+		);
+	}
+
+	static auto validateInsertionTest(
+		IStorageRegistry& registry_,
+		const PackageGuid& packGuid_,
+		const StorageSystem& system_,
+		const ArchiveGuid& initialArchiveGuid_,
+		const auto& initialArchiveSize_,
+		const auto& initialArchiveData_
+	) {
+		auto packStore = registry_.findStorageByUrl(PackageUrl { clone(packGuid_) });
+		ASSERT_NE(packStore, nullptr);
+
+		system_.query(
+			::hg::move(packStore).into<PackageStorage>(),
+			[
+				&initialGuid = initialArchiveGuid_, initialArchiveSize_, initialArchiveData_
+			](_In_ cref<ReadonlyPackage> rp_) {
+
+				ASSERT_GE(rp_.getHeader().packageSize, initialArchiveSize_);
+				ASSERT_GE(rp_.getHeader().indexSize, sizeof(package::Indexed));
+
+				EXPECT_NE(reinterpret_cast<u64>(rp_.getFooter().crc32.data()), u64 {});
+
+				/**/
+
+				const auto& linker = rp_.getLinker();
+
+				EXPECT_EQ(linker.count(), 1uLL);
+				EXPECT_EQ(linker.computeArchiveSize(), rp_.getHeader().packageSize - sizeof(package::Indexed));
+				EXPECT_EQ(linker.computeArchiveSize(), package::computeContentSize(rp_.getHeader()));
+				EXPECT_TRUE(linker.contains(initialGuid));
+
+				/**/
+
+				auto linked = linker.get(initialGuid);
+				ASSERT_TRUE(linked.has_value());
+
+				EXPECT_EQ(linked->header.guid, initialGuid);
+				EXPECT_EQ(linked->header.type, ArchiveType::eRaw);
+
+				/**/
+
+				constexpr auto contentOffset = sizeof(PackageHeader);
+				auto archive = StorageReadonlyArchive {
+					linker.getStorage(),
+					static_cast<streamoff>(contentOffset + linked->data.offset),
+					linked->data.size
+				};
+				ASSERT_EQ(archive.totalSize(), initialArchiveSize_);
+
+				for (const auto& ch : initialArchiveData_) {
+					char ac {};
+					archive >> ac;
+					EXPECT_EQ(ch, ac);
+				}
+			}
+		);
+	}
+
+	static auto replaceTest(
+		IStorageRegistry& registry_,
+		const PackageGuid& packGuid_,
+		const auto& replaceArchiveData_,
+		const StorageSystem& system_,
+		const ArchiveGuid& initialArchiveGuid_
+	) {
+		auto packStore = registry_.findStorageByUrl(PackageUrl { clone(packGuid_) });
+		ASSERT_NE(packStore, nullptr);
+
+		auto replaceArchive = BufferArchive {};
+		replaceArchive.reserve(replaceArchiveData_.size());
+		replaceArchive.serializeBytes(
+			const_cast<char*>(replaceArchiveData_.data()),
+			replaceArchiveData_.size(),
+			ArchiveStreamMode::eStore
+		);
+		replaceArchive.setType(ArchiveType::eSerializedStructure);
+
+		system_.mutate(
+			::hg::move(packStore).into<PackageStorage>(),
+			[&replaceArchive, &initialGuid = initialArchiveGuid_](_Inout_ ref<ReadWritePackage> rwp_) {
+
+				auto linked = rwp_.getLinker().get(initialGuid);
+				ASSERT_TRUE(linked.has_value());
+
+				linked->changes.emplace_back(
+					package::ArchiveDeltaReplace {
+						.where = streamoff {},
+						.size = static_cast<streamsize>(replaceArchive.size()),
+						.data = replaceArchive
+					}
+				);
+			}
+		);
+	}
+
+	static auto validateReplaceTest(
+		const IStorageRegistry& registry_,
+		const PackageGuid& packGuid_,
+		const StorageSystem& system_,
+		const ArchiveGuid& initialArchiveGuid_,
+		const auto& replaceArchiveSize_,
+		const auto& replaceArchiveData_
+	) {
+		auto packStore = registry_.findStorageByUrl(PackageUrl { clone(packGuid_) });
+		ASSERT_NE(packStore, nullptr);
+
+		system_.query(
+			::hg::move(packStore).into<PackageStorage>(),
+			[
+				&initialGuid = initialArchiveGuid_, replaceArchiveSize_, replaceArchiveData_
+			](_In_ ref<const ReadonlyPackage> rp_) {
+
+				ASSERT_GE(rp_.getHeader().packageSize, replaceArchiveSize_);
+				ASSERT_GE(rp_.getHeader().indexSize, sizeof(package::Indexed));
+
+				EXPECT_NE(reinterpret_cast<u64>(rp_.getFooter().crc32.data()), u64 {});
+
+				/**/
+
+				const auto& linker = rp_.getLinker();
+
+				EXPECT_EQ(linker.count(), 1uLL);
+				EXPECT_EQ(linker.computeArchiveSize(), rp_.getHeader().packageSize - sizeof(package::Indexed));
+				EXPECT_EQ(linker.computeArchiveSize(), package::computeContentSize(rp_.getHeader()));
+				EXPECT_TRUE(linker.contains(initialGuid));
+
+				/**/
+
+				auto linked = linker.get(initialGuid);
+				ASSERT_TRUE(linked.has_value());
+
+				EXPECT_EQ(linked->header.guid, initialGuid);
+				EXPECT_EQ(linked->header.type, ArchiveType::eSerializedStructure);
+
+				/**/
+
+				constexpr auto contentOffset = sizeof(PackageHeader);
+				auto archive = StorageReadonlyArchive {
+					linker.getStorage(),
+					static_cast<streamoff>(contentOffset + linked->data.offset),
+					linked->data.size
+				};
+				ASSERT_EQ(archive.totalSize(), replaceArchiveSize_);
+
+				for (const auto& ch : replaceArchiveData_) {
+					char ac {};
+					archive >> ac;
+					EXPECT_EQ(ch, ac);
+				}
+			}
+		);
+	}
+
+	static auto removeTest(
+		const IStorageRegistry& registry_,
+		const PackageGuid& packGuid_,
+		const StorageSystem& system_,
+		const auto& initialArchiveGuid_
+	) {
+		auto packStore = registry_.findStorageByUrl(PackageUrl { clone(packGuid_) });
+		ASSERT_NE(packStore, nullptr);
+
+		system_.mutate(
+			::hg::move(packStore).into<PackageStorage>(),
+			[&initialGuid = initialArchiveGuid_](_Inout_ ref<ReadWritePackage> rwp_) {
+
+				auto linked = rwp_.getLinker().get(initialGuid);
+				ASSERT_TRUE(linked.has_value());
+
+				linked->changes.emplace_back(
+					package::ArchiveDeltaDrop {
+						.where = streamoff {},
+						.size = linked->data.size
+					}
+				);
+			}
+		);
+	}
+
+	static auto validateRemoveTest(
+		const IStorageRegistry& registry_,
+		const PackageGuid& packGuid_,
+		const StorageSystem& system_,
+		const auto& initialArchiveGuid_
+	) {
+		auto packStore = registry_.findStorageByUrl(PackageUrl { clone(packGuid_) });
+		ASSERT_NE(packStore, nullptr);
+
+		system_.query(
+			::hg::move(packStore).into<PackageStorage>(),
+			[&initialGuid = initialArchiveGuid_](_In_ ref<const ReadonlyPackage> rp_) {
+
+				ASSERT_EQ(rp_.getHeader().packageSize, 0uLL);
+				ASSERT_GE(rp_.getHeader().indexSize, 0uLL);
+
+				EXPECT_NE(reinterpret_cast<u64>(rp_.getFooter().crc32.data()), u64 {});
+
+				/**/
+
+				const auto& linker = rp_.getLinker();
+
+				EXPECT_EQ(linker.count(), 0uLL);
+				EXPECT_EQ(linker.computeArchiveSize(), 0uLL);
+				EXPECT_EQ(linker.computeArchiveSize(), package::computeContentSize(rp_.getHeader()));
+				EXPECT_FALSE(linker.contains(initialGuid));
+			}
+		);
+	}
+
 	TEST_F(MutationIntegrationFixture, MultiStepPackageDirectCycle) {
 
 		auto memGuid = Guid {};
@@ -88,222 +333,26 @@ namespace StorageModule {
 
 		/* I : Insertion testing */
 
-		{
-			auto packStore = _registry.findStorageByUrl(PackageUrl { clone(packGuid) });
-			ASSERT_NE(packStore, nullptr);
-
-			auto initialArchive = BufferArchive {};
-			initialArchive.reserve(initial_archive_data.size());
-			initialArchive.serializeBytes(
-				const_cast<char*>(initial_archive_data.data()),
-				initial_archive_data.size(),
-				ArchiveStreamMode::eStore
-			);
-			initialArchive.setType(ArchiveType::eRaw);
-
-			_system.mutate(
-				::hg::move(packStore).into<PackageStorage>(),
-				[&initialArchive, &initialGuid = initial_archive_guid](_Inout_ ref<ReadWritePackage> rwp_) -> bool {
-					auto& linked = rwp_.getLinker().add({ .type = ArchiveType::eUndefined, .guid = initialGuid });
-					linked.changes.emplace_back(
-						package::ArchiveDeltaAdd {
-							.where = streamoff {},
-							.size = static_cast<streamsize>(initialArchive.size()),
-							.data = initialArchive
-						}
-					);
-					return true;
-				}
-			);
-		}
+		insertionTest(_registry, packGuid, initial_archive_guid, initial_archive_data, _system);
 
 		/* I : Validating insertion */
 
-		{
-			auto packStore = _registry.findStorageByUrl(PackageUrl { clone(packGuid) });
-			ASSERT_NE(packStore, nullptr);
-
-			_system.query(
-				::hg::move(packStore).into<PackageStorage>(),
-				[
-					&initialGuid = initial_archive_guid, initial_archive_size, initial_archive_data
-				](_In_ cref<ReadonlyPackage> rp_) {
-
-					ASSERT_GE(rp_.getHeader().packageSize, initial_archive_size);
-					ASSERT_GE(rp_.getHeader().indexSize, sizeof(package::Indexed));
-
-					EXPECT_NE(reinterpret_cast<u64>(rp_.getFooter().crc32.data()), u64 {});
-
-					/**/
-
-					const auto& linker = rp_.getLinker();
-
-					EXPECT_EQ(linker.count(), 1uLL);
-					EXPECT_EQ(linker.computeArchiveSize(), rp_.getHeader().packageSize - sizeof(package::Indexed));
-					EXPECT_EQ(linker.computeArchiveSize(), package::computeContentSize(rp_.getHeader()));
-					EXPECT_TRUE(linker.contains(initialGuid));
-
-					/**/
-
-					auto linked = linker.get(initialGuid);
-					ASSERT_TRUE(linked.has_value());
-
-					EXPECT_EQ(linked->header.guid, initialGuid);
-					EXPECT_EQ(linked->header.type, ArchiveType::eRaw);
-
-					/**/
-
-					constexpr auto contentOffset = sizeof(PackageHeader);
-					auto archive = StorageReadonlyArchive {
-						linker.getStorage(),
-						static_cast<streamoff>(contentOffset + linked->data.offset),
-						linked->data.size
-					};
-					ASSERT_EQ(archive.totalSize(), initial_archive_size);
-
-					for (const auto& ch : initial_archive_data) {
-						char ac {};
-						archive >> ac;
-						EXPECT_EQ(ch, ac);
-					}
-				}
-			);
-		}
+		validateInsertionTest(_registry, packGuid, _system, initial_archive_guid, initial_archive_size, initial_archive_data);
 
 		/* II : Replace initial data testing */
 
-		{
-			auto packStore = _registry.findStorageByUrl(PackageUrl { clone(packGuid) });
-			ASSERT_NE(packStore, nullptr);
-
-			auto replaceArchive = BufferArchive {};
-			replaceArchive.reserve(replace_archive_data.size());
-			replaceArchive.serializeBytes(
-				const_cast<char*>(replace_archive_data.data()),
-				replace_archive_data.size(),
-				ArchiveStreamMode::eStore
-			);
-			replaceArchive.setType(ArchiveType::eSerializedStructure);
-
-			_system.mutate(
-				::hg::move(packStore).into<PackageStorage>(),
-				[&replaceArchive, &initialGuid = initial_archive_guid](_Inout_ ref<ReadWritePackage> rwp_) {
-
-					auto linked = rwp_.getLinker().get(initialGuid);
-					ASSERT_TRUE(linked.has_value());
-
-					linked->changes.emplace_back(
-						package::ArchiveDeltaReplace {
-							.where = streamoff {},
-							.size = static_cast<streamsize>(replaceArchive.size()),
-							.data = replaceArchive
-						}
-					);
-				}
-			);
-		}
+		replaceTest(_registry, packGuid, replace_archive_data, _system, initial_archive_guid);
 
 		/* II : Validating replacement */
 
-		{
-			auto packStore = _registry.findStorageByUrl(PackageUrl { clone(packGuid) });
-			ASSERT_NE(packStore, nullptr);
-
-			_system.query(
-				::hg::move(packStore).into<PackageStorage>(),
-				[
-					&initialGuid = initial_archive_guid, replace_archive_size, replace_archive_data
-				](_In_ ref<const ReadonlyPackage> rp_) {
-
-					ASSERT_GE(rp_.getHeader().packageSize, replace_archive_size);
-					ASSERT_GE(rp_.getHeader().indexSize, sizeof(package::Indexed));
-
-					EXPECT_NE(reinterpret_cast<u64>(rp_.getFooter().crc32.data()), u64 {});
-
-					/**/
-
-					const auto& linker = rp_.getLinker();
-
-					EXPECT_EQ(linker.count(), 1uLL);
-					EXPECT_EQ(linker.computeArchiveSize(), rp_.getHeader().packageSize - sizeof(package::Indexed));
-					EXPECT_EQ(linker.computeArchiveSize(), package::computeContentSize(rp_.getHeader()));
-					EXPECT_TRUE(linker.contains(initialGuid));
-
-					/**/
-
-					auto linked = linker.get(initialGuid);
-					ASSERT_TRUE(linked.has_value());
-
-					EXPECT_EQ(linked->header.guid, initialGuid);
-					EXPECT_EQ(linked->header.type, ArchiveType::eSerializedStructure);
-
-					/**/
-
-					constexpr auto contentOffset = sizeof(PackageHeader);
-					auto archive = StorageReadonlyArchive {
-						linker.getStorage(),
-						static_cast<streamoff>(contentOffset + linked->data.offset),
-						linked->data.size
-					};
-					ASSERT_EQ(archive.totalSize(), replace_archive_size);
-
-					for (const auto& ch : replace_archive_data) {
-						char ac {};
-						archive >> ac;
-						EXPECT_EQ(ch, ac);
-					}
-				}
-			);
-		}
+		validateReplaceTest(_registry, packGuid, _system, initial_archive_guid, replace_archive_size, replace_archive_data);
 
 		/* III : Removing replace data testing */
 
-		{
-			auto packStore = _registry.findStorageByUrl(PackageUrl { clone(packGuid) });
-			ASSERT_NE(packStore, nullptr);
-
-			_system.mutate(
-				::hg::move(packStore).into<PackageStorage>(),
-				[&initialGuid = initial_archive_guid](_Inout_ ref<ReadWritePackage> rwp_) {
-
-					auto linked = rwp_.getLinker().get(initialGuid);
-					ASSERT_TRUE(linked.has_value());
-
-					linked->changes.emplace_back(
-						package::ArchiveDeltaDrop {
-							.where = streamoff {},
-							.size = linked->data.size
-						}
-					);
-				}
-			);
-		}
+		removeTest(_registry, packGuid, _system, initial_archive_guid);
 
 		/* III : Validating removal */
 
-		{
-			auto packStore = _registry.findStorageByUrl(PackageUrl { clone(packGuid) });
-			ASSERT_NE(packStore, nullptr);
-
-			_system.query(
-				::hg::move(packStore).into<PackageStorage>(),
-				[&initialGuid = initial_archive_guid](_In_ ref<const ReadonlyPackage> rp_) {
-
-					ASSERT_EQ(rp_.getHeader().packageSize, 0uLL);
-					ASSERT_GE(rp_.getHeader().indexSize, 0uLL);
-
-					EXPECT_NE(reinterpret_cast<u64>(rp_.getFooter().crc32.data()), u64 {});
-
-					/**/
-
-					const auto& linker = rp_.getLinker();
-
-					EXPECT_EQ(linker.count(), 0uLL);
-					EXPECT_EQ(linker.computeArchiveSize(), 0uLL);
-					EXPECT_EQ(linker.computeArchiveSize(), package::computeContentSize(rp_.getHeader()));
-					EXPECT_FALSE(linker.contains(initialGuid));
-				}
-			);
-		}
+		validateRemoveTest(_registry, packGuid, _system, initial_archive_guid);
 	}
 }
