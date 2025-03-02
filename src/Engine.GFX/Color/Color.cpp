9a0448471b0909a09af0b00b564a06f366810629
LtJulius <senator.spartas@googlemail.com>
2025-03-02 17:47:25 +0100

    feat: moved fanned-out color constructor to header supporting constexpr

--- src/Engine.GFX/Color/Color.cpp
+++ src/Engine.GFX/Color/Color.cpp
@@ -4,39 +4,36 @@ using namespace hg::engine;
 using namespace hg::math;
 
 color::color() :
-    math::fvec4() {}
-
-color::color(const float& r_, const float& g_, const float& b_, const float& a_) :
-    math::fvec4(r_, g_, b_, a_) {}
+	math::fvec4() {}
 
 color::color(const fvec3& rgb_, const float a_) :
-    math::fvec4(rgb_, a_) {}
+	math::fvec4(rgb_, a_) {}
 
 color::color(const fvec4& rgba_) :
-    math::fvec4(rgba_) {}
+	math::fvec4(rgba_) {}
 
 fvec3 color::rgb() const {
-    return fvec4::xyz();
+	return fvec4::xyz();
 }
 
 fvec4 color::rgba() const {
-    return *static_cast<const fvec4*>(this);
+	return *static_cast<const fvec4*>(this);
 }
 
 fvec3 color::hsl() const {
-    // TODO: return types::fvec4::xyz();
-    return {};
+	// TODO: return types::fvec4::xyz();
+	return {};
 }
 
 fvec4 color::hsla() const {
-    // TODO *static_cast<const types::fvec4*>(this);
-    return {};
+	// TODO *static_cast<const types::fvec4*>(this);
+	return {};
 }
 
 color::operator vec3_t<float>() const {
-    return fvec4::xyz();
+	return fvec4::xyz();
 }
 
 color::operator vec4_t<float>() const {
-    return *static_cast<const fvec4*>(this);
+	return *static_cast<const fvec4*>(this);
 }
--- src/Engine.GFX/Color/Color.hpp
+++ src/Engine.GFX/Color/Color.hpp
@@ -1,110 +1,112 @@
 #pragma once
+
 #include <Engine.Common/Math/Vector.hpp>
 
 namespace hg::engine {
-    class color :
-        public math::fvec4 {
-    public:
-        /**
-         * Default constructor
-         *
-         * @author Julius
-         * @date 31.08.2020
-         */
-        color();
+	class color :
+		public math::fvec4 {
+	public:
+		/**
+		 * Default constructor
+		 *
+		 * @author Julius
+		 * @date 31.08.2020
+		 */
+		color();
 
-        /**
-         * Constructor
-         *
-         * @author Julius
-         * @date 31.08.2020
-         *
-         * @param  r_ The r.
-         * @param  g_ The g.
-         * @param  b_ The b.
-         * @param  a_ (Optional) A.
-         */
-        color(const float& r_, const float& g_, const float& b_, const float& a_ = 1.F);
+		/**
+		 * Constructor
+		 *
+		 * @author Julius
+		 * @date 31.08.2020
+		 *
+		 * @param  r_ The r.
+		 * @param  g_ The g.
+		 * @param  b_ The b.
+		 * @param  a_ (Optional) A.
+		 */
+		constexpr color(const float& r_, const float& g_, const float& b_, const float& a_ = 1.F) noexcept:
+			math::fvec4(r_, g_, b_, a_) {}
 
-        /**
-         * Constructor
-         *
-         * @author Julius
-         * @date 31.08.2020
-         *
-         * @param  rgb_ The RGB.
-         * @param  a_ (Optional) A.
-         */
-        color(const math::fvec3& rgb_, const float a_ = 1.F);
+		/**
+		 * Constructor
+		 *
+		 * @author Julius
+		 * @date 31.08.2020
+		 *
+		 * @param  rgb_ The RGB.
+		 * @param  a_ (Optional) A.
+		 */
+		color(const math::fvec3& rgb_, const float a_ = 1.F);
 
-        /**
-         * Constructor
-         *
-         * @author Julius
-         * @date 31.08.2020
-         *
-         * @param  rgba_ The RGBA.
-         */
-        color(const math::fvec4& rgba_);
+		/**
+		 * Constructor
+		 *
+		 * @author Julius
+		 * @date 31.08.2020
+		 *
+		 * @param  rgba_ The RGBA.
+		 */
+		color(const math::fvec4& rgba_);
 
-        /**
-         * Gets the RGB space values
-         *
-         * @author Julius
-         * @date 31.08.2020
-         *
-         * @returns A types::fvec3.
-         */
-        [[nodiscard]] math::fvec3 rgb() const;
+		/**
+		 * Gets the RGB space values
+		 *
+		 * @author Julius
+		 * @date 31.08.2020
+		 *
+		 * @returns A types::fvec3.
+		 */
+		[[nodiscard]] math::fvec3 rgb() const;
 
-        /**
-         * Gets the RGBA space values
-         *
-         * @author Julius
-         * @date 31.08.2020
-         *
-         * @returns A types::fvec4.
-         */
-        [[nodiscard]] math::fvec4 rgba() const;
+		/**
+		 * Gets the RGBA space values
+		 *
+		 * @author Julius
+		 * @date 31.08.2020
+		 *
+		 * @returns A types::fvec4.
+		 */
+		[[nodiscard]] math::fvec4 rgba() const;
 
-        /**
-         * Gets the HSL space values
-         *
-         * @author Julius
-         * @date 31.08.2020
-         *
-         * @returns A types::fvec3.
-         */
-        [[nodiscard]] math::fvec3 hsl() const;
+		/**
+		 * Gets the HSL space values
+		 *
+		 * @author Julius
+		 * @date 31.08.2020
+		 *
+		 * @returns A types::fvec3.
+		 */
+		[[nodiscard]] math::fvec3 hsl() const;
 
-        /**
-         * Gets the HSLA space values
-         *
-         * @author Julius
-         * @date 31.08.2020
-         *
-         * @returns A types::fvec4.
-         */
-        [[nodiscard]] math::fvec4 hsla() const;
+		/**
+		 * Gets the HSLA space values
+		 *
+		 * @author Julius
+		 * @date 31.08.2020
+		 *
+		 * @returns A types::fvec4.
+		 */
+		[[nodiscard]] math::fvec4 hsla() const;
 
-        /**
-         * Converts this to a vector 3
-         *
-         * @author Julius
-         * @date 17.11.2020
-         *
-         * @returns The result of the operation.
-         */
-        operator math::fvec3() const;
+		/**
+		 * Converts this to a vector 3
+		 *
+		 * @author Julius
+		 * @date 17.11.2020
+		 *
+		 * @returns The result of the operation.
+		 */
+		operator math::fvec3() const;
 
-        /**
-         * Converts this to a vector 4
-         *
-         * @author Julius
-         * @date 17.11.2020
-         *
-         * @returns The result of the operation.
-         */
-        operator math::fvec4() const;
-    };
+		/**
+		 * Converts this to a vector 4
+		 *
+		 * @author Julius
+		 * @date 17.11.2020
+		 *
+		 * @returns The result of the operation.
+		 */
+		operator math::fvec4() const;
+	};
 }
