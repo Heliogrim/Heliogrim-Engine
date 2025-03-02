13cb8f08ce75c70e563f7e6ea9e6b102298cbe87
LtJulius <senator.spartas@googlemail.com>
2025-03-02 18:08:03 +0100

    feat(reflow): reworked widget state into technical and interactive state flags

--- src/Engine.Reflow/Widget/WidgetState.hpp
+++ src/Engine.Reflow/Widget/WidgetState.hpp
@@ -4,81 +4,104 @@
 #include <Engine.Common/Math/Vector.hpp>
 
 namespace hg::engine::reflow {
-    enum class WidgetStateFlagBits : u8 {
-        eFocus = 0b0000'0001,
-        eHover = 0b0000'0010,
-        eVisible = 0b0000'0100,
-        /**/
-        ePending = 0b0001'0000,
-        ePendingInherit = 0b0010'0000
-    };
+	enum class InteractiveStateFlagBits : u8 {
+		eFocus = 0b0000'0001,
+		eHover = 0b0000'0010,
+		eDisable = 0b0000'0100,
+		ePress = 0b0000'1000,
+		eDrag = 0b0001'0000,
+	};
 
-    typedef Flag<WidgetStateFlagBits, hg::u8> WidgetStateFlag;
+	enum class WidgetStateFlagBits : u8 {
+		eFocus = 0b0000'0001,
+		eHover = 0b0000'0010,
+		eDisable = 0b0000'0100,
+		ePress = 0b0000'1000,
+		eDrag = 0b0001'0000,
+		/**/
+		eVisible = 0b0010'0000,
+		ePending = 0b0100'0000,
+		ePendingInherit = 0b1000'0000
+	};
 
-    struct WidgetState :
-        public WidgetStateFlag {
-    public:
-        constexpr WidgetState(const WidgetStateFlag initialFlags_) noexcept :
-            Flag(initialFlags_) {}
+	typedef Flag<WidgetStateFlagBits, hg::u8> WidgetStateFlag;
 
-    public:
-        [[nodiscard]] bool isFocus() const noexcept {
-            return (*this) & WidgetStateFlagBits::eFocus;
-        }
+	struct WidgetState :
+		public WidgetStateFlag {
+	public:
+		constexpr WidgetState(const WidgetStateFlag initialFlags_) noexcept :
+			Flag(initialFlags_) {}
 
-        [[nodiscard]] bool isHover() const noexcept {
-            return (*this) & WidgetStateFlagBits::eHover;
-        }
+	public:
+		[[nodiscard]] bool isFocus() const noexcept {
+			return (*this) & WidgetStateFlagBits::eFocus;
+		}
 
-        [[nodiscard]] bool isVisible() const noexcept {
-            return (*this) & WidgetStateFlagBits::eVisible;
-        }
+		[[nodiscard]] bool isHover() const noexcept {
+			return (*this) & WidgetStateFlagBits::eHover;
+		}
 
-        [[nodiscard]] bool isPending() const noexcept {
-            return (*this) & WidgetStateFlagBits::ePending;
-        }
+		[[nodiscard]] bool isDisabled() const noexcept {
+			return (*this) & WidgetStateFlagBits::eDisable;
+		}
 
-        [[nodiscard]] bool isPendingInherit() const noexcept {
-            return (*this) & WidgetStateFlagBits::ePendingInherit;
-        }
+		[[nodiscard]] bool isPressed() const noexcept {
+			return (*this) & WidgetStateFlagBits::ePress;
+		}
 
-        [[nodiscard]] bool isProxyPending() const noexcept {
-            return unwrap & (static_cast<value_type>(WidgetStateFlagBits::ePending) |
-                static_cast<value_type>(WidgetStateFlagBits::ePendingInherit));
-        }
+		[[nodiscard]] bool isDragged() const noexcept {
+			return (*this) & WidgetStateFlagBits::eDrag;
+		}
 
-    public:
-        void set(const WidgetStateFlagBits flag_) {
-            (*this) |= flag_;
-        }
+		[[nodiscard]] bool isVisible() const noexcept {
+			return (*this) & WidgetStateFlagBits::eVisible;
+		}
 
-        void unset(const WidgetStateFlagBits flag_) {
-            this->unwrap &= (~static_cast<WidgetState::value_type>(flag_));
-        }
+		[[nodiscard]] bool isPending() const noexcept {
+			return (*this) & WidgetStateFlagBits::ePending;
+		}
 
-    private:
-        u16 _proxyRenderStateVersion = 0u;
-        u16 _renderStateVersion = 0u;
+		[[nodiscard]] bool isPendingInherit() const noexcept {
+			return (*this) & WidgetStateFlagBits::ePendingInherit;
+		}
 
-    public:
-        [[nodiscard]] constexpr bool shouldRender(const u16 compareVersion_) const noexcept {
-            return _renderStateVersion >= compareVersion_ || _proxyRenderStateVersion >= compareVersion_;
-        }
+		[[nodiscard]] bool isProxyPending() const noexcept {
+			return unwrap & (static_cast<value_type>(WidgetStateFlagBits::ePending) |
+				static_cast<value_type>(WidgetStateFlagBits::ePendingInherit));
+		}
 
-        [[nodiscard]] constexpr bool shouldRenderSelf(const u16 compareVersion_) const noexcept {
-            return _renderStateVersion >= compareVersion_;
-        }
+	public:
+		void set(const WidgetStateFlagBits flag_) {
+			(*this) |= flag_;
+		}
 
-        [[nodiscard]] constexpr bool shouldProxyRender(const u16 compareVersion_) const noexcept {
-            return _proxyRenderStateVersion >= compareVersion_;
-        }
+		void unset(const WidgetStateFlagBits flag_) {
+			this->unwrap &= (~static_cast<WidgetState::value_type>(flag_));
+		}
 
-        constexpr void setRenderVersion(const u16 version_) {
-            _renderStateVersion = version_;
-        }
+	private:
+		u16 _proxyRenderStateVersion = 0u;
+		u16 _renderStateVersion = 0u;
 
-        constexpr void setProxyRenderVersion(const u16 version_) {
-            _proxyRenderStateVersion = version_;
-        }
-    };
+	public:
+		[[nodiscard]] constexpr bool shouldRender(const u16 compareVersion_) const noexcept {
+			return _renderStateVersion >= compareVersion_ || _proxyRenderStateVersion >= compareVersion_;
+		}
+
+		[[nodiscard]] constexpr bool shouldRenderSelf(const u16 compareVersion_) const noexcept {
+			return _renderStateVersion >= compareVersion_;
+		}
+
+		[[nodiscard]] constexpr bool shouldProxyRender(const u16 compareVersion_) const noexcept {
+			return _proxyRenderStateVersion >= compareVersion_;
+		}
+
+		constexpr void setRenderVersion(const u16 version_) {
+			_renderStateVersion = version_;
+		}
+
+		constexpr void setProxyRenderVersion(const u16 version_) {
+			_proxyRenderStateVersion = version_;
+		}
+	};
 }
