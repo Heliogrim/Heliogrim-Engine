4ed4fa4c36f3eb282e62ed06405f4edbc9e50dcb
LtJulius <senator.spartas@googlemail.com>
2025-03-07 23:42:06 +0100

    feat(reflow): migrate float input as forwarding widget

--- src/Engine.Reflow/Widget/Input/InputFloat.cpp
+++ src/Engine.Reflow/Widget/Input/InputFloat.cpp
@@ -8,131 +8,138 @@ using namespace hg::engine::reflow;
 using namespace hg;
 
 InputFloat::InputFloat() :
-    Input(),
-    _wrapper(make_sptr<HorizontalPanel>()),
-    _text(make_sptr<Text>()),
-    _value(R"(0)"),
-    _limits(std::numeric_limits<input_type>::min(), std::numeric_limits<input_type>::max()),
-    _stepSize(0),
-    _precision(InputFloatPrecision::eFloat),
-    _children() {
-    /**/
-    _wrapper->addChild(_text);
-    _children.setChild(_wrapper);
+	Input(),
+	_wrapper(make_sptr<uikit::HorizontalLayout>()),
+	_text(make_sptr<uikit::Text>()),
+	_value(R"(0)"),
+	_limits(std::numeric_limits<input_type>::min(), std::numeric_limits<input_type>::max()),
+	_stepSize(0),
+	_precision(InputFloatPrecision::eFloat),
+	_children() {
+	/**/
+	_wrapper->addChild(_text);
+	_children.setChild(_wrapper);
 }
 
 InputFloat::~InputFloat() = default;
 
 string InputFloat::getTag() const noexcept {
-    return std::format(R"(InputFloat <{:#x}>)", reinterpret_cast<u64>(this));
+	return std::format(R"(InputFloat <{:#x}>)", reinterpret_cast<u64>(this));
 }
 
 void InputFloat::valueToText() {
 
-    const auto val { value() };
+	const auto val { value() };
 
-    string txt {};
-    if (_precision == InputFloatPrecision::eFloat) {
-        txt = std::format(R"({:f})", static_cast<float>(val));
-    } else {
-        txt = std::format(R"({:f})", static_cast<long double>(val));
-    }
+	string txt {};
+	if (_precision == InputFloatPrecision::eFloat) {
+		txt = std::format(R"({:f})", static_cast<float>(val));
+	} else {
+		txt = std::format(R"({:f})", static_cast<long double>(val));
+	}
 
-    auto it { txt.rbegin() };
-    for (; it != txt.rend(); ++it) {
-        if ((*it) == '.') {
-            ++it;
-            break;
-        }
+	auto it { txt.rbegin() };
+	for (; it != txt.rend(); ++it) {
+		if ((*it) == '.') {
+			++it;
+			break;
+		}
 
-        if ((*it) != '0') {
-            break;
-        }
-    }
-    txt.erase(it.base(), txt.end());
+		if ((*it) != '0') {
+			break;
+		}
+	}
+	txt.erase(it.base(), txt.end());
 
-    _text->setText(txt);
+	_text->setText(txt);
 }
 
 void InputFloat::setValue(const input_type value_) {
-    _value = std::format(R"({:})", value_);
-    valueToText();
+	_value = std::format(R"({:})", value_);
+	valueToText();
 }
 
 void InputFloat::setLimits(const input_type lower_, const input_type upper_) {
-    _limits.min = lower_;
-    _limits.max = upper_;
+	_limits.min = lower_;
+	_limits.max = upper_;
 }
 
 void InputFloat::setStepSize(const input_type stepSize_) {
-    _stepSize = stepSize_;
+	_stepSize = stepSize_;
 }
 
 void InputFloat::setPrecision(const InputFloatPrecision precision_) {
-    _precision = precision_;
+	_precision = precision_;
 }
 
 void InputFloat::updateValueAndValidity(const bool propagate_, const bool emit_) {
 
-    // TODO: Try to poll last value
+	// TODO: Try to poll last value
 
-    const auto val { value() };
+	const auto val { value() };
 
-    if (_precision == InputFloatPrecision::eFloat) {
-        _value = std::format(R"({})", static_cast<float>(val));
-    } else if (_precision == InputFloatPrecision::eDouble) {
-        _value = std::format(R"({})", static_cast<long double>(val));
-    }
+	if (_precision == InputFloatPrecision::eFloat) {
+		_value = std::format(R"({})", static_cast<float>(val));
+	} else if (_precision == InputFloatPrecision::eDouble) {
+		_value = std::format(R"({})", static_cast<long double>(val));
+	}
 
-    if (val < _limits.min || _limits.max < val) {
-        _status = InputControlStatus::eInvalid;
-    }
+	if (val < _limits.min || _limits.max < val) {
+		_status = InputControlStatus::eInvalid;
+	}
 
-    _status = InputControlStatus::eValid;
+	_status = InputControlStatus::eValid;
 
-    // TODO:
-    if (_callback) {
-        _callback(value());
-    }
+	// TODO:
+	if (_callback) {
+		_callback(value());
+	}
 }
 
 InputFloat::input_type InputFloat::value() const noexcept {
-    if (_value.empty()) {
-        return 0.0;
-    }
+	if (_value.empty()) {
+		return 0.0;
+	}
 
-    return std::stold(_value);
+	return std::stold(_value);
 }
 
 const ptr<const SingleChildren> InputFloat::children() const {
-    return &_children;
+	return &_children;
 }
 
 void InputFloat::render(const ptr<ReflowCommandBuffer> cmd_) {
-    _wrapper->render(cmd_);
+	_wrapper->render(cmd_);
 }
 
-math::vec2 InputFloat::prefetchDesiredSize(cref<ReflowState> state_, float scale_) const {
-    _wrapper->setParent(const_cast<this_type*>(this)->shared_from_this());
-    return _children.getChild()->getDesiredSize();
+PrefetchSizing InputFloat::prefetchSizing(ReflowAxis axis_, ref<const ReflowState> state_) const {
+	_wrapper->setParent(const_cast<this_type*>(this)->shared_from_this());
+	return {
+		_children.getChild()->getLayoutState().prefetchMinSize,
+		_children.getChild()->getLayoutState().prefetchSize
+	};
 }
 
-math::vec2 InputFloat::computeDesiredSize(cref<ReflowPassState> passState_) const {
-    return _children.getChild()->computeDesiredSize(passState_);
+PassPrefetchSizing InputFloat::passPrefetchSizing(ReflowAxis axis_, ref<const ReflowPassState> passState_) const {
+	return _children.getChild()->passPrefetchSizing(axis_, passState_);
 }
 
-void InputFloat::applyLayout(ref<ReflowState> state_, mref<LayoutContext> ctx_) {
-    const auto childState = state_.getStateOf(_children.getChild());
-    childState->layoutOffset = ctx_.localOffset;
-    childState->layoutSize = ctx_.localSize;
+void InputFloat::computeSizing(ReflowAxis axis_, ref<const ReflowPassState> passState_) {
+	_children.getChild()->getLayoutState().computeSize = passState_.computeSize;
 }
 
-float InputFloat::shrinkFactor() const noexcept {
-    return _wrapper->shrinkFactor();
+void InputFloat::applyLayout(ref<ReflowState> state_) {
+	const auto childState = state_.getStateOf(_children.getChild());
+	childState->layoutOffset = getLayoutState().layoutOffset;
+	childState->layoutSize = getLayoutState().layoutSize;
 }
 
-float InputFloat::growFactor() const noexcept {
-    return _wrapper->growFactor();
+math::fvec2 InputFloat::getShrinkFactor() const noexcept {
+	return _children.getChild()->getShrinkFactor();
+}
+
+math::fvec2 InputFloat::getGrowFactor() const noexcept {
+	return _children.getChild()->getGrowFactor();
 }
 
 EventResponse InputFloat::onFocus(cref<FocusEvent> event_) {
@@ -150,85 +157,85 @@ EventResponse InputFloat::onBlur(cref<FocusEvent> event_) {
 
 EventResponse InputFloat::onKeyDown(cref<KeyboardEvent> event_) {
 
-    if (not _state.isFocus()) {
-        return EventResponse::eUnhandled;
-    }
+	if (not _state.isFocus()) {
+		return EventResponse::eUnhandled;
+	}
 
-    if (
-        not(event_._key >= 0x30 && event_._key <= 0x39) &&
-        not(event_._key >= 0x2C && event_._key <= 0x2E) &&
-        not(event_._key != '-') &&
-        not(event_._key == '\b' || event_._key == '\r' || event_._key == '\x1B')
-    ) {
-        return EventResponse::eConsumed;
-    }
+	if (
+		not(event_._key >= 0x30 && event_._key <= 0x39) &&
+		not(event_._key >= 0x2C && event_._key <= 0x2E) &&
+		not(event_._key != '-') &&
+		not(event_._key == '\b' || event_._key == '\r' || event_._key == '\x1B')
+	) {
+		return EventResponse::eConsumed;
+	}
 
-    if (event_._key == 0x4D) {
+	if (event_._key == 0x4D) {
 
-        if (not _value.empty()) {
-            return EventResponse::eConsumed;
-        }
+		if (not _value.empty()) {
+			return EventResponse::eConsumed;
+		}
 
-        _value.append(&event_._key, 1);
+		_value.append(&event_._key, 1);
 
-    } else if (event_._key == 0x2C || event_._key == 0x2E) {
+	} else if (event_._key == 0x2C || event_._key == 0x2E) {
 
-        if (_value.find(0x2C) != std::string::npos) {
-            return EventResponse::eConsumed;
-        }
+		if (_value.find(0x2C) != std::string::npos) {
+			return EventResponse::eConsumed;
+		}
 
-        if (_value.find(0x2E) != std::string::npos) {
-            return EventResponse::eConsumed;
-        }
+		if (_value.find(0x2E) != std::string::npos) {
+			return EventResponse::eConsumed;
+		}
 
-        //_value.append(&event_._key, 1);
-        constexpr char code { 0x2E };
-        _value.append(&code, 1);
+		//_value.append(&event_._key, 1);
+		constexpr char code { 0x2E };
+		_value.append(&code, 1);
 
-    } else if (event_._key >= 0x30 && event_._key <= 0x39) {
+	} else if (event_._key >= 0x30 && event_._key <= 0x39) {
 
-        const auto maxChars { static_cast<u64>(std::floor(std::log10(static_cast<long double>(_limits.max)))) + 1uLL };
+		const auto maxChars { static_cast<u64>(std::floor(std::log10(static_cast<long double>(_limits.max)))) + 1uLL };
 
-        if (_value.size() >= maxChars) {
-            return EventResponse::eConsumed;
-        }
+		if (_value.size() >= maxChars) {
+			return EventResponse::eConsumed;
+		}
 
-        _value.append(&event_._key, 1);
+		_value.append(&event_._key, 1);
 
-    } else if (event_._key == '-') {
+	} else if (event_._key == '-') {
 
-        if (not _value.empty()) {
-            return EventResponse::eConsumed;
-        }
+		if (not _value.empty()) {
+			return EventResponse::eConsumed;
+		}
 
-        _value.append(&event_._key, 1);
+		_value.append(&event_._key, 1);
 
-    } else if (event_._key == /* Backspace */'\b') {
+	} else if (event_._key == /* Backspace */'\b') {
 
-        if (_value.empty()) {
-            return EventResponse::eConsumed;
-        }
+		if (_value.empty()) {
+			return EventResponse::eConsumed;
+		}
 
-        _value = _value.substr(0, _value.size() - 1);
+		_value = _value.substr(0, _value.size() - 1);
 
-    } else if (event_._key == /* Return */'\r' || event_._key == /* Escape */'\x1B') {
+	} else if (event_._key == /* Return */'\r' || event_._key == /* Escape */'\x1B') {
 
-        const auto window { root() };
-        const FocusEvent focusEvent { window };
+		const auto window { root() };
+		const FocusEvent focusEvent { window };
 
-        WindowManager::get()->dispatch<FocusEvent>(std::static_pointer_cast<Window, Widget>(window), focusEvent);
-        return EventResponse::eConsumed;
-    }
+		WindowManager::get()->dispatch<FocusEvent>(std::static_pointer_cast<Window, Widget>(window), focusEvent);
+		return EventResponse::eConsumed;
+	}
 
-    _text->setText(_value);
-    return EventResponse::eConsumed;
+	_text->setText(_value);
+	return EventResponse::eConsumed;
 }
 
 EventResponse InputFloat::onKeyUp(cref<KeyboardEvent> event_) {
 
-    if (not _state.isFocus()) {
-        return EventResponse::eUnhandled;
-    }
+	if (not _state.isFocus()) {
+		return EventResponse::eUnhandled;
+	}
 
-    return EventResponse::eConsumed;
+	return EventResponse::eConsumed;
 }
--- src/Engine.Reflow/Widget/Input/InputFloat.hpp
+++ src/Engine.Reflow/Widget/Input/InputFloat.hpp
@@ -1,83 +1,90 @@
 #pragma once
 
+#include <Engine.Reflow.Uikit/Atom/Text.hpp>
+#include <Engine.Reflow.Uikit/Atom/Layout/HorizontalLayout.hpp>
+
 #include "../Input.hpp"
-#include "../HorizontalPanel.hpp"
-#include "../Text.hpp"
 #include "../../Children.hpp"
 
 namespace hg::engine::reflow {
-    enum class InputFloatPrecision {
-        eFloat,
-        eDouble
-    };
+	enum class InputFloatPrecision {
+		eFloat,
+		eDouble
+	};
 
-    class InputFloat :
-        public Input<long double> {
-    public:
-        using this_type = InputFloat;
+	class InputFloat :
+		public Input<long double> {
+	public:
+		using this_type = InputFloat;
 
-        using input_type = Input<long double>::input_type;
+		using input_type = Input<long double>::input_type;
 
-    public:
-        InputFloat();
+	public:
+		InputFloat();
 
-        ~InputFloat() override;
+		~InputFloat() override;
 
-    public:
-        [[nodiscard]] string getTag() const noexcept override;
+	public:
+		[[nodiscard]] string getTag() const noexcept override;
 
-    private:
-    public:
-        sptr<HorizontalPanel> _wrapper;
-        sptr<Text> _text;
+	private:
+	public:
+		sptr<uikit::HorizontalLayout> _wrapper;
+		sptr<uikit::Text> _text;
 
-    private:
-        void valueToText();
+	private:
+		void valueToText();
 
-    private:
-        string _value;
+	private:
+		string _value;
 
-        math::vec2_t<input_type> _limits;
-        input_type _stepSize;
+		math::vec2_t<input_type> _limits;
+		input_type _stepSize;
 
-        InputFloatPrecision _precision;
+		InputFloatPrecision _precision;
 
-    public:
-        void setValue(const input_type value_);
+	public:
+		void setValue(const input_type value_);
 
-        void setLimits(
-            const input_type lower_ = std::numeric_limits<input_type>::min(),
-            const input_type upper_ = std::numeric_limits<input_type>::max()
-        );
+		void setLimits(
+			const input_type lower_ = std::numeric_limits<input_type>::min(),
+			const input_type upper_ = std::numeric_limits<input_type>::max()
+		);
 
-        void setStepSize(const input_type stepSize_);
+		void setStepSize(const input_type stepSize_);
 
-        void setPrecision(const InputFloatPrecision precision_);
+		void setPrecision(const InputFloatPrecision precision_);
 
-    public:
-        void updateValueAndValidity(const bool propagate_, const bool emit_) override;
+	public:
+		void updateValueAndValidity(const bool propagate_, const bool emit_) override;
 
-    public:
-        std::function<void(input_type)> _callback = nullptr;
+	public:
+		std::function<void(input_type)> _callback = nullptr;
 
-    public:
-        [[nodiscard]] input_type value() const noexcept override;
+	public:
+		[[nodiscard]] input_type value() const noexcept override;
 
-    private:
-        SingleChildren _children;
+	private:
+		SingleChildren _children;
 
-    public:
-        [[nodiscard]] const ptr<const SingleChildren> children() const override;
+	public:
+		[[nodiscard]] const ptr<const SingleChildren> children() const override;
 
-    public:
-        void render(const ptr<ReflowCommandBuffer> cmd_) override;
+	public:
+		void render(const ptr<ReflowCommandBuffer> cmd_) override;
 
-    public:
-        math::vec2 prefetchDesiredSize(cref<ReflowState> state_, float scale_) const override;
+	public:
+		PrefetchSizing prefetchSizing(ReflowAxis axis_, ref<const ReflowState> state_) const override;
 
-        math::vec2 computeDesiredSize(cref<ReflowPassState> passState_) const override;
+		PassPrefetchSizing passPrefetchSizing(ReflowAxis axis_, ref<const ReflowPassState> passState_) const override;
 
-        void applyLayout(ref<ReflowState> state_, mref<LayoutContext> ctx_) override;
+		void computeSizing(ReflowAxis axis_, ref<const ReflowPassState> passState_) override;
+
+		void applyLayout(ref<ReflowState> state_) override;
+
+		math::fvec2 getGrowFactor() const noexcept override;
+
+		math::fvec2 getShrinkFactor() const noexcept override;
 
     public:
         [[nodiscard]] float shrinkFactor() const noexcept override;
@@ -89,8 +96,8 @@ namespace hg::engine::reflow {
 
         EventResponse onBlur(cref<FocusEvent> event_) override;
 
-        EventResponse onKeyDown(cref<KeyboardEvent> event_) override;
+		EventResponse onKeyDown(cref<KeyboardEvent> event_) override;
 
-        EventResponse onKeyUp(cref<KeyboardEvent> event_) override;
-    };
+		EventResponse onKeyUp(cref<KeyboardEvent> event_) override;
+	};
 }
