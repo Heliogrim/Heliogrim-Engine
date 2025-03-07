b2cdbb9ad64f038169a4ea4c2b510198fe631d70
LtJulius <senator.spartas@googlemail.com>
2025-03-08 00:04:28 +0100

    feat: functional decompose asset input for migration

--- src/Editor.UI/Widget/Input/InputAsset.cpp
+++ src/Editor.UI/Widget/Input/InputAsset.cpp
@@ -12,16 +12,15 @@
 #include <Engine.GFX.Loader/Texture/TextureResource.hpp>
 #include <Engine.GFX.Loader/Texture/Traits.hpp>
 #include <Engine.Logging/Logger.hpp>
-#include <Engine.Reflow/Widget/VerticalPanel.hpp>
+#include <Engine.Reflow.Uikit/Atom/Layout/BoxLayout.hpp>
+#include <Engine.Reflow.Uikit/Atom/Layout/HorizontalLayout.hpp>
 #include <Engine.Resource/LoaderManager.hpp>
 #include <Engine.Resource/ResourceManager.hpp>
 #include <Heliogrim/AssetDatabase.hpp>
 #include <Heliogrim/AssetDatabaseResult.hpp>
 #include <Heliogrim/Heliogrim.hpp>
 
-#include "../../Color/Dark.hpp"
 #include "../../Helper/AssetBrowserHelper.hpp"
-#include "../../Theme/Theme.hpp"
 
 using namespace hg::editor::ui;
 using namespace hg::engine::reflow;
@@ -35,43 +34,40 @@ InputAsset::~InputAsset() = default;
 
 void InputAsset::setup() {
 
-	const auto theme = Theme::get();
-
-	_content = make_sptr<VerticalPanel>();
-	_content->attr.minWidth.setValue({ ReflowUnitType::eAbsolute, 72.F + 4.F + 16.F });
-	_content->attr.width.setValue({ ReflowUnitType::eRelative, 1.F });
-	_content->attr.minHeight.setValue({ ReflowUnitType::eAbsolute, 72.F + 4.F + 20.F });
-	_content->attr.maxHeight.setValue({ ReflowUnitType::eAbsolute, 72.F + 4.F + 20.F });
-	_content->attr.colGap.setValue(4.F);
+	_content = make_sptr<uikit::VerticalLayout>();
+	//_content->attr.minWidth.setValue({ ReflowUnitType::eAbsolute, 72.F + 4.F + 16.F });
+	//_content->attr.width.setValue({ ReflowUnitType::eRelative, 1.F });
+	//_content->attr.minHeight.setValue({ ReflowUnitType::eAbsolute, 72.F + 4.F + 20.F });
+	//_content->attr.maxHeight.setValue({ ReflowUnitType::eAbsolute, 72.F + 4.F + 20.F });
+	//_content->attr.colGap.setValue(4.F);
 
 	_content->setParent(shared_from_this());
 	_children.setChild(_content);
 
 	/**/
 
-	auto upper { make_sptr<HorizontalPanel>() }; {
-		auto& attr = upper->attr;
-		attr.minWidth.setValue({ ReflowUnitType::eAbsolute, 72.F + 4.F + 16.F });
-		attr.width.setValue({ ReflowUnitType::eRelative, 1.F });
+	auto upper { make_sptr<uikit::HorizontalLayout>() }; {
+		auto& attr = upper->getLayoutAttributes().attributeSets;
 		//attr.maxWidth.setValue({ ReflowUnitType::eAbsolute, 72.F + 4.F + 16.F });
-		attr.minHeight.setValue({ ReflowUnitType::eAbsolute, 72.F });
-		attr.height.setValue({ ReflowUnitType::eAbsolute, 72.F });
-		attr.maxHeight.setValue({ ReflowUnitType::eAbsolute, 72.F });
-		attr.rowGap.setValue(4.F);
-		attr.justify.setValue(ReflowSpacing::eSpaceAround);
-		attr.align.setValue(ReflowAlignment::eCenter);
+		std::get<0>(attr).update<attr::BoxLayout::minWidth>({ ReflowUnitType::eAbsolute, 72.F + 4.F + 16.F });
+		std::get<0>(attr).update<attr::BoxLayout::minHeight>({ ReflowUnitType::eAbsolute, 72.F });
+		std::get<0>(attr).update<attr::BoxLayout::minHeight>({ ReflowUnitType::eAbsolute, 72.F });
+		std::get<0>(attr).update<attr::BoxLayout::maxHeight>({ ReflowUnitType::eAbsolute, 72.F });
+		std::get<1>(attr).update<attr::FlexLayout::rowGap>(4.F);
+		std::get<1>(attr).update<attr::FlexLayout::justify>(ReflowSpacing::eSpaceAround);
+		std::get<1>(attr).update<attr::FlexLayout::align>(ReflowAlignment::eCenter);
 	}
 	_content->addChild(upper);
 
-	auto previewBox { make_sptr<BoxPanel>() };
-	previewBox->attributes().width.setValue({ ReflowUnitType::eAbsolute, 72.F });
-	previewBox->attributes().maxWidth.setValue({ ReflowUnitType::eAbsolute, 72.F });
-	previewBox->attributes().height.setValue({ ReflowUnitType::eAbsolute, 72.F });
-	previewBox->attributes().maxHeight.setValue({ ReflowUnitType::eAbsolute, 72.F });
+	auto previewBox { make_sptr<uikit::BoxLayout>() };
+	previewBox->getLayoutAttributes().update<attr::BoxLayout::minWidth>({ ReflowUnitType::eAbsolute, 72.F });
+	previewBox->getLayoutAttributes().update<attr::BoxLayout::maxWidth>({ ReflowUnitType::eAbsolute, 72.F });
+	previewBox->getLayoutAttributes().update<attr::BoxLayout::minHeight>({ ReflowUnitType::eAbsolute, 72.F });
+	previewBox->getLayoutAttributes().update<attr::BoxLayout::maxHeight>({ ReflowUnitType::eAbsolute, 72.F });
 	upper->addChild(previewBox);
 
-	_preview = make_sptr<Image>();
-	theme->applyIcon72(_preview);
+	_preview = make_sptr<uikit::Image>();
+	// TODO: theme->applyIcon72(_preview);
 
 	auto* previewAsset = AssetBrowserHelper::get()->getItemIconByAssetType(asset_type_id {});
 
@@ -82,63 +78,53 @@ void InputAsset::setup() {
 
 	/**/
 
-	_preview->setImage(*previewGuard.imm(), iconRes.get());
+	_preview->getDataAttributes().update<attr::ImageData::image>(*previewGuard.imm());
+	_preview->getDataAttributes().update<attr::ImageData::resource>(iconRes.get());
 	previewBox->setChild(_preview);
 
-	auto actions { make_sptr<VerticalPanel>() };
-	actions->attr.minWidth.setValue({ ReflowUnitType::eAbsolute, 16.F });
-	actions->attr.maxWidth.setValue({ ReflowUnitType::eRelative, 16.F });
-	actions->attr.minHeight.setValue({ ReflowUnitType::eAbsolute, 72.F });
-	actions->attr.maxHeight.setValue({ ReflowUnitType::eAbsolute, 72.F });
-	actions->attr.colGap.setValue(4.F);
+	auto actions { make_sptr<uikit::VerticalLayout>() };
+	//actions->attr.minWidth.setValue({ ReflowUnitType::eAbsolute, 16.F });
+	//actions->attr.maxWidth.setValue({ ReflowUnitType::eRelative, 16.F });
+	//actions->attr.minHeight.setValue({ ReflowUnitType::eAbsolute, 72.F });
+	//actions->attr.maxHeight.setValue({ ReflowUnitType::eAbsolute, 72.F });
+	//actions->attr.colGap.setValue(4.F);
 	upper->addChild(actions);
 
 	/**/
 
-	_search = make_sptr<Button>();
-	theme->applyTextButton(_search);
+	auto txt { make_sptr<uikit::Text>() };
+	txt->setText("S");
+	_search = uikit::makeButton(uikit::TextButtonCreateOptions { .level = 2, .text = ::hg::move(txt) });
 	actions->addChild(_search);
 
-	auto txt { make_sptr<Text>() };
-	theme->applyLabel(txt);
-	txt->setText("S");
-	_search->setChild(txt);
-
-	_reset = make_sptr<Button>();
-	theme->applyTextButton(_reset);
-	actions->addChild(_reset);
-
-	txt = make_sptr<Text>();
-	theme->applyLabel(txt);
+	txt = make_sptr<uikit::Text>();
 	txt->setText("R");
-	_reset->setChild(txt);
+	_reset = uikit::makeButton(uikit::TextButtonCreateOptions { .level = 2, .text = ::hg::move(txt) });
+	actions->addChild(_reset);
 
 	/**/
 
-	auto lower { make_sptr<HorizontalPanel>() };
-	lower->attr.minWidth.setValue({ ReflowUnitType::eAbsolute, 72.F + 4.F + 16.F });
-	lower->attr.width.setValue({ ReflowUnitType::eRelative, 1.F });
-	lower->attr.maxWidth.setValue({ ReflowUnitType::eRelative, 1.F });
-	lower->attr.minHeight.setValue({ ReflowUnitType::eAbsolute, 20.F });
-	lower->attr.maxHeight.setValue({ ReflowUnitType::eAbsolute, 20.F });
+	auto lower { make_sptr<uikit::HorizontalLayout>() }; {
+		auto& attr = lower->getLayoutAttributes().attributeSets;
+		std::get<0>(attr).update<attr::BoxLayout::minWidth>({ ReflowUnitType::eAbsolute, 72.F + 4.F + 16.F });
+		std::get<0>(attr).update<attr::BoxLayout::maxWidth>({ ReflowUnitType::eRelative, 1.F });
+		std::get<0>(attr).update<attr::BoxLayout::minHeight>({ ReflowUnitType::eAbsolute, 20.F });
+		std::get<0>(attr).update<attr::BoxLayout::maxHeight>({ ReflowUnitType::eAbsolute, 20.F });
+	}
 
 	_content->addChild(lower);
 
 	_input = make_sptr<InputText>(); {
-		auto& attr = _input->_wrapper->attr;
-		attr.minWidth.setValue({ ReflowUnitType::eRelative, 1.F });
-		attr.width.setValue({ ReflowUnitType::eRelative, 1.F });
-		attr.maxWidth.setValue({ ReflowUnitType::eRelative, 1.F });
-		attr.minHeight.setValue({ ReflowUnitType::eAbsolute, 20.F });
-		attr.height.setValue({ ReflowUnitType::eAbsolute, 20.F });
-		attr.maxHeight.setValue({ ReflowUnitType::eAbsolute, 20.F });
-		attr.padding.setValue(Padding { 4.F, 2.F });
-		attr.flexShrink.setValue(1.F);
+		auto& attr = _input->_wrapper->getLayoutAttributes().attributeSets;
+		std::get<0>(attr).update<attr::BoxLayout::minWidth>({ ReflowUnitType::eRelative, 1.F });
+		std::get<0>(attr).update<attr::BoxLayout::maxWidth>({ ReflowUnitType::eRelative, 1.F });
+		std::get<0>(attr).update<attr::BoxLayout::minHeight>({ ReflowUnitType::eAbsolute, 20.F });
+		std::get<0>(attr).update<attr::BoxLayout::maxHeight>({ ReflowUnitType::eAbsolute, 20.F });
+		std::get<0>(attr).update<attr::BoxLayout::padding>(Padding { 4.F, 2.F });
 	}
 
-	theme->applyText(_input->_text);
-	_input->_text->attr.textAlign.setValue(TextAlign::eLeftMiddle);
-	_input->_text->attr.textEllipse.setValue(1uL);
+	_input->_text->getStyleAttributes().update<attr::TextStyle::textAlign>(TextAlign::eMiddleLeft);
+	_input->_text->getStyleAttributes().update<attr::TextStyle::textEllipse>(1uL);
 
 	lower->addChild(_input);
 }
@@ -149,12 +135,14 @@ string InputAsset::getTag() const noexcept {
 
 EventResponse InputAsset::onDrop(cref<DragDropEvent> event_) {
 
-	if (event_._type != engine::input::DragDropObjectType::eTextType) {
+	if (not event_._payload.is<engine::input::event::DragDropEventTextPayload>()) {
 		return Input<asset_guid>::onDrop(event_);
 	}
 
-	assert(event_._data._ != nullptr);
-	const auto& text = event_._data.text->data;
+	/**/
+
+	const auto& payload = event_._payload.as<engine::input::event::DragDropEventTextPayload>();
+	const auto& text = payload.data;
 
 	if (not text.starts_with("asset://")) {
 		return Input<asset_guid>::onDrop(event_);
@@ -218,26 +206,31 @@ void InputAsset::render(const ptr<ReflowCommandBuffer> cmd_) {
 	_content->render(cmd_);
 }
 
-math::vec2 InputAsset::prefetchDesiredSize(cref<ReflowState> state_, float scale_) const {
-	return _content->prefetchDesiredSize(state_, scale_);
+PrefetchSizing InputAsset::prefetchSizing(ReflowAxis axis_, ref<const ReflowState> state_) const {
+	::hg::todo_panic();
 }
 
-math::vec2 InputAsset::computeDesiredSize(cref<engine::reflow::ReflowPassState> passState_) const {
-	return _content->computeDesiredSize(passState_);
+engine::reflow::PassPrefetchSizing InputAsset::passPrefetchSizing(
+	engine::reflow::ReflowAxis axis_,
+	ref<const engine::reflow::ReflowPassState> passState_
+) const {
+	::hg::todo_panic();
 }
 
-void InputAsset::applyLayout(ref<ReflowState> state_, mref<LayoutContext> ctx_) {
-	const auto childState = state_.getStateOf(_content);
-	childState->layoutOffset = ctx_.localOffset;
-	childState->layoutSize = ctx_.localSize;
+void InputAsset::computeSizing(ReflowAxis axis_, ref<const ReflowPassState> passState_) {
+	::hg::todo_panic();
 }
 
-float InputAsset::shrinkFactor() const noexcept {
-	return _content->shrinkFactor();
+void InputAsset::applyLayout(ref<ReflowState> state_) {
+	::hg::todo_panic();
 }
 
-float InputAsset::growFactor() const noexcept {
-	return _content->growFactor();
+math::fvec2 InputAsset::getShrinkFactor() const noexcept {
+	return _content->getShrinkFactor();
+}
+
+math::fvec2 InputAsset::getGrowFactor() const noexcept {
+	return _content->getGrowFactor();
 }
 
 void InputAsset::enable() {
--- src/Editor.UI/Widget/Input/InputAsset.hpp
+++ src/Editor.UI/Widget/Input/InputAsset.hpp
@@ -3,102 +3,108 @@
 #include <Engine.Assets/AssetGuid.hpp>
 #include <Engine.Assets/AssetTypeId.hpp>
 #include <Engine.Reflow/Widget/Input.hpp>
-#include <Engine.Reflow/Widget/BoxPanel.hpp>
-#include <Engine.Reflow/Widget/Image.hpp>
-#include <Engine.Reflow/Widget/Button.hpp>
 #include <Engine.Reflow/Widget/Input/InputText.hpp>
-#include <Engine.Reflow/Widget/VerticalPanel.hpp>
+#include <Engine.Reflow.Uikit/Atom/Image.hpp>
+#include <Engine.Reflow.Uikit/Atom/Layout/VerticalLayout.hpp>
+#include <Engine.Reflow.Uikit/Exp/Button.hpp>
 
 namespace hg::editor::ui {
-    class InputAsset :
-        public engine::reflow::Input<asset_guid> {
-    public:
-        using this_type = InputAsset;
+	class InputAsset :
+		public engine::reflow::Input<asset_guid> {
+	public:
+		using this_type = InputAsset;
 
-    public:
-        InputAsset();
+	public:
+		InputAsset();
 
-        ~InputAsset() override;
+		~InputAsset() override;
 
-    public:
-        void setup();
+	public:
+		void setup();
 
-    public:
-        [[nodiscard]] string getTag() const noexcept override;
+	public:
+		[[nodiscard]] string getTag() const noexcept override;
 
-    public:
-        engine::reflow::EventResponse onDrop(cref<engine::reflow::DragDropEvent> event_) override;
+	public:
+		engine::reflow::EventResponse onDrop(cref<engine::reflow::DragDropEvent> event_) override;
 
-        engine::reflow::EventResponse onDragOver(cref<engine::reflow::DragDropEvent> event_) override;
+		engine::reflow::EventResponse onDragOver(cref<engine::reflow::DragDropEvent> event_) override;
 
-    protected:
-        sptr<engine::reflow::Image> _preview;
-        sptr<engine::reflow::InputText> _input;
-        sptr<engine::reflow::Button> _search;
-        sptr<engine::reflow::Button> _reset;
-        sptr<engine::reflow::VerticalPanel> _content;
+	protected:
+		sptr<engine::reflow::uikit::Image> _preview;
+		sptr<engine::reflow::InputText> _input;
+		sptr<engine::reflow::uikit::Button> _search;
+		sptr<engine::reflow::uikit::Button> _reset;
+		sptr<engine::reflow::uikit::VerticalLayout> _content;
 
-        engine::reflow::SingleChildren _children;
+		engine::reflow::SingleChildren _children;
 
-    public:
-        [[nodiscard]] const ptr<const engine::reflow::Children> children() const override;
+	public:
+		[[nodiscard]] const ptr<const engine::reflow::Children> children() const override;
 
-    public:
-        void render(
-            const ptr<engine::reflow::ReflowCommandBuffer> cmd_
-        ) override;
+	public:
+		void render(
+			const ptr<engine::reflow::ReflowCommandBuffer> cmd_
+		) override;
 
-    public:
-        math::vec2 prefetchDesiredSize(cref<engine::reflow::ReflowState> state_, float scale_) const override;
+	public:
+		engine::reflow::PrefetchSizing prefetchSizing(
+			engine::reflow::ReflowAxis axis_,
+			ref<const engine::reflow::ReflowState> state_
+		) const override;
 
-        math::vec2 computeDesiredSize(cref<engine::reflow::ReflowPassState> passState_) const override;
+		engine::reflow::PassPrefetchSizing passPrefetchSizing(
+			engine::reflow::ReflowAxis axis_,
+			ref<const engine::reflow::ReflowPassState> passState_
+		) const override;
 
-        void applyLayout(ref<engine::reflow::ReflowState> state_, mref<engine::reflow::LayoutContext> ctx_) override;
+		void computeSizing(engine::reflow::ReflowAxis axis_, ref<const engine::reflow::ReflowPassState> passState_) override;
 
-    public:
-        [[nodiscard]] float shrinkFactor() const noexcept override;
+		void applyLayout(ref<engine::reflow::ReflowState> state_) override;
 
-        [[nodiscard]] float growFactor() const noexcept override;
+		[[nodiscard]] math::fvec2 getShrinkFactor() const noexcept override;
 
-    public:
-        void enable() override;
+		[[nodiscard]] math::fvec2 getGrowFactor() const noexcept override;
 
-        void disable() override;
+	public:
+		void enable() override;
 
-        void markAsDirty() override;
+		void disable() override;
 
-        void markAsPristine() override;
+		void markAsDirty() override;
 
-        void markAsTouched() override;
+		void markAsPristine() override;
 
-        void markAsUntouched() override;
+		void markAsTouched() override;
 
-        void reset() override;
+		void markAsUntouched() override;
 
-        void updateValueAndValidity(const bool propagate_, const bool emit_) override;
+		void reset() override;
 
-    protected:
-        asset_guid _value;
+		void updateValueAndValidity(const bool propagate_, const bool emit_) override;
 
-    public:
-        [[nodiscard]] input_type value() const noexcept override;
+	protected:
+		asset_guid _value;
 
-        void setValue(cref<asset_guid> assetGuid_);
+	public:
+		[[nodiscard]] input_type value() const noexcept override;
 
-        /**/
+		void setValue(cref<asset_guid> assetGuid_);
 
-    protected:
-        Vector<asset_type_id> _acceptedTypes;
+		/**/
 
-    public:
-        [[nodiscard]] cref<Vector<asset_type_id>> acceptedTypes() const noexcept;
+	protected:
+		Vector<asset_type_id> _acceptedTypes;
 
-        void addAcceptedType(asset_type_id typeId_);
+	public:
+		[[nodiscard]] cref<Vector<asset_type_id>> acceptedTypes() const noexcept;
 
-        void dropAcceptedType(asset_type_id typeId_);
+		void addAcceptedType(asset_type_id typeId_);
 
-    private:
-    public:
-        std::function<void(input_type)> _callback = nullptr;
-    };
+		void dropAcceptedType(asset_type_id typeId_);
+
+	private:
+	public:
+		std::function<void(input_type)> _callback = nullptr;
+	};
 }
