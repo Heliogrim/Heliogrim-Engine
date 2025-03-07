a5c129afd2433c51f8d35cb355973e139de432b6
LtJulius <senator.spartas@googlemail.com>
2025-03-07 23:45:03 +0100

    feat(reflow): migrate text input as forwarding widget

--- src/Engine.Reflow/Widget/Input/InputText.cpp
+++ src/Engine.Reflow/Widget/Input/InputText.cpp
@@ -8,55 +8,70 @@ using namespace hg::engine::reflow;
 using namespace hg;
 
 InputText::InputText() :
-    Input(),
-    _wrapper(make_sptr<HorizontalPanel>()),
-    _text(make_sptr<Text>()),
-    _children() {
-    /**/
-    _wrapper->addChild(_text);
-    _children.setChild(_wrapper);
+	Input(),
+	_wrapper(make_sptr<uikit::HorizontalLayout>()),
+	_text(make_sptr<uikit::Text>()),
+	_children() {
+	/**/
+	_wrapper->addChild(_text);
+	_children.setChild(_wrapper);
 }
 
 InputText::~InputText() = default;
 
 string InputText::getTag() const noexcept {
-    return std::format(R"(InputText <{:#x}>)", reinterpret_cast<u64>(this));
+	return std::format(R"(InputText <{:#x}>)", reinterpret_cast<u64>(this));
 }
 
 void InputText::setPlaceholder(cref<string> placeholder_) {
-    _placeholder = placeholder_;
+	_placeholder = placeholder_;
 
-    if (_value.empty() && _text) {
-        _text->setText(_placeholder);
-    }
+	if (_value.empty() && _text) {
+		_text->setText(_placeholder);
+	}
 }
 
 InputText::input_type InputText::value() const noexcept {
-    return _value;
+	return _value;
 }
 
 const ptr<const SingleChildren> InputText::children() const {
-    return &_children;
+	return &_children;
 }
 
 void InputText::render(const ptr<ReflowCommandBuffer> cmd_) {
-    _wrapper->render(cmd_);
-    _text->render(cmd_);
+	_wrapper->render(cmd_);
+	_text->render(cmd_);
 }
 
-math::vec2 InputText::prefetchDesiredSize(cref<ReflowState> state_, float scale_) const {
-    _wrapper->setParent(const_cast<this_type*>(this)->shared_from_this());
-    return _children.getChild()->getDesiredSize();
+PrefetchSizing InputText::prefetchSizing(ReflowAxis axis_, ref<const ReflowState> state_) const {
+	_wrapper->setParent(const_cast<this_type*>(this)->shared_from_this());
+	return {
+		_children.getChild()->getLayoutState().prefetchMinSize,
+		_children.getChild()->getLayoutState().prefetchSize
+	};
 }
 
-math::vec2 InputText::computeDesiredSize(cref<ReflowPassState> passState_) const {
-    return _children.getChild()->getDesiredSize();
+PassPrefetchSizing InputText::passPrefetchSizing(ReflowAxis axis_, ref<const ReflowPassState> passState_) const {
+	return _children.getChild()->passPrefetchSizing(axis_, passState_);
 }
 
-void InputText::applyLayout(ref<ReflowState> state_, mref<LayoutContext> ctx_) {
-    const auto childState = state_.getStateOf(_children.getChild());
-    childState->layoutOffset = ctx_.localOffset;
-    childState->layoutSize = ctx_.localSize;
+void InputText::computeSizing(ReflowAxis axis_, ref<const ReflowPassState> passState_) {
+	_children.getChild()->getLayoutState().computeSize = passState_.computeSize;
+}
+
+void InputText::applyLayout(ref<ReflowState> state_) {
+	const auto childState = state_.getStateOf(_children.getChild());
+	childState->layoutOffset = getLayoutState().layoutOffset;
+	childState->layoutSize = getLayoutState().layoutSize;
+}
+
+math::fvec2 InputText::getGrowFactor() const noexcept {
+	return _children.getChild()->getGrowFactor();
+}
+
+math::fvec2 InputText::getShrinkFactor() const noexcept {
+	return _children.getChild()->getShrinkFactor();
 }
 
 EventResponse InputText::onFocus(cref<FocusEvent> event_) {
@@ -68,10 +83,10 @@ EventResponse InputText::onFocus(cref<FocusEvent> event_) {
 
 EventResponse InputText::onBlur(cref<FocusEvent> event_) {
 
-    if (_value.empty()) {
-        _text->setText(_placeholder);
-        markAsPending();
-    }
+	if (_value.empty()) {
+		_text->setText(_placeholder);
+		markAsPending();
+	}
 
     _wrapper->state().unwrap &= (~static_cast<WidgetState::value_type>(WidgetStateFlagBits::eFocus));
     return Input<std::string>::onBlur(event_);
@@ -79,56 +94,56 @@ EventResponse InputText::onBlur(cref<FocusEvent> event_) {
 
 EventResponse InputText::onKeyDown(cref<KeyboardEvent> event_) {
 
-    if (not _state.isFocus()) {
-        return EventResponse::eUnhandled;
-    }
+	if (not _state.isFocus()) {
+		return EventResponse::eUnhandled;
+	}
 
-    if (event_._key == /* Backspace */'\b') {
-        if (not _value.empty()) {
-            _value = _value.substr(0, _value.size() - 1);
-        }
-        // TODO:
+	if (event_._key == /* Backspace */'\b') {
+		if (not _value.empty()) {
+			_value = _value.substr(0, _value.size() - 1);
+		}
+		// TODO:
 
-    } else if (event_._key == /* Return */'\r' || event_._key == /* Escape */'\x1B') {
+	} else if (event_._key == /* Return */'\r' || event_._key == /* Escape */'\x1B') {
 
-        const auto window { root() };
-        const FocusEvent focusEvent { window };
+		const auto window { root() };
+		const FocusEvent focusEvent { window };
 
-        WindowManager::get()->dispatch<FocusEvent>(std::static_pointer_cast<Window, Widget>(window), focusEvent);
-        return EventResponse::eConsumed;
+		WindowManager::get()->dispatch<FocusEvent>(std::static_pointer_cast<Window, Widget>(window), focusEvent);
+		return EventResponse::eConsumed;
 
-    } else if (event_._key < 0x20 || event_._key > 0x7E) {
-        return EventResponse::eConsumed;
+	} else if (event_._key < 0x20 || event_._key > 0x7E) {
+		return EventResponse::eConsumed;
 
-    } else {
+	} else {
 
-        if (event_._modifier & 0x40 || event_._modifier & 0x80 /* Ctrl + <> */) {
-            // TODO:
+		if (event_._modifier & 0x40 || event_._modifier & 0x80 /* Ctrl + <> */) {
+			// TODO:
 
-        } else if (event_._modifier & 0x1 || event_._modifier & 0x2 /* Shift L/R */) {
+		} else if (event_._modifier & 0x1 || event_._modifier & 0x2 /* Shift L/R */) {
 
-            char code { event_._key };
-            if (code >= 0x61 && code < 0x7A) {
-                code -= 32;
-            }
+			char code { event_._key };
+			if (code >= 0x61 && code < 0x7A) {
+				code -= 32;
+			}
 
-            _value.append(&code, 1);
+			_value.append(&code, 1);
 
-        } else {
-            _value.append(&event_._key, 1);
-        }
+		} else {
+			_value.append(&event_._key, 1);
+		}
 
-    }
+	}
 
-    _text->setText(_value);
-    return EventResponse::eConsumed;
+	_text->setText(_value);
+	return EventResponse::eConsumed;
 }
 
 EventResponse InputText::onKeyUp(cref<KeyboardEvent> event_) {
 
-    if (not _state.isFocus()) {
-        return EventResponse::eUnhandled;
-    }
+	if (not _state.isFocus()) {
+		return EventResponse::eUnhandled;
+	}
 
-    return EventResponse::eConsumed;
+	return EventResponse::eConsumed;
 }
--- src/Engine.Reflow/Widget/Input/InputText.hpp
+++ src/Engine.Reflow/Widget/Input/InputText.hpp
@@ -1,62 +1,69 @@
 #pragma once
 
+#include <Engine.Reflow.Uikit/Atom/Text.hpp>
+#include <Engine.Reflow.Uikit/Atom/Layout/HorizontalLayout.hpp>
+
 #include "../Input.hpp"
-#include "../HorizontalPanel.hpp"
-#include "../Text.hpp"
 #include "../../Children.hpp"
 
 namespace hg::engine::reflow {
-    class InputText :
-        public Input<string> {
-    public:
-        using this_type = InputText;
+	class InputText :
+		public Input<string> {
+	public:
+		using this_type = InputText;
 
-    public:
-        InputText();
+	public:
+		InputText();
 
-        ~InputText() override;
+		~InputText() override;
 
-    public:
-        [[nodiscard]] string getTag() const noexcept override;
+	public:
+		[[nodiscard]] string getTag() const noexcept override;
 
-    private:
-    public:
-        sptr<HorizontalPanel> _wrapper;
-        sptr<Text> _text;
+	private:
+	public:
+		sptr<uikit::HorizontalLayout> _wrapper;
+		sptr<uikit::Text> _text;
 
-    private:
-        string _value;
-        string _placeholder;
+	private:
+		string _value;
+		string _placeholder;
 
-    public:
-        void setPlaceholder(cref<string> placeholder_);
+	public:
+		void setPlaceholder(cref<string> placeholder_);
 
-    public:
-        [[nodiscard]] input_type value() const noexcept override;
+	public:
+		[[nodiscard]] input_type value() const noexcept override;
 
-    private:
-        SingleChildren _children;
+	private:
+		SingleChildren _children;
 
-    public:
-        [[nodiscard]] const ptr<const SingleChildren> children() const override;
+	public:
+		[[nodiscard]] const ptr<const SingleChildren> children() const override;
 
-    public:
-        void render(const ptr<ReflowCommandBuffer> cmd_) override;
+	public:
+		void render(const ptr<ReflowCommandBuffer> cmd_) override;
 
-    public:
-        math::vec2 prefetchDesiredSize(cref<ReflowState> state_, float scale_) const override;
+	public:
+		PrefetchSizing prefetchSizing(ReflowAxis axis_, ref<const ReflowState> state_) const override;
 
-        math::vec2 computeDesiredSize(cref<ReflowPassState> passState_) const override;
+		PassPrefetchSizing passPrefetchSizing(ReflowAxis axis_, ref<const ReflowPassState> passState_) const override;
 
-        void applyLayout(ref<ReflowState> state_, mref<LayoutContext> ctx_) override;
+		void computeSizing(ReflowAxis axis_, ref<const ReflowPassState> passState_) override;
+
+		void applyLayout(ref<ReflowState> state_) override;
+
+		math::fvec2 getGrowFactor() const noexcept override;
+
+		math::fvec2 getShrinkFactor() const noexcept override;
 
     public:
         EventResponse onFocus(cref<FocusEvent> event_) override;
 
         EventResponse onBlur(cref<FocusEvent> event_) override;
 
-        EventResponse onKeyDown(cref<KeyboardEvent> event_) override;
+		EventResponse onKeyDown(cref<KeyboardEvent> event_) override;
 
-        EventResponse onKeyUp(cref<KeyboardEvent> event_) override;
-    };
+		EventResponse onKeyUp(cref<KeyboardEvent> event_) override;
+	};
 }
