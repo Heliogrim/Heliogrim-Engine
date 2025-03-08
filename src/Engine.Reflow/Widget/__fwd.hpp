4868c808cd4fc7e3a7c7756715efda07c2085c22
LtJulius <senator.spartas@googlemail.com>
2025-03-08 19:09:19 +0100

    fix(reflow): drop widget forward declaration from reflow package

--- src/Engine.Reflow/Widget/__fwd.hpp
+++ src/Engine.Reflow/Widget/__fwd.hpp
@@ -6,24 +6,11 @@ namespace hg::engine::reflow {
 
 	/**/
 
-	class BoxPanel;
-	class Button;
-	class Dialog;
-	class GridPanel;
-	class HorizontalPanel;
-	class Image;
-
 	template <typename>
 	class Input;
 
 	class Menu;
 	class NullWidget;
-	class Overlay;
-	class Panel;
-	class Popup;
-	class Text;
-	class UniformGridPanel;
-	class VerticalPanel;
 	class Viewport;
 	class Widget;
 }
