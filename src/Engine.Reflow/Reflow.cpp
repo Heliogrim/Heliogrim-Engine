5b75eb2b4165d5b062b00e58f375a5a618916568
LtJulius <senator.spartas@googlemail.com>
2025-03-12 21:13:49 +0100

    feat(reflow): add theming container to reflow module lifecycle

--- src/Engine.Reflow/Reflow.cpp
+++ src/Engine.Reflow/Reflow.cpp
@@ -2,14 +2,15 @@
 #include "Module/Reflow.hpp"
 
 #include <Engine.Common/Make.hpp>
+#include <Engine.Core/Engine.hpp>
 #include <Engine.Core/Module/CoreDependencies.hpp>
 #include <Engine.Core/Module/DependencyKey.hpp>
 #include <Engine.Core/Module/SubModuleDependency.hpp>
 #include <Engine.Reflow.Schedule/ReflowPipeline.hpp>
+#include <Engine.Reflow.Theming/Theming.hpp>
 #include <Engine.Scheduler/Scheduler.hpp>
 #include <Engine.Scheduler/Pipeline/CompositePipeline.hpp>
 
-#include "Engine.Core/Engine.hpp"
 #include "Window/WindowManager.hpp"
 
 using namespace hg::engine::core;
@@ -17,42 +18,55 @@ using namespace hg::engine;
 using namespace hg;
 
 Reflow::Reflow(const non_owning_rptr<Engine> engine_) :
-    SubModule(engine_),
-    _dependencies(
-        {
-            SubModuleDependency {
-                GraphicsDepKey,
-                SubModuleOrder::eSuccessor,
-                true
-            }
-        }
-    ) {}
+	SubModule(engine_),
+	_dependencies(
+		{
+			SubModuleDependency {
+				GraphicsDepKey,
+				SubModuleOrder::eSuccessor,
+				true
+			}
+		}
+	),
+	_theming() {}
 
 Reflow::~Reflow() = default;
 
 core::DependencyKey Reflow::moduleKey() const noexcept {
-    return ReflowDepKey;
+	return ReflowDepKey;
 }
 
 cref<CompactSet<core::SubModuleDependency>> Reflow::dependencies() const noexcept {
-    return _dependencies;
+	return _dependencies;
 }
 
 void Reflow::setup() {
 
-    /**
-     * Scheduling Pipelines
-     */
-    auto reflowPipeline = make_uptr<reflow::schedule::ReflowPipeline>();
-    _engine->getScheduler()->getCompositePipeline()->addPipeline(std::move(reflowPipeline));
+	_theming = make_uptr<reflow::theming::Theming>();
+
+	/**
+	 * Scheduling Pipelines
+	 */
+	auto reflowPipeline = make_uptr<reflow::schedule::ReflowPipeline>();
+	_engine->getScheduler()->getCompositePipeline()->addPipeline(std::move(reflowPipeline));
 }
 
 void Reflow::start() {
-    reflow::WindowManager::make();
+	reflow::WindowManager::make();
 }
 
 void Reflow::stop() {
-    reflow::WindowManager::destroy();
+	reflow::WindowManager::destroy();
 }
 
-void Reflow::destroy() {}
+void Reflow::destroy() {
+	_theming.reset();
+}
+
+ref<const reflow::theming::Theming> Reflow::getTheming() const noexcept {
+	return *_theming;
+}
+
+ref<reflow::theming::Theming> Reflow::getTheming() noexcept {
+	return *_theming;
+}
--- src/Engine.Reflow/Reflow.hpp
+++ src/Engine.Reflow/Reflow.hpp
@@ -2,32 +2,44 @@
 
 #include <Engine.Core/Module/SubModule.hpp>
 
+namespace hg::engine::reflow::theming {
+	class Theming;
+}
+
 namespace hg::engine {
-    class Reflow :
-        public core::SubModule {
-    public:
-        using this_type = Reflow;
+	class Reflow :
+		public core::SubModule {
+	public:
+		using this_type = Reflow;
 
-    public:
-        Reflow(const non_owning_rptr<Engine> engine_);
+	public:
+		Reflow(const non_owning_rptr<Engine> engine_);
 
-        ~Reflow() override;
+		~Reflow() override;
 
-    private:
-        CompactSet<core::SubModuleDependency> _dependencies;
+	private:
+		CompactSet<core::SubModuleDependency> _dependencies;
 
-    public:
-        [[nodiscard]] core::DependencyKey moduleKey() const noexcept override;
+	public:
+		[[nodiscard]] core::DependencyKey moduleKey() const noexcept override;
 
-        [[nodiscard]] cref<CompactSet<core::SubModuleDependency>> dependencies() const noexcept override;
+		[[nodiscard]] cref<CompactSet<core::SubModuleDependency>> dependencies() const noexcept override;
 
-    public:
-        void setup() override;
+	public:
+		void setup() override;
 
-        void start() override;
+		void start() override;
 
-        void stop() override;
+		void stop() override;
 
-        void destroy() override;
-    };
+		void destroy() override;
+
+	private:
+		UniquePtr<reflow::theming::Theming> _theming;
+
+	public:
+		[[nodiscard]] ref<const reflow::theming::Theming> getTheming() const noexcept;
+
+		[[nodiscard]] ref<reflow::theming::Theming> getTheming() noexcept;
+	};
 }
