970ce187bd785ed738bd1721a3ca8d11e19141fe
LtJulius <senator.spartas@googlemail.com>
2025-03-22 22:56:56 +0100

    feat(ui): add second event name list for editor ui events

---
+++ src/Editor.UI/EditorUiEventNames.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <Engine.Common/String.hpp>
+
+namespace hg::editor::ui {
+	struct EditorUiEventNames {
+		constexpr static auto InputChangeEvent = "InputChangeEvent"sv;
+	};
+}
