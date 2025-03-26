61d82b72353f43c93d73b1f8b114a5d19e1fe599
LtJulius <senator.spartas@googlemail.com>
2025-03-27 00:33:56 +0100

    feat(ui): add naive impl for generic popover menu widgets

--- src/Editor.UI/Widget/Menu.cpp
+++ src/Editor.UI/Widget/Menu.cpp
@@ -1,16 +1,40 @@
 #include "Menu.hpp"
 
 #include <format>
+#include <Engine.Reflow.Uikit/Atom/Layout/VerticalLayout.hpp>
+#include <Engine.Reflow.Uikit/Molecule/Layout/Stack.hpp>
+
+#include "MenuItem.hpp"
 
 using namespace hg::editor::ui;
 using namespace hg::engine::reflow;
 using namespace hg;
 
-editor::ui::Menu::Menu() :
-	CompoundWidget(ReflowClassList {}, nullptr) {}
+editor::ui::Menu::Menu(
+	mref<SharedPtr<uikit::Stack>> content_,
+	mref<SharedPtr<uikit::VerticalLayout>> layout_
+) :
+	CompoundWidget(::hg::move(content_), ReflowClassList {}, nullptr),
+	_layout(::hg::move(layout_)) {}
 
 editor::ui::Menu::~Menu() = default;
 
 string editor::ui::Menu::getTag() const noexcept {
 	return std::format(R"(Menu <{:#x}>)", reinterpret_cast<u64>(this));
+}
+
+void editor::ui::Menu::addMenuItem(mref<SharedPtr<MenuItem>> menuItem_) {
+	_layout->addChild(::hg::move(menuItem_));
+}
+
+SharedPtr<editor::ui::Menu> editor::ui::makeMenu() {
+
+	auto stack = make_sptr<uikit::Stack>();
+	auto layout = make_sptr<uikit::VerticalLayout>();
+	stack->addChild(clone(layout));
+
+	auto menu = make_sptr<Menu>(clone(stack), hg::move(layout));
+	stack->setParent(menu);
+
+	return menu;
 }
--- src/Editor.UI/Widget/Menu.hpp
+++ src/Editor.UI/Widget/Menu.hpp
@@ -2,6 +2,15 @@
 
 #include <Engine.Reflow/Widget/CompoundWidget.hpp>
 
+namespace hg::engine::reflow::uikit {
+	class VerticalLayout;
+	class Stack;
+}
+
+namespace hg::editor::ui {
+	class MenuItem;
+}
+
 namespace hg::editor::ui {
 	class Menu :
 		public engine::reflow::CompoundWidget {
@@ -9,11 +18,24 @@ namespace hg::editor::ui {
 		using this_type = Menu;
 
 	public:
-		Menu();
+		Menu(
+			mref<SharedPtr<engine::reflow::uikit::Stack>> content_,
+			mref<SharedPtr<engine::reflow::uikit::VerticalLayout>> layout_
+		);
 
 		~Menu() override;
 
 	public:
 		[[nodiscard]] string getTag() const noexcept override;
+
+	private:
+		SharedPtr<engine::reflow::uikit::VerticalLayout> _layout;
+
+	public:
+		void addMenuItem(mref<SharedPtr<MenuItem>> menuItem_);
 	};
+
+	/**/
+
+	[[nodiscard]] extern SharedPtr<Menu> makeMenu();
 }
