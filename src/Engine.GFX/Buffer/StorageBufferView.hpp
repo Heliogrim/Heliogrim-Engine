f8897a5b53b527312d69db1aa77e535f26542575
LtJulius <senator.spartas@googlemail.com>
2025-04-17 14:06:48 +0200

    feat(gfx): allow raii for storage buffer view with buffer like objects

--- src/Engine.GFX/Buffer/StorageBufferView.hpp
+++ src/Engine.GFX/Buffer/StorageBufferView.hpp
@@ -1,21 +1,35 @@
 #pragma once
+
 #include <Engine.Common/Memory/MemoryPointer.hpp>
 #include <Engine.Reflect/Inherit/InheritBase.hpp>
 
 #include "BufferLikeObject.hpp"
 
 namespace hg::engine::gfx {
-    class StorageBufferView :
-        public InheritBase<StorageBufferView> {
-    public:
-        using this_type = StorageBufferView;
+	class StorageBufferView :
+		public InheritBase<StorageBufferView> {
+	public:
+		using this_type = StorageBufferView;
 
-    private:
-        nmpt<BufferLikeObject> _blo;
+	public:
+		constexpr StorageBufferView() noexcept = default;
 
-    public:
-        [[nodiscard]] nmpt<BufferLikeObject> object() const noexcept;
+		constexpr StorageBufferView(nmpt<BufferLikeObject> view_) noexcept :
+			InheritBase(),
+			_blo(view_) {}
 
-        void storeBuffer(mref<nmpt<BufferLikeObject>> object_) noexcept;
-    };
+		constexpr StorageBufferView(ref<const this_type>) noexcept = default;
+
+		constexpr StorageBufferView(mref<this_type>) noexcept = default;
+
+		constexpr ~StorageBufferView() noexcept override = default;
+
+	private:
+		nmpt<BufferLikeObject> _blo;
+
+	public:
+		[[nodiscard]] nmpt<BufferLikeObject> object() const noexcept;
+
+		void storeBuffer(mref<nmpt<BufferLikeObject>> object_) noexcept;
+	};
 }
