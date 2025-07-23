c2a552d38a887373a915069f0d158462d54b307c
LtJulius <senator.spartas@googlemail.com>
2025-07-23 15:11:08 +0200

    fix(editor): update universe init to adopt asset handle changes

--- src/Editor.Main/Boot/UniverseInit.cpp
+++ src/Editor.Main/Boot/UniverseInit.cpp
@@ -8,20 +8,21 @@
 #include <Editor.Scene/SceneFactory.hpp>
 #include <Engine.ACS/ActorModule.hpp>
 #include <Engine.Common/Make.hpp>
+#include <Engine.Common/Move.hpp>
 #include <Engine.Core/Session.hpp>
 #include <Engine.Core/Universe.hpp>
 #include <Engine.Core/UniverseContext.hpp>
-
-/**/
-
-#include <Editor.Assets.Default/GfxMaterials/Default.hpp>
-#include <Editor.Assets.Default/Meshes/Default.hpp>
+#include <Heliogrim/Universe/Level.hpp>
 
 using namespace hg::editor::boot;
 using namespace hg::engine;
 using namespace hg;
 
-static void addDefaultSkybox();
+/**/
+
+static void add_default_skybox(mref<Level> level_);
+
+/**/
 
 void editor::boot::initEditorUniverse() {
 
@@ -47,33 +48,32 @@ void editor::boot::initPrimaryUniverse() {
 
 	/**/
 
-	auto scene = SceneFactory::createGameScene();
-	scene->prepare();
-
 	auto level = core::make_root_like_level();
-	auto universe { make_sptr<core::Universe>(std::move(scene), DenseSet<Arci<core::Level>> { clone(level) }) };
-
-	engine->addUniverse(clone(universe));
-	primary->getUniverseContext().setCurrentUniverse(std::move(universe));
+	add_default_skybox(Level { clone(level) });
 
 	/**/
 
-	addDefaultSkybox();
+	auto scene = SceneFactory::createGameScene();
+	scene->prepare();
+
+	auto universe { make_sptr<core::Universe>(std::move(scene), DenseSet<Arci<core::Level>> { clone(level) }) };
+	engine->addUniverse(clone(universe));
+
+	universe->commitLevel(std::move(level));
+	primary->getUniverseContext().setCurrentUniverse(std::move(universe));
 }
 
-#include <Editor.Assets.Default/GfxMaterials/DefaultSkybox.hpp>
+/**/
+
+#include <Editor.Assets.Default/GfxMaterials/Default.hpp>
 #include <Editor.Assets.Default/Meshes/Default.hpp>
 #include <Heliogrim/ActorInitializer.hpp>
 #include <Heliogrim/Heliogrim.hpp>
 #include <Heliogrim/Actor/Actor.hpp>
+#include <Heliogrim/Asset/GeometryAssetHandles.hpp>
 #include <Heliogrim/Component/Scene/Sky/SkyboxComponent.hpp>
-#include <Heliogrim/Core/Session.hpp>
-#include <Heliogrim/Universe/Universe.hpp>
 
-static void addDefaultSkybox() {
-
-	auto session = GetSession();
-	auto primaryUniverse = GetUniverse(session);
+static void add_default_skybox(mref<Level> level_) {
 
 	auto actor { CreateActor() };
 	auto init = ActorInitializer { *Engine::getEngine()->getActors()->getRegistry() };
@@ -89,9 +89,7 @@ static void addDefaultSkybox() {
 	::hg::assertrt(static_cast<bool>(meshQuery));
 	sc->setSkyboxGeometry(meshQuery.value);
 
-
-
 	/**/
 
-	primaryUniverse.addActor(std::move(actor));
+	level_.addActor(::hg::move(actor));
 }
