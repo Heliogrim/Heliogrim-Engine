9a9e724bbf3e5299514fb767d0a10a79dadefc63
LtJulius <senator.spartas@googlemail.com>
2025-12-20 18:08:40 +0100

    fix(gfx): update swapchain setup to guarantee surface capabilities

--- src/Engine.GFX/Swapchain/Swapchain.cpp
+++ src/Engine.GFX/Swapchain/Swapchain.cpp
@@ -4,21 +4,21 @@ using namespace hg::engine::gfx;
 using namespace hg;
 
 void Swapchain::setup(cref<sptr<Device>> device_) {
-    _device = device_;
+	_device = device_;
 }
 
 cref<TextureFormat> Swapchain::format() const noexcept {
-    return _format;
+	return _format;
 }
 
 cref<math::uivec2> Swapchain::extent() const noexcept {
-    return _extent;
+	return _extent;
 }
 
 s64 Swapchain::imageCount() const noexcept {
-    return -1LL;
+	return -1LL;
 }
 
 cref<smr<Texture>> Swapchain::at(u64 idx_) const {
-    return _images[idx_].image;
+	return _images[idx_].image;
 }
