478859bb9a3f2203347946387072a3638c66c840
LtJulius <senator.spartas@googlemail.com>
2025-12-21 02:47:14 +0100

    feat(serial): rebuild hierarchical actor serialize and hydrate with helpers

--- src/Heliogrim/Actor/Light/DirectionalLightActor.Structured.cpp
+++ src/Heliogrim/Actor/Light/DirectionalLightActor.Structured.cpp
@@ -1,7 +1,6 @@
+#include <Engine.Serialization.Structures/Common/Guid.hpp>
 #include <Engine.Serialization/Access/Structure.hpp>
-#include <Engine.Serialization/Structure/IntegralScopedSlot.hpp>
 #include <Engine.Serialization/Structure/StructScopedSlot.hpp>
-#include <Engine.Serialization.Structures/Symbols.hpp>
 
 #include "DirectionalLightActor.hpp"
 
@@ -9,36 +8,32 @@ namespace hg::engine::serialization {
 	template <>
 	void access::Structure<DirectionalLightActor>::serialize(
 		const DirectionalLightActor& self_,
-		mref<StructScopedSlot> slot_
+		mref<StructScopedSlot> record_
 	) {
 
-		slot_.insertSlot<u64>(Symbols::Type) << DirectionalLightActor::typeId.data;
+		insert_named_guid_slot(record_, "directional"sv, self_._directionalLightComponent->getComponentGuid());
 
 		/**/
 
-		access::Structure<Guid>::serialize(self_._guid, slot_.insertStructSlot(Symbols::Guid));
-		access::Structure<DirectionalLightComponent>::serialize(
-			*self_._directionalLightComponent,
-			slot_.insertStructSlot("directional")
-		);
+		access::Structure<Actor>::serialize(self_, ::hg::move(record_));
 	}
 
 	template <>
 	void access::Structure<DirectionalLightActor>::hydrate(
-		cref<StructScopedSlot> slot_,
+		cref<StructScopedSlot> record_,
 		ref<DirectionalLightActor> target_
 	) {
 
-		type_id checkTypeId {};
-		slot_.getSlot<u64>(Symbols::Type) >> checkTypeId.data;
-		::hg::assertrt(checkTypeId == DirectionalLightActor::typeId);
+		access::Structure<Actor>::hydrate(record_, target_);
 
 		/**/
 
-		access::Structure<Guid>::hydrate(slot_.getStructSlot(Symbols::Guid), target_._guid);
-		access::Structure<DirectionalLightComponent>::hydrate(
-			slot_.getStructSlot("directional"),
-			*target_._directionalLightComponent
-		);
+		const auto directionalLightGuid = get_named_guid_slot(record_, "directional"sv);
+		for (auto* const candidate : target_._components.values()) {
+			if (candidate->getComponentGuid() == directionalLightGuid) {
+				target_._directionalLightComponent = static_cast<ptr<DirectionalLightComponent>>(candidate);
+				break;
+			}
+		}
 	}
 }
