999502b4f353a24c74e486d2215300d3b38711e9
LtJulius <senator.spartas@googlemail.com>
2025-12-21 02:41:27 +0100

    feat(serial): add support for sequence record loops

--- src/Engine.Serialization/Structure/SeqScopedSlot.hpp
+++ src/Engine.Serialization/Structure/SeqScopedSlot.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "RecordScopedSlot.hpp"
 #include "ScopedStructureSlot.hpp"
 
 namespace hg::engine::serialization {
@@ -20,5 +21,30 @@ namespace hg::engine::serialization {
 		[[nodiscard]] RecordScopedSlot getRecordSlot(const u64 index_) const;
 
 		[[nodiscard]] s64 getRecordCount() const;
+
+	public:
+		template <typename Fn_> requires std::is_invocable_v<Fn_, RecordScopedSlot&&>
+		void each(Fn_&& fn_) const & {
+			const auto count = getRecordCount();
+			if (count <= 0) {
+				return;
+			}
+
+			for (u64 idx = 0uLL; idx < static_cast<u64>(count); ++idx) {
+				std::invoke(fn_, getRecordSlot(idx));
+			}
+		}
+
+		template <typename Fn_> requires std::is_invocable_v<Fn_, RecordScopedSlot&&, size_t>
+		void each(Fn_&& fn_) const & {
+			const auto count = getRecordCount();
+			if (count <= 0) {
+				return;
+			}
+
+			for (u64 idx = 0uLL; idx < static_cast<u64>(count); ++idx) {
+				std::invoke(fn_, getRecordSlot(idx), idx);
+			}
+		}
 	};
 }
