6dcd9857d49bf3e9a9720097bee00c6d5a2db775
LtJulius <senator.spartas@googlemail.com>
2025-12-21 03:28:25 +0100

    fix(actor): update constructor component creation for derived actor types

--- src/Heliogrim/Actor/Light/DirectionalLightActor.cpp
+++ src/Heliogrim/Actor/Light/DirectionalLightActor.cpp
@@ -7,7 +7,7 @@ using namespace hg;
 DirectionalLightActor::DirectionalLightActor(cref<ActorInitializer> initializer_) :
 	InheritMeta(initializer_),
 	_directionalLightComponent(nullptr) {
-	_directionalLightComponent = initializer_.createComponent<DirectionalLightComponent>(this);
+	_directionalLightComponent = initializer_.createComponent<DirectionalLightComponent>(*this, { .parent = None });
 }
 
 nmpt<DirectionalLightComponent> DirectionalLightActor::getDirectionalLightComponent() const noexcept {
--- src/Heliogrim/Actor/Light/PointLightActor.cpp
+++ src/Heliogrim/Actor/Light/PointLightActor.cpp
@@ -7,7 +7,7 @@ using namespace hg;
 PointLightActor::PointLightActor(cref<ActorInitializer> initializer_) :
 	InheritMeta(initializer_),
 	_pointLightComponent(nullptr) {
-	_pointLightComponent = initializer_.createComponent<PointLightComponent>(this);
+	_pointLightComponent = initializer_.createComponent<PointLightComponent>(*this, { .parent = None });
 }
 
 nmpt<PointLightComponent> PointLightActor::getPointLightComponent() const noexcept {
