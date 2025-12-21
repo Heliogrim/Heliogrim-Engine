48a38446755e5cd2431d7ee7083fe9983cf76914
LtJulius <senator.spartas@googlemail.com>
2025-12-21 16:41:05 +0100

    feat: add explicit string factory functions

--- src/Engine.Common/String.hpp
+++ src/Engine.Common/String.hpp
@@ -4,12 +4,20 @@
 #include <string_view>
 
 namespace hg {
-    using cstr = const char*;
-    using string = std::string;
-    using string_view = std::string_view;
+	using cstr = const char*;
+	using string = std::string;
+	using string_view = std::string_view;
 
-    using String = string;
-    using StringView = string_view;
+	using String = string;
+	using StringView = string_view;
 
-    using namespace ::std::literals::string_view_literals;
+	using namespace ::std::literals::string_view_literals;
+
+	[[nodiscard]] constexpr String make_string_from_repeated(const char ch_, const String::size_type count_) {
+		return String { count_, ch_, String::allocator_type {} };
+	}
+
+	[[nodiscard]] constexpr String make_null_string(const String::size_type count_) {
+		return make_string_from_repeated(0, count_);
+	}
 }
