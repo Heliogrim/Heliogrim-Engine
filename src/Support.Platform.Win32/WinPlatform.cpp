cbe3f3f0f8b5e4e38de16f322b05277d64304d3c
LtJulius <senator.spartas@googlemail.com>
2025-12-21 02:22:56 +0100

    fix(platform): update sdl bindings and code config after upgrade

--- src/Support.Platform.Win32/WinPlatform.cpp
+++ src/Support.Platform.Win32/WinPlatform.cpp
@@ -4,7 +4,7 @@
 
 #include <Engine.Asserts/Breakpoint.hpp>
 #include <Engine.Async/Await/Promise.hpp>
-#include <Engine.Common/SDL2.hpp>
+#include <Engine.Common/SDL3.hpp>
 #include <Engine.Common/stdafx.h>
 #include <Engine.Core/Engine.hpp>
 #include <Engine.Core/Event/SignalShutdownEvent.hpp>
@@ -37,8 +37,8 @@ void WinPlatform::setupThread() {
 			SetThreadDescription(GetCurrentThread(), L"Platform Thread");
 
 			// Each context which interacts with ole requires this init
-			const auto oleInitRes = OleInitialize(NULL);
-			assert(oleInitRes == S_OK);
+			const auto oleInitRes = OleInitialize(nullptr);
+			::hg::assertrt(oleInitRes == S_OK);
 
 			// Loop every signal and dispatch events
 			while (!_platformQueue.finalized()) {
@@ -46,7 +46,7 @@ void WinPlatform::setupThread() {
 			}
 
 			// While exiting, make sure queue is done
-			std::function<void()> fnc { nullptr };
+			std::function < void() > fnc { nullptr };
 			while (_platformQueue.waitPop(fnc)) {
 				fnc();
 			}
@@ -57,7 +57,9 @@ void WinPlatform::setupThread() {
 
 void WinPlatform::tidy() {
 
-	if (not _platformQueue.finalized()) {
+	if (not
+		_platformQueue.finalized()
+	) {
 		_platformQueue.finalize();
 	}
 
@@ -67,7 +69,9 @@ void WinPlatform::tidy() {
 
 	/**/
 
-	if (not _windows.empty()) {
+	if (not
+		_windows.empty()
+	) {
 		IM_CORE_ERROR("Tidy up platform interface failed due to undestructed windows.");
 		#ifdef _DEBUG
 		::hg::breakpoint();
@@ -82,16 +86,21 @@ void WinPlatform::tidy() {
 void WinPlatform::setup() {
 
 	const auto oleInitRes = OleInitialize(NULL);
-	assert(oleInitRes == S_OK);
+	::hg::assertrt(oleInitRes == S_OK);
+	::hg::assertd(not SDL_WasInit(SDL_INIT_VIDEO));
 
 	/**/
 
-	auto sdlInitRes = SDL_Init(SDL_INIT_VIDEO);
-	if (sdlInitRes != 0) {
+	const auto sdlInitSuccess = SDL_InitSubSystem(SDL_INIT_VIDEO);
+	if (not sdlInitSuccess) {
 		IM_CORE_ERROR("SDL could not initialize correctly.");
 		IM_CORE_ERROR(SDL_GetError());
 		throw std::runtime_error("Could not initialize SDL.");
 	}
+
+	// Attention: Configure SDL3 events to prevent registration of drag-drop handler
+	SDL_SetEventEnabled(SDL_EVENT_DROP_FILE, false);
+	SDL_SetEventEnabled(SDL_EVENT_DROP_TEXT, false);
 
 	/**/
 
@@ -114,11 +123,9 @@ hg::concurrent::future<uptr<NativeWindow>> WinPlatform::makeNativeWindow(
 			auto wnd = make_uptr<Win32Window>(
 				SDL_CreateWindow(
 					title.data(),
-					SDL_WINDOWPOS_CENTERED,
-					SDL_WINDOWPOS_CENTERED,
 					extent.width,
 					extent.height,
-					SDL_WINDOW_VULKAN | SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_RESIZABLE
+					SDL_WINDOW_VULKAN | SDL_WINDOW_HIGH_PIXEL_DENSITY | SDL_WINDOW_RESIZABLE
 				),
 				NativeWindowFlags { NativeWindowFlagBits::eMovable } | NativeWindowFlagBits::eResizable | NativeWindowFlagBits::eShown,
 				math::ivec2 { extent.offsetX, extent.offsetY },
@@ -130,6 +137,7 @@ hg::concurrent::future<uptr<NativeWindow>> WinPlatform::makeNativeWindow(
 
 			/**/
 
+			SDL_SetWindowPosition(wnd->sdl(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
 			SDL_SetWindowMinimumSize(wnd->sdl(), 128L, 128L);
 			Engine::getEngine()->getInput()->captureWindow(wnd.get());
 			_windows.push_back(wnd.get());
@@ -141,7 +149,9 @@ hg::concurrent::future<uptr<NativeWindow>> WinPlatform::makeNativeWindow(
 	};
 
 	const auto future { promise.get() };
-	while (!_platformQueue.finalized() && not _platformQueue.try_push(std::move(promise))) {
+	while (!_platformQueue.finalized() && not
+		_platformQueue.try_push(std::move(promise))
+	) {
 		// __noop();
 	}
 
@@ -170,7 +180,9 @@ hg::concurrent::future<bool> WinPlatform::destroyNativeWindow(
 	};
 
 	const auto future { promise.get() };
-	while (!_platformQueue.finalized() && not _platformQueue.try_push(std::move(promise))) {
+	while (!_platformQueue.finalized() && not
+		_platformQueue.try_push(std::move(promise))
+	) {
 		// __noop();
 	}
 
@@ -178,14 +190,13 @@ hg::concurrent::future<bool> WinPlatform::destroyNativeWindow(
 }
 
 void WinPlatform::processInternal() {
-
 	// Poll events to stay responsive
 
 	SDL_Event event;
 	while (SDL_PollEvent(&event)) {
 		// TODO: Replace
 		switch (event.type) {
-			case SDL_EventType::SDL_QUIT: {
+			case SDL_EventType::SDL_EVENT_QUIT: {
 				// Warning: We need to call stop via scheduler, cause this thread will deadlock itself (recursive) due to window ownership and close behavior
 				// Warning: Undefined behavior if called multiple times
 				::hg::engine::scheduler::exec(
@@ -195,61 +206,57 @@ void WinPlatform::processInternal() {
 				);
 				break;
 			}
-			case SDL_EventType::SDL_WINDOWEVENT: {
+			case SDL_EventType::SDL_EVENT_WINDOW_RESIZED: {
 				// Warning: We need to call stop via scheduler
 				// Warning: Temporary solution
-				if (event.window.event == SDL_WINDOWEVENT_RESIZED) {
+				auto* targetWnd = SDL_GetWindowFromID(event.window.windowID);
+				const auto iter {
+					std::find_if(
+						_windows.begin(),
+						_windows.end(),
+						[targetWnd](const auto& entry_) {
+							return static_cast<ptr<Win32Window>>(entry_)->sdl() == targetWnd;
+						}
+					)
+				};
 
-					auto* targetWnd = SDL_GetWindowFromID(event.window.windowID);
-					const auto iter {
-						std::find_if(
-							_windows.begin(),
-							_windows.end(),
-							[targetWnd](const auto& entry_) {
-								return static_cast<ptr<Win32Window>>(entry_)->sdl() == targetWnd;
-							}
-						)
-					};
-
-					if (iter == _windows.end()) {
-						break;
-					}
-
-					Engine::getEngine()->getInput()->bufferEvent(
-						PlatformResizeEvent::typeId,
-						make_uptr<PlatformResizeEvent>(*iter, math::ivec2 { event.window.data1, event.window.data2 })
-					);
+				if (iter == _windows.end()) {
 					break;
 				}
 
-				if (event.window.event == SDL_WINDOWEVENT_CLOSE) {
-					// TODO:
-					break;
-				}
+				Engine::getEngine()->getInput()->bufferEvent(
+					PlatformResizeEvent::typeId,
+					make_uptr<PlatformResizeEvent>(*iter, math::ivec2 { event.window.data1, event.window.data2 })
+				);
+				break;
 			}
-			case SDL_EventType::SDL_KEYUP:
-			case SDL_EventType::SDL_KEYDOWN: {
+			case SDL_EventType::SDL_EVENT_WINDOW_CLOSE_REQUESTED: {
+				// TODO:
+				break;
+			}
+			case SDL_EventType::SDL_EVENT_KEY_UP:
+			case SDL_EventType::SDL_EVENT_KEY_DOWN: {
 				// Warning: Temporary solution
 
-				if (event.key.keysym.sym == SDLK_UNKNOWN) {
+				if (event.key.key == SDLK_UNKNOWN) {
 					break;
 				}
 
 				Engine::getEngine()->getInput()->bufferEvent(
 					input::event::KeyboardEvent::typeId,
 					make_uptr<input::event::KeyboardEvent>(
-						static_cast<char>(event.key.keysym.sym),
-						event.type == SDL_EventType::SDL_KEYDOWN,
-						event.key.keysym.mod
+						static_cast<char>(event.key.key),
+						event.type == SDL_EventType::SDL_EVENT_KEY_DOWN,
+						event.key.mod
 					)
 				);
 				break;
 			}
-			case SDL_EventType::SDL_MOUSEMOTION: {
+			case SDL_EventType::SDL_EVENT_MOUSE_MOTION: {
 				// Warning: Temporary solution
 
-				math::ivec2 point { event.motion.x, event.motion.y };
-				math::ivec2 delta { event.motion.xrel, event.motion.yrel };
+				math::fvec2 point { event.motion.x, event.motion.y };
+				math::fvec2 delta { event.motion.xrel, event.motion.yrel };
 				const auto button { event.motion.state };
 
 				Engine::getEngine()->getInput()->bufferEvent(
@@ -263,30 +270,29 @@ void WinPlatform::processInternal() {
 				);
 				break;
 			}
-			case SDL_EventType::SDL_MOUSEBUTTONDOWN:
-			case SDL_EventType::SDL_MOUSEBUTTONUP: {
+			case SDL_EventType::SDL_EVENT_MOUSE_BUTTON_DOWN:
+			case SDL_EventType::SDL_EVENT_MOUSE_BUTTON_UP: {
 				// Warning: Temporary solution
 
-				math::ivec2 point { event.button.x, event.button.y };
-				const bool down { event.button.state == SDL_PRESSED };
-				u32 button { event.button.button };
+				const auto point = math::fvec2 { event.button.x, event.button.y };
+				const auto button = event.button.button;
 
 				Engine::getEngine()->getInput()->bufferEvent(
 					input::event::MouseButtonEvent::typeId,
 					make_uptr<input::event::MouseButtonEvent>(
 						point,
 						button,
-						down,
+						event.button.down,
 						0uL
 					)
 				);
 				break;
 			}
-			case SDL_EventType::SDL_MOUSEWHEEL: {
+			case SDL_EventType::SDL_EVENT_MOUSE_WHEEL: {
 				// Warning: Temporary solution
 
-				math::ivec2 point { -1, -1 };
-				math::vec2 value { event.wheel.preciseX, event.wheel.preciseY };
+				auto point = math::fvec2 { -1.f, -1.f };
+				const auto value = math::vec2 { event.wheel.mouse_x, event.wheel.mouse_y };
 
 				SDL_GetMouseState(&point.x, &point.y);
 
@@ -299,32 +305,13 @@ void WinPlatform::processInternal() {
 				);
 				break;
 			}
-			case SDL_EventType::SDL_DROPFILE: {
-				// Warning: Temporary solution
-
-				auto* rfile { event.drop.file };
-
-				/*
-				math::ivec2 point { lastPoint };
-				SDL_PumpEvents();
-				[[maybe_unused]] u32 _ { SDL_GetMouseState(&point.x, &point.y) };
-
-				auto type { input::event::DragDropEventType::eDropFileType };
-				void* data { rfile };
-
-				const input::event::DragDropEvent dde { point, type, data };
-				emitter().emit(dde);
-				 */
-
-				SDL_free(rfile);
-			}
 		}
 	}
 
 	/**
 	 * Check for signaled queue
 	 */
-	std::function<void()> fnc { nullptr };
+	std::function < void() > fnc { nullptr };
 	while (!_platformQueue.empty()) {
 		_platformQueue.waitPop(fnc);
 		fnc();
