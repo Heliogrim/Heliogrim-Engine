fdd4d0ed4d694dfe9b38933ace6e5800514348c6
LtJulius <senator.spartas@googlemail.com>
2025-12-21 16:09:04 +0100

    chore: remove obsolete, commented and unused code

--- src/Engine.GFX/MeshPipeline.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "GraphicPipeline.hpp"
-
-namespace hg::engine::gfx {
-    class MeshPipeline :
-        public GraphicPipeline { };
-}
--- src/Engine.GFX/RayTracePipeline.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "GraphicPipeline.hpp"
-
-namespace hg::engine::gfx {
-    class RayTracePipeline :
-        public GraphicPipeline { };
-}
