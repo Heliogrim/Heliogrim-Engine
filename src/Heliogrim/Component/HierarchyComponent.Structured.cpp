17af37bc17397ab19e3f5f99bf8658b5a02d4edd
LtJulius <senator.spartas@googlemail.com>
2025-12-21 03:18:16 +0100

    fix(serial): update hierarchical serialization of derived components

--- src/Heliogrim/Component/HierarchyComponent.Structured.cpp
+++ src/Heliogrim/Component/HierarchyComponent.Structured.cpp
@@ -1,31 +1,35 @@
 #include <Engine.Serialization/Access/Structure.hpp>
-#include <Engine.Serialization/Structure/IntegralScopedSlot.hpp>
 #include <Engine.Serialization/Structure/StructScopedSlot.hpp>
 #include <Engine.Serialization.Structures/Symbols.hpp>
+#include <Engine.Serialization.Structures/Common/Guid.hpp>
 
+#include "ComponentGuid.hpp"
 #include "HierarchyComponent.hpp"
 
 namespace hg::engine::serialization {
 	template <>
 	void access::Structure<HierarchyComponent>::serialize(
 		const HierarchyComponent& self_,
-		mref<StructScopedSlot> slot_
+		mref<StructScopedSlot> record_
 	) {
-
-		slot_.insertSlot<u64>(Symbols::Type) << HierarchyComponent::typeId.data;
+		Structure<math::Bounding>::serialize(self_._boundaries, record_.insertStructSlot("boundaries"));
+		Structure<math::Transform>::serialize(self_._localTransform, record_.insertStructSlot("localTransform"));
 
 		/**/
 
-		Structure<math::Bounding>::serialize(self_._boundaries, slot_.insertStructSlot("boundaries"));
-		Structure<math::Transform>::serialize(self_._localTransform, slot_.insertStructSlot("localTransform"));
+		return Structure<LogicComponent>::serialize(self_, ::hg::move(record_));
 	}
 
 	template <>
 	void access::Structure<HierarchyComponent>::hydrate(
-		cref<StructScopedSlot> slot_,
+		cref<StructScopedSlot> record_,
 		ref<HierarchyComponent> target_
 	) {
-		Structure<math::Bounding>::hydrate(slot_.getStructSlot("boundaries"), target_._boundaries);
-		Structure<math::Transform>::hydrate(slot_.getStructSlot("localTransform"), target_._localTransform);
+		Structure<LogicComponent>::hydrate(record_, target_);
+
+		/**/
+
+		Structure<math::Bounding>::hydrate(record_.getStructSlot("boundaries"), target_._boundaries);
+		Structure<math::Transform>::hydrate(record_.getStructSlot("localTransform"), target_._localTransform);
 	}
 }
--- src/Heliogrim/Component/Scene/Light/SpotLightComponent.Structured.cpp
+++ src/Heliogrim/Component/Scene/Light/SpotLightComponent.Structured.cpp
@@ -1,3 +1,4 @@
+#include <Engine.Common/Move.hpp>
 #include <Engine.Serialization/Access/Structure.hpp>
 #include <Engine.Serialization/Structure/FloatScopedSlot.hpp>
 #include <Engine.Serialization/Structure/IntegralScopedSlot.hpp>
@@ -12,32 +13,29 @@ using namespace hg;
 template <>
 void access::Structure<SpotLightComponent>::serialize(
 	cref<SpotLightComponent> self_,
-	mref<StructScopedSlot> slot_
+	mref<StructScopedSlot> record_
 ) {
-	slot_.insertSlot<u64>(Symbols::Type) << SpotLightComponent::typeId.data;
+	record_.insertSlot<f32>("innerCone") << self_._innerCone;
+	record_.insertSlot<f32>("outerCone") << self_._outerCone;
+	record_.insertSlot<f32>("distanceLimit") << self_._distanceLimit;
+	Structure<math::fvec3>::serialize(self_._luminance, record_.insertStructSlot("luminance"));
 
 	/**/
 
-	slot_.insertSlot<f32>("innerCone") << self_._innerCone;
-	slot_.insertSlot<f32>("outerCone") << self_._outerCone;
-	slot_.insertSlot<f32>("distanceLimit") << self_._distanceLimit;
-	Structure<math::fvec3>::serialize(self_._luminance, slot_.insertStructSlot("luminance"));
-
+	return Structure<HierarchyComponent>::serialize(self_, ::hg::move(record_));
 }
 
 template <>
 void access::Structure<SpotLightComponent>::hydrate(
-	cref<StructScopedSlot> slot_,
+	cref<StructScopedSlot> record_,
 	SpotLightComponent& target_
 ) {
-	component_type_id checkTypeId {};
-	slot_.getSlot<u64>(Symbols::Type) >> checkTypeId.data;
-	::hg::assertrt(checkTypeId == SpotLightComponent::typeId);
+	Structure<HierarchyComponent>::hydrate(record_, target_);
 
 	/**/
 
-	slot_.getSlot<f32>("innerCone") >> target_._innerCone;
-	slot_.getSlot<f32>("outerCone") >> target_._outerCone;
-	slot_.getSlot<f32>("distanceLimit") >> target_._distanceLimit;
-	Structure<math::fvec3>::hydrate(slot_.getStructSlot("luminance"), target_._luminance);
+	record_.getSlot<f32>("innerCone") >> target_._innerCone;
+	record_.getSlot<f32>("outerCone") >> target_._outerCone;
+	record_.getSlot<f32>("distanceLimit") >> target_._distanceLimit;
+	Structure<math::fvec3>::hydrate(record_.getStructSlot("luminance"), target_._luminance);
 }
