7984e5ca8d337bce5fea85047a1dc4330ef95609
LtJulius <senator.spartas@googlemail.com>
2025-12-21 03:34:23 +0100

    feat(scene): implement scene change queue for game scenes

--- src/Engine.Scene.Game/GameScene.cpp
+++ src/Engine.Scene.Game/GameScene.cpp
@@ -1,21 +1,11 @@
 #include "GameScene.hpp"
 
-#include <Engine.Asserts/Todo.hpp>
-#include <Engine.Reflect/Cast.hpp>
-#include <Heliogrim/Component/HierarchyComponent.hpp>
-#include <Heliogrim/Component/SceneComponent.hpp>
-
 using namespace hg::engine::scene;
 using namespace hg;
 
-void GameScene::add(const ptr<HierarchyComponent> component_) {
-	if (auto* const sceneComponent = Cast<SceneComponent>(component_)) {
-		Scene::add(sceneComponent);
-	}
-}
-
-void GameScene::add(cref<ComponentHierarchy> hierarchy_) {
-	::hg::todo_panic();
+void GameScene::broadcast(const SceneBroadcastFlags flags_) {
+	commitChanges<>();
+	Scene::broadcast(flags_);
 }
 
 nmpt<IComponentRegisterContext> GameScene::registerContext() noexcept {
