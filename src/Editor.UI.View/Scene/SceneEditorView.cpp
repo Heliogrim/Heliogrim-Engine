78f2902c59b4e116e90206aa16b1085fde534adf
LtJulius <senator.spartas@googlemail.com>
2025-12-26 17:16:03 +0100

    feat(editor): update editor scene view layout and style

--- src/Editor.UI.View/Scene/SceneEditorView.cpp
+++ src/Editor.UI.View/Scene/SceneEditorView.cpp
@@ -1,7 +1,6 @@
 #include "SceneEditorView.hpp"
 
 #include <Editor.UI.Main/EditorUI.hpp>
-#include <Editor.UI.Main/Module/EditorUI.hpp>
 #include <Editor.UI.Panel/Assets/AssetBrowserController.hpp>
 #include <Editor.UI.Panel/Assets/AssetBrowserView.hpp>
 #include <Editor.UI.Panel/Properties/PropertyController.hpp>
@@ -9,8 +8,6 @@
 #include <Editor.UI.Panel/Scene/EditorSceneViewportPanel.hpp>
 #include <Editor.UI.Panel/Scene/Hierarchy/SceneHierarchyController.hpp>
 #include <Editor.UI.Panel/Scene/Hierarchy/SceneHierarchyView.hpp>
-#include <Engine.Core/Engine.hpp>
-#include <Engine.Core/Module/Modules.hpp>
 #include <Engine.Reflow.Uikit/Atom/Layout/BoxLayout.hpp>
 
 #include "SceneEditorController.hpp"
@@ -27,6 +24,9 @@ UniquePtr<SceneEditorView> editor::ui::makeSceneEditorView(
 		auto& box = std::get<0>(layout.attributeSets);
 		auto& flex = std::get<1>(layout.attributeSets);
 
+		box.update<attr::BoxLayout::maxWidth>(ReflowUnit { ReflowUnitType::eRelative, 1.F });
+		box.update<attr::BoxLayout::maxHeight>(ReflowUnit { ReflowUnitType::eRelative, 1.F });
+
 		box.update<attr::BoxLayout::widthGrow>(1.F);
 		box.update<attr::BoxLayout::heightGrow>(1.F);
 
@@ -38,112 +38,97 @@ UniquePtr<SceneEditorView> editor::ui::makeSceneEditorView(
 
 	/**/
 
-	auto left = make_sptr<uikit::VerticalLayout>(); {
-		auto& layout = left->getLayoutAttributes();
+	auto mainRest = make_sptr<uikit::VerticalLayout>();
+
+	[](ref<SharedPtr<uikit::VerticalLayout>> mainRest_) {
+		auto& layout = mainRest_->getLayoutAttributes();
 		auto& box = std::get<0>(layout.attributeSets);
 		auto& flex = std::get<1>(layout.attributeSets);
 
 		/**/
 
-		box.update<attr::BoxLayout::minWidth>(ReflowUnit { ReflowUnitType::eAbsolute, 360.F });
-		box.update<attr::BoxLayout::maxWidth>(ReflowUnit { ReflowUnitType::eAbsolute, 360.F });
+		box.update<attr::BoxLayout::maxWidth>(ReflowUnit { ReflowUnitType::eRelative, 1.F });
+		box.update<attr::BoxLayout::minHeight>(ReflowUnit { ReflowUnitType::eRelative, 1.F });
+		box.update<attr::BoxLayout::maxHeight>(ReflowUnit { ReflowUnitType::eRelative, 1.F });
+		box.update<attr::BoxLayout::widthGrow>(1.F);
 		box.update<attr::BoxLayout::heightGrow>(1.F);
 
 		flex.update<attr::FlexLayout::justify>(ReflowSpacing::eStart);
 		flex.update<attr::FlexLayout::align>(ReflowAlignment::eStart);
 
 		flex.update<attr::FlexLayout::rowGap>(8.F);
-	} {
+	}(mainRest);
+
+	[](ref<const SceneEditorController> controller_, ref<SharedPtr<uikit::VerticalLayout>> mainRest_) {
+		auto top = make_sptr<uikit::HorizontalLayout>();
+
+		std::get<0>(top->getLayoutAttributes().attributeSets).update<attr::BoxLayout::maxWidth>(
+			ReflowUnit { ReflowUnitType::eRelative, 1.F }
+		);
+		std::get<0>(top->getLayoutAttributes().attributeSets).update<attr::BoxLayout::heightGrow>(1.F);
+		std::get<0>(top->getLayoutAttributes().attributeSets).update<attr::BoxLayout::widthGrow>(1.F);
+
+		std::get<1>(top->getLayoutAttributes().attributeSets).update<attr::FlexLayout::justify>(ReflowSpacing::eStart);
+		std::get<1>(top->getLayoutAttributes().attributeSets).update<attr::FlexLayout::align>(ReflowAlignment::eStart);
+
+		std::get<1>(top->getLayoutAttributes().attributeSets).update<attr::FlexLayout::rowGap>(8.F);
+
+		/**/
+
 		auto sceneViewCard = controller_.sceneHierarchy->getView().main;
 		auto sceneViewBox = make_sptr<uikit::BoxLayout>();
 
-		sceneViewCard->getLayoutAttributes().update<attr::BoxLayout::widthGrow>(1.F);
 		sceneViewCard->getLayoutAttributes().update<attr::BoxLayout::heightGrow>(1.F);
+		sceneViewCard->getLayoutAttributes().update<attr::BoxLayout::widthGrow>(1.F);
 
-		sceneViewBox->getLayoutAttributes().update<attr::BoxLayout::minWidth>({ ReflowUnitType::eRelative, 1.F });
+		sceneViewBox->getLayoutAttributes().update<attr::BoxLayout::heightGrow>(1.F);
+		sceneViewBox->getLayoutAttributes().update<attr::BoxLayout::minWidth>({ ReflowUnitType::eAbsolute, 320.F });
 		sceneViewBox->getLayoutAttributes().update<attr::BoxLayout::maxWidth>({ ReflowUnitType::eRelative, 1.F });
-		sceneViewBox->getLayoutAttributes().update<attr::BoxLayout::minHeight>(
-			ReflowUnit { ReflowUnitType::eRelative, 2.F / 3.F }
-		);
-		sceneViewBox->getLayoutAttributes().update<attr::BoxLayout::maxHeight>(
-			ReflowUnit { ReflowUnitType::eRelative, 2.F / 3.F }
-		);
+		sceneViewBox->getLayoutAttributes().update<attr::BoxLayout::minHeight>(ReflowUnit { ReflowUnitType::eRelative, 1.F });
+		sceneViewBox->getLayoutAttributes().update<attr::BoxLayout::maxHeight>(ReflowUnit { ReflowUnitType::eRelative, 1.F });
 
 		sceneViewBox->setChild(sceneViewCard);
-		left->addChild(::hg::move(sceneViewBox));
+		top->addChild(::hg::move(sceneViewBox));
 
 		/**/
 
-		auto sceneStatsBox = make_sptr<uikit::BoxLayout>();
-
-		sceneStatsBox->getLayoutAttributes().update<attr::BoxLayout::minWidth>({ ReflowUnitType::eRelative, 1.F });
-		sceneStatsBox->getLayoutAttributes().update<attr::BoxLayout::maxWidth>({ ReflowUnitType::eRelative, 1.F });
-		sceneStatsBox->getLayoutAttributes().update<attr::BoxLayout::minHeight>(
-			{ ReflowUnitType::eRelative, 1.F / 3.F }
-		);
-		sceneStatsBox->getLayoutAttributes().update<attr::BoxLayout::maxHeight>(
-			{ ReflowUnitType::eRelative, 1.F / 3.F }
-		);
-
-		left->addChild(::hg::move(sceneStatsBox));
-	}
-
-	/**/
-
-	auto center = make_sptr<uikit::VerticalLayout>(); {
-		auto& layout = center->getLayoutAttributes();
-		auto& box = std::get<0>(layout.attributeSets);
-		auto& flex = std::get<1>(layout.attributeSets);
-
-		/**/
-
-		box.update<attr::BoxLayout::widthGrow>(1.F);
-		box.update<attr::BoxLayout::heightGrow>(1.F);
-
-		flex.update<attr::FlexLayout::justify>(ReflowSpacing::eStart);
-		flex.update<attr::FlexLayout::align>(ReflowAlignment::eCenter);
-
-		flex.update<attr::FlexLayout::rowGap>(8.F);
-	} {
 		auto esvp = EditorSceneViewportPanel::make();
 
-		std::get<0>(esvp->getLayoutAttributes().attributeSets).update<attr::BoxLayout::minWidth>(
-			ReflowUnit { ReflowUnitType::eRelative, 1.F }
-		);
 		std::get<0>(esvp->getLayoutAttributes().attributeSets).update<attr::BoxLayout::maxWidth>(
 			ReflowUnit { ReflowUnitType::eRelative, 1.F }
 		);
+		std::get<0>(esvp->getLayoutAttributes().attributeSets).update<attr::BoxLayout::widthGrow>(1.F);
 		std::get<0>(esvp->getLayoutAttributes().attributeSets).update<attr::BoxLayout::minHeight>(
-			ReflowUnit { ReflowUnitType::eRelative, 2.F / 3.F }
+			ReflowUnit { ReflowUnitType::eRelative, 1.F }
 		);
 		std::get<0>(esvp->getLayoutAttributes().attributeSets).update<attr::BoxLayout::maxHeight>(
-			ReflowUnit { ReflowUnitType::eRelative, 2.F / 3.F }
+			ReflowUnit { ReflowUnitType::eRelative, 1.F }
 		);
 
 		std::get<1>(esvp->getLayoutAttributes().attributeSets).update<attr::FlexLayout::rowGap>(8.F);
 
-		center->addChild(esvp);
+		top->addChild(esvp);
+		mainRest_->addChild(::hg::move(top));
+	}(controller_, mainRest);
 
-		/**/
+	[](ref<const SceneEditorController> controller_, ref<SharedPtr<uikit::VerticalLayout>> mainRest_) {
 
 		auto assetBrowserCard = controller_.assetBrowser->getView().main;
 
 		assetBrowserCard->getLayoutAttributes().update<attr::BoxLayout::minWidth>({ ReflowUnitType::eRelative, 1.F });
 		assetBrowserCard->getLayoutAttributes().update<attr::BoxLayout::maxWidth>({ ReflowUnitType::eRelative, 1.F });
-		assetBrowserCard->getLayoutAttributes().update<attr::BoxLayout::minHeight>(
-			ReflowUnit { ReflowUnitType::eRelative, 1.F / 3.F }
-		);
-		assetBrowserCard->getLayoutAttributes().update<attr::BoxLayout::maxHeight>(
-			ReflowUnit { ReflowUnitType::eRelative, 1.F / 3.F }
-		);
+		assetBrowserCard->getLayoutAttributes().update<attr::BoxLayout::minHeight>(ReflowUnit { ReflowUnitType::eAbsolute, 320.F });
+		assetBrowserCard->getLayoutAttributes().update<attr::BoxLayout::maxHeight>(ReflowUnit { ReflowUnitType::eAbsolute, 320.F });
 
-		center->addChild(::hg::move(assetBrowserCard));
-	}
+		mainRest_->addChild(::hg::move(assetBrowserCard));
+	}(controller_, mainRest);
 
 	/**/
 
-	auto right = make_sptr<uikit::VerticalLayout>(); {
-		auto& layout = right->getLayoutAttributes();
+	auto right = make_sptr<uikit::VerticalLayout>();
+
+	[](ref<SharedPtr<uikit::VerticalLayout>> right_) {
+		auto& layout = right_->getLayoutAttributes();
 		auto& box = std::get<0>(layout.attributeSets);
 		auto& flex = std::get<1>(layout.attributeSets);
 
@@ -157,7 +142,9 @@ UniquePtr<SceneEditorView> editor::ui::makeSceneEditorView(
 		flex.update<attr::FlexLayout::align>(ReflowAlignment::eStart);
 
 		flex.update<attr::FlexLayout::rowGap>(8.F);
-	} {
+	}(right);
+
+	[](ref<const SceneEditorController> controller_, ref<SharedPtr<uikit::VerticalLayout>> right_) {
 		auto propertiesCard = controller_.properties->getView().main;
 		auto propertiesBox = make_sptr<uikit::BoxLayout>();
 
@@ -169,13 +156,12 @@ UniquePtr<SceneEditorView> editor::ui::makeSceneEditorView(
 		propertiesBox->getLayoutAttributes().update<attr::BoxLayout::heightGrow>(1.F);
 
 		propertiesBox->setChild(::hg::move(propertiesCard));
-		right->addChild(::hg::move(propertiesBox));
-	}
+		right_->addChild(::hg::move(propertiesBox));
+	}(controller_, right);
 
 	/**/
 
-	shell->addChild(::hg::move(left));
-	shell->addChild(::hg::move(center));
+	shell->addChild(::hg::move(mainRest));
 	shell->addChild(::hg::move(right));
 	return make_uptr<SceneEditorView>(::hg::move(shell));
 }
