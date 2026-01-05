fe0f7fe42b9d34e4ce85912c166578bc040fffa4
LtJulius <senator.spartas@googlemail.com>
2026-01-05 15:33:13 +0100

    fix(editor): update actor and universe actions and init

--- src/Editor.Scene.Actions/SpawnGeometryActor.cpp
+++ src/Editor.Scene.Actions/SpawnGeometryActor.cpp
@@ -91,7 +91,7 @@ void despawnActorImpl(mref<ActorGuid> actorGuid_) {
 	auto universe = GetUniverse(session);
 
 	auto* actor = engine::Engine::getEngine()->getActors()->getRegistry()->getOrCreateActorPool<Actor>()->get(actorGuid_);
-	::hg::discard = Destroy(::hg::move(actor), universe);
+	universe.dropActor(::hg::move(actor));
 
 	/**/
 
