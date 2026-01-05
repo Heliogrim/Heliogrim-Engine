cb3152f318f6f451a6c5036ec4eec2e2c2aeeaa2
LtJulius <senator.spartas@googlemail.com>
2026-01-05 15:32:00 +0100

    fix(actor): fix stub initialization of actors for deserialization

--- src/Heliogrim/Actor/Light/DirectionalLightActor.cpp
+++ src/Heliogrim/Actor/Light/DirectionalLightActor.cpp
@@ -7,7 +7,10 @@ using namespace hg;
 DirectionalLightActor::DirectionalLightActor(cref<ActorInitializer> initializer_) :
 	InheritMeta(initializer_),
 	_directionalLightComponent(nullptr) {
-	_directionalLightComponent = initializer_.createComponent<DirectionalLightComponent>(*this, { .parent = None });
+
+	if (initializer_.shouldAutoInitialize()) {
+		_directionalLightComponent = initializer_.createComponent<DirectionalLightComponent>(*this, { .parent = None });
+	}
 }
 
 nmpt<DirectionalLightComponent> DirectionalLightActor::getDirectionalLightComponent() const noexcept {
